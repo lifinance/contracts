e14db06bda170574616c0c0bd8c19cb08ede98cc
Satoshi Naoki <satoshi@li.finance>
2022-09-30 11:14:26 +1000

    update arbitrum facet test

--- test/facets/ArbitrumBridgeFacet.test.ts
+++ test/facets/ArbitrumBridgeFacet.test.ts
@@ -1,22 +1,18 @@
 import {
   IERC20 as ERC20,
   IERC20__factory as ERC20__factory,
-  IGatewayRouter,
-  IGatewayRouter__factory,
   ArbitrumBridgeFacet,
   DexManagerFacet,
 } from '../../typechain'
 import { deployments, network } from 'hardhat'
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signers'
-import { constants, Contract, ethers, utils } from 'ethers'
+import { constants, Contract, utils } from 'ethers'
 import { node_url } from '../../utils/network'
 import { expect } from '../chai-setup'
 import approvedFunctionSelectors from '../../utils/approvedFunctions'
-import config from '../../config/arbitrum'
 
 const USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
 const DAI_L1_ADDRESS = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
-const DAI_L2_ADDRESS = '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1'
 const UNISWAP_ADDRESS = '0xE592427A0AEce92De3Edee1F18E0157C05861564'
 const ZERO_ADDRESS = constants.AddressZero
 const SEND_AMOUNT = utils.parseEther('1000')
@@ -34,8 +30,8 @@ describe('ArbitrumBridgeFacet', function () {
   let owner: any
   let dai: ERC20
   let usdc: ERC20
-  let gatewayRouter: IGatewayRouter
   let swapData: any
+  let validBridgeData: any
   let validArbitrumData: any
 
   /* eslint-enable @typescript-eslint/no-explicit-any */
@@ -70,10 +66,19 @@ describe('ArbitrumBridgeFacet', function () {
 
       dai = ERC20__factory.connect(DAI_L1_ADDRESS, alice)
       usdc = ERC20__factory.connect(USDC_ADDRESS, alice)
-      gatewayRouter = IGatewayRouter__factory.connect(
-        config['mainnet'].gatewayRouter,
-        alice
-      )
+
+      validBridgeData = {
+        transactionId: utils.randomBytes(32),
+        bridge: 'arbitrum',
+        integrator: 'ACME Devs',
+        referrer: ZERO_ADDRESS,
+        sendingAssetId: DAI_L1_ADDRESS,
+        receiver: alice.address,
+        minAmount: SEND_AMOUNT,
+        destinationChainId: 42161,
+        hasSourceSwaps: false,
+        hasDestinationCall: false,
+      }
 
       validArbitrumData = {
         maxSubmissionCost: MAX_SUBMISSION_COST,
@@ -144,51 +149,28 @@ describe('ArbitrumBridgeFacet', function () {
   describe('startBridgeTokensViaArbitrumBridge function', () => {
     describe('should be reverted to starts a bridge transaction', () => {
       it('when the sending amount is zero', async function () {
-        const arbitrumData = {
-          ...validArbitrumData,
-        }
         const bridgeData = {
-          transactionId: utils.randomBytes(32),
-          bridge: 'arbitrum',
-          integrator: 'ACME Devs',
-          referrer: ethers.constants.AddressZero,
-          sendingAssetId: DAI_L1_ADDRESS,
-          receiver: alice.address,
+          ...validBridgeData,
           minAmount: 0,
-          destinationChainId: 42161,
-          hasSourceSwaps: false,
-          hasDestinationCall: false,
         }
 
         await expect(
           lifi
             .connect(alice)
-            .startBridgeTokensViaArbitrumBridge(bridgeData, arbitrumData)
+            .startBridgeTokensViaArbitrumBridge(bridgeData, validArbitrumData)
         ).to.be.revertedWith('InvalidAmount()')
       })
 
       it('when the receiver is zero address', async function () {
-        const arbitrumData = {
-          ...validArbitrumData,
-        }
-
         const bridgeData = {
-          transactionId: utils.randomBytes(32),
-          bridge: 'arbitrum',
-          integrator: 'ACME Devs',
-          referrer: ethers.constants.AddressZero,
-          sendingAssetId: DAI_L1_ADDRESS,
-          receiver: ethers.constants.AddressZero,
-          minAmount: SEND_AMOUNT,
-          destinationChainId: 42161,
-          hasSourceSwaps: false,
-          hasDestinationCall: false,
+          ...validBridgeData,
+          receiver: ZERO_ADDRESS,
         }
 
         await expect(
           lifi
             .connect(alice)
-            .startBridgeTokensViaArbitrumBridge(bridgeData, arbitrumData)
+            .startBridgeTokensViaArbitrumBridge(bridgeData, validArbitrumData)
         ).to.be.revertedWith('InvalidReceiver()')
       })
 
@@ -196,69 +178,42 @@ describe('ArbitrumBridgeFacet', function () {
         const daiBalance = await dai.balanceOf(alice.address)
         await dai.transfer(lifi.address, daiBalance)
 
-        const bridgeData = {
-          transactionId: utils.randomBytes(32),
-          bridge: 'arbitrum',
-          integrator: 'ACME Devs',
-          referrer: ethers.constants.AddressZero,
-          sendingAssetId: DAI_L1_ADDRESS,
-          receiver: alice.address,
-          minAmount: SEND_AMOUNT,
-          destinationChainId: 42161,
-          hasSourceSwaps: false,
-          hasDestinationCall: false,
-        }
-
         await expect(
           lifi
             .connect(alice)
-            .startBridgeTokensViaArbitrumBridge(bridgeData, validArbitrumData)
+            .startBridgeTokensViaArbitrumBridge(
+              validBridgeData,
+              validArbitrumData
+            )
         ).to.be.revertedWith('InsufficientBalance')
       })
 
       it('when the user sent no enough gas', async () => {
         const cost = MAX_SUBMISSION_COST.add(MAX_GAS_PRICE.mul(MAX_GAS))
+
+        await expect(
+          lifi
+            .connect(alice)
+            .startBridgeTokensViaArbitrumBridge(
+              validBridgeData,
+              validArbitrumData,
+              {
+                value: cost.sub(1),
+              }
+            )
+        ).to.be.revertedWith('InvalidFee()')
+      })
+
+      it('when the sending native asset amount is not enough', async () => {
         const bridgeData = {
-          transactionId: utils.randomBytes(32),
-          bridge: 'arbitrum',
-          integrator: 'ACME Devs',
-          referrer: ethers.constants.AddressZero,
-          sendingAssetId: DAI_L1_ADDRESS,
-          receiver: alice.address,
-          minAmount: SEND_AMOUNT,
-          destinationChainId: 42161,
-          hasSourceSwaps: false,
-          hasDestinationCall: false,
+          ...validBridgeData,
+          sendingAssetId: ZERO_ADDRESS,
+          minAmount: utils.parseEther('10'),
         }
         await expect(
           lifi
             .connect(alice)
             .startBridgeTokensViaArbitrumBridge(bridgeData, validArbitrumData, {
-              value: cost.sub(1),
-            })
-        ).to.be.revertedWith('InvalidFee()')
-      })
-
-      it('when the sending native asset amount is not enough', async () => {
-        const arbitrumData = {
-          ...validArbitrumData,
-        }
-        const bridgeData = {
-          transactionId: utils.randomBytes(32),
-          bridge: 'arbitrum',
-          integrator: 'ACME Devs',
-          referrer: ethers.constants.AddressZero,
-          sendingAssetId: ethers.constants.AddressZero,
-          receiver: alice.address,
-          minAmount: utils.parseEther('10'),
-          destinationChainId: 42161,
-          hasSourceSwaps: false,
-          hasDestinationCall: false,
-        }
-        await expect(
-          lifi
-            .connect(alice)
-            .startBridgeTokensViaArbitrumBridge(bridgeData, arbitrumData, {
               value: utils.parseEther('9'),
             })
         ).to.be.revertedWith('InvalidAmount()')
@@ -268,29 +223,17 @@ describe('ArbitrumBridgeFacet', function () {
     describe('should be possible to starts a bridge transaction', () => {
       it('when transfer non-native asset', async function () {
         const cost = MAX_SUBMISSION_COST.add(MAX_GAS_PRICE.mul(MAX_GAS))
-        const receivingAssetId = await gatewayRouter.calculateL2TokenAddress(
-          DAI_L1_ADDRESS
-        )
-
-        const bridgeData = {
-          transactionId: utils.randomBytes(32),
-          bridge: 'arbitrum',
-          integrator: 'ACME Devs',
-          referrer: ethers.constants.AddressZero,
-          sendingAssetId: DAI_L1_ADDRESS,
-          receiver: alice.address,
-          minAmount: SEND_AMOUNT,
-          destinationChainId: 42161,
-          hasSourceSwaps: false,
-          hasDestinationCall: false,
-        }
 
         await expect(
           lifi
             .connect(alice)
-            .startBridgeTokensViaArbitrumBridge(bridgeData, validArbitrumData, {
-              value: cost,
-            })
+            .startBridgeTokensViaArbitrumBridge(
+              validBridgeData,
+              validArbitrumData,
+              {
+                value: cost,
+              }
+            )
         ).to.emit(lifi, 'LiFiTransferStarted')
       })
 
@@ -298,16 +241,9 @@ describe('ArbitrumBridgeFacet', function () {
         const cost = MAX_SUBMISSION_COST.add(MAX_GAS_PRICE.mul(MAX_GAS))
 
         const bridgeData = {
-          transactionId: utils.randomBytes(32),
-          bridge: 'arbitrum',
-          integrator: 'ACME Devs',
-          referrer: ethers.constants.AddressZero,
-          sendingAssetId: ethers.constants.AddressZero,
-          receiver: alice.address,
+          ...validBridgeData,
+          sendingAssetId: ZERO_ADDRESS,
           minAmount: utils.parseEther('10'),
-          destinationChainId: 42161,
-          hasSourceSwaps: false,
-          hasDestinationCall: false,
         }
 
         await expect(
@@ -316,9 +252,7 @@ describe('ArbitrumBridgeFacet', function () {
             .startBridgeTokensViaArbitrumBridge(bridgeData, validArbitrumData, {
               value: utils.parseEther('10').add(cost),
             })
-        )
-          .to.emit(lifi, 'LiFiTransferStarted')
-          .withArgs(bridgeData)
+        ).to.emit(lifi, 'LiFiTransferStarted')
       })
     })
   })
@@ -326,22 +260,10 @@ describe('ArbitrumBridgeFacet', function () {
   describe('swapAndStartBridgeTokensViaArbitrumBridge function', () => {
     describe('should be reverted to perform a swap then starts a bridge transaction', () => {
       it('when the receiver is zero address', async function () {
-        const arbitrumData = {
-          ...validArbitrumData,
-          receiver: ZERO_ADDRESS,
-        }
-
         const bridgeData = {
-          transactionId: utils.randomBytes(32),
-          bridge: 'arbitrum',
-          integrator: 'ACME Devs',
-          referrer: ethers.constants.AddressZero,
-          sendingAssetId: DAI_L1_ADDRESS,
-          receiver: ethers.constants.AddressZero,
-          minAmount: SEND_AMOUNT,
-          destinationChainId: 42161,
-          hasSourceSwaps: false,
-          hasDestinationCall: false,
+          ...validBridgeData,
+          receiver: ZERO_ADDRESS,
+          hasSourceSwaps: true,
         }
 
         await expect(
@@ -350,7 +272,7 @@ describe('ArbitrumBridgeFacet', function () {
             .swapAndStartBridgeTokensViaArbitrumBridge(
               bridgeData,
               swapData,
-              arbitrumData
+              validArbitrumData
             )
         ).to.be.revertedWith('InvalidReceiver()')
       })
@@ -360,16 +282,8 @@ describe('ArbitrumBridgeFacet', function () {
         await usdc.transfer(dai.address, usdcBalance)
 
         const bridgeData = {
-          transactionId: utils.randomBytes(32),
-          bridge: 'arbitrum',
-          integrator: 'ACME Devs',
-          referrer: ethers.constants.AddressZero,
-          sendingAssetId: DAI_L1_ADDRESS,
-          receiver: alice.address,
-          minAmount: SEND_AMOUNT,
-          destinationChainId: 42161,
-          hasSourceSwaps: false,
-          hasDestinationCall: false,
+          ...validBridgeData,
+          hasSourceSwaps: true,
         }
 
         await expect(
@@ -387,16 +301,8 @@ describe('ArbitrumBridgeFacet', function () {
         await dexMgr.removeDex(UNISWAP_ADDRESS)
 
         const bridgeData = {
-          transactionId: utils.randomBytes(32),
-          bridge: 'arbitrum',
-          integrator: 'ACME Devs',
-          referrer: ethers.constants.AddressZero,
-          sendingAssetId: DAI_L1_ADDRESS,
-          receiver: alice.address,
-          minAmount: SEND_AMOUNT,
-          destinationChainId: 42161,
-          hasSourceSwaps: false,
-          hasDestinationCall: false,
+          ...validBridgeData,
+          hasSourceSwaps: true,
         }
 
         await expect(
@@ -415,16 +321,8 @@ describe('ArbitrumBridgeFacet', function () {
       const cost = MAX_SUBMISSION_COST.add(MAX_GAS_PRICE.mul(MAX_GAS))
 
       const bridgeData = {
-        transactionId: utils.randomBytes(32),
-        bridge: 'arbitrum',
-        integrator: 'ACME Devs',
-        referrer: ethers.constants.AddressZero,
-        sendingAssetId: DAI_L1_ADDRESS,
-        receiver: alice.address,
-        minAmount: SEND_AMOUNT,
-        destinationChainId: 42161,
-        hasSourceSwaps: false,
-        hasDestinationCall: false,
+        ...validBridgeData,
+        hasSourceSwaps: true,
       }
 
       await expect(
