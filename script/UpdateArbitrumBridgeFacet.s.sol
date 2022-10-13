3b386ee3f653fe1bcac54653b9b149d8027d52f9
Ed Zynda <ezynda3@gmail.com>
2022-10-13 12:29:08 +0300

    add update scripts for facets without init

---
+++ script/UpdateArbitrumBridgeFacet.s.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import { UpdateScriptBase } from "./utils/UpdateScriptBase.sol";
+import { stdJson } from "forge-std/StdJson.sol";
+import "forge-std/console.sol";
+import { DiamondCutFacet, IDiamondCut } from "lifi/Facets/DiamondCutFacet.sol";
+import { ArbitrumBridgeFacet } from "lifi/Facets/ArbitrumBridgeFacet.sol";
+
+contract DeployScript is UpdateScriptBase {
+    using stdJson for string;
+
+    function run() public returns (address[] memory facets) {
+        string memory root = vm.projectRoot();
+        string memory path = string.concat(root, "/deployments/", network, ".json");
+        string memory json = vm.readFile(path);
+        address facet = json.readAddress(".ArbitrumBridgeFacet");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        // ArbitrumBridge
+        if (loupe.facetFunctionSelectors(facet).length == 0) {
+            bytes4[] memory exclude;
+            cut.push(
+                IDiamondCut.FacetCut({
+                    facetAddress: address(facet),
+                    action: IDiamondCut.FacetCutAction.Add,
+                    functionSelectors: getSelectors("ArbitrumBridgeFacet", exclude)
+                })
+            );
+            cutter.diamondCut(cut, address(0), "");
+        }
+
+        facets = loupe.facetAddresses();
+
+        vm.stopBroadcast();
+    }
+}
---
+++ script/UpdatePeripheryRegistryFacet.s.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import { UpdateScriptBase } from "./utils/UpdateScriptBase.sol";
+import { stdJson } from "forge-std/StdJson.sol";
+import "forge-std/console.sol";
+import { DiamondCutFacet, IDiamondCut } from "lifi/Facets/DiamondCutFacet.sol";
+import { PeripheryRegistryFacet } from "lifi/Facets/PeripheryRegistryFacet.sol";
+
+contract DeployScript is UpdateScriptBase {
+    using stdJson for string;
+
+    function run() public returns (address[] memory facets) {
+        string memory root = vm.projectRoot();
+        string memory path = string.concat(root, "/deployments/", network, ".json");
+        string memory json = vm.readFile(path);
+        address facet = json.readAddress(".PeripheryRegistryFacet");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        // PeripheryRegistry
+        if (loupe.facetFunctionSelectors(facet).length == 0) {
+            bytes4[] memory exclude;
+            cut.push(
+                IDiamondCut.FacetCut({
+                    facetAddress: address(facet),
+                    action: IDiamondCut.FacetCutAction.Add,
+                    functionSelectors: getSelectors("PeripheryRegistryFacet", exclude)
+                })
+            );
+            cutter.diamondCut(cut, address(0), "");
+        }
+
+        facets = loupe.facetAddresses();
+
+        vm.stopBroadcast();
+    }
+}
