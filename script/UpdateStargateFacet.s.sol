4234693bfe8e2972c285edf000920190669f085b
Ed Zynda <ezynda3@gmail.com>
2022-10-13 16:38:29 +0300

    create update script for Stargate

---
+++ script/UpdateStargateFacet.s.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import { UpdateScriptBase } from "./utils/UpdateScriptBase.sol";
+import { stdJson } from "forge-std/StdJson.sol";
+import "forge-std/console.sol";
+import { DiamondCutFacet, IDiamondCut } from "lifi/Facets/DiamondCutFacet.sol";
+import { StargateFacet } from "lifi/Facets/StargateFacet.sol";
+
+contract DeployScript is UpdateScriptBase {
+    using stdJson for string;
+
+    struct ConfigFragment {
+        uint256 chainId;
+        uint256 lzChainId;
+        string[] pools;
+        address sgRouter;
+    }
+
+    struct Config {
+        mapping(string => Config) cfg;
+    }
+
+    struct PoolIdConfig {
+        address token;
+        uint16 poolId;
+    }
+
+    struct ChainIdConfig {
+        uint256 chainId;
+        uint16 layerZeroChainId;
+    }
+
+    function run() public returns (address[] memory facets) {
+        string memory root = vm.projectRoot();
+        string memory path = string.concat(root, "/deployments/", network, ".json");
+        string memory json = vm.readFile(path);
+        address facet = json.readAddress(".StargateFacet");
+
+        path = string.concat(root, "/config/stargate.json");
+        json = vm.readFile(path);
+        bytes memory rawConfig = json.parseRaw(string.concat(".config"));
+        Config storage config = abi.decode(rawConfig, (Config));
+
+        // bytes memory callData = abi.encodeWithSelector(StargateFacet.initStargate.selector, bridges);
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Stargate
+        if (loupe.facetFunctionSelectors(facet).length == 0) {
+            bytes4[] memory exclude = new bytes4[](1);
+            exclude[0] = StargateFacet.initStargate.selector;
+            cut.push(
+                IDiamondCut.FacetCut({
+                    facetAddress: address(facet),
+                    action: IDiamondCut.FacetCutAction.Add,
+                    functionSelectors: getSelectors("StargateFacet", exclude)
+                })
+            );
+            cutter.diamondCut(cut, address(0), "");
+        }
+
+        facets = loupe.facetAddresses();
+
+        vm.stopBroadcast();
+    }
+}
