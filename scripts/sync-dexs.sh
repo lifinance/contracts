4d44720bb3c2b519512852ff40dfb0eada54bb2a
Ed Zynda <ezynda3@gmail.com>
2022-10-26 17:20:36 +0300

    Add sync-dexs utility

---
+++ scripts/sync-dexs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+source .env
+
+NETWORK=$(cat ./networks | gum filter --placeholder "Network")
+
+DIAMOND=$(jq -r '.LiFiDiamond' "./deployments/${NETWORK}.json")
+CFG_DEXS=($(jq --arg n "$NETWORK" -r '.[$n] | @sh' "./config/dexs.json" | tr -d \' | tr '[:upper:]' '[:lower:]'))
+
+RPC="ETH_NODE_URI_$(tr '[:lower:]' '[:upper:]' <<< "$NETWORK")"
+
+RESULT=$(cast call "$DIAMOND" "approvedDexs() returns (address[])" --rpc-url "${!RPC}")
+DEXS=($(echo ${RESULT:1:-1} | tr ',' '\n' | tr '[:upper:]' '[:lower:]'))
+
+NEW_DEXS=()
+for dex in "${CFG_DEXS[@]}"; do
+  if [[ ! " ${DEXS[*]} " =~ " ${dex} " ]]; then
+    NEW_DEXS+=($dex)
+  fi
+done
+
+if [[ ! ${#NEW_DEXS[@]} -eq 0 ]]; then
+  echo 'Adding missing DEXs'
+  for d in "${NEW_DEXS[@]}"; do
+    PARAMS+="${d},"
+  done
+  cast send $DIAMOND "batchAddDex(address[])" "[${PARAMS::-1}]" --rpc-url ${!RPC} --private-key ${PRIVATE_KEY} 
+else
+  echo 'No new DEXs to add'
+fi
