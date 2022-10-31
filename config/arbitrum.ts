8427a2cd957c882c77be1712a6c676a072838f87
Satoshi Naoki <satoshi@li.finance>
2022-10-31 22:13:14 +1000

    add arbitrum bridge facet demo test

--- config/arbitrum.ts
+++ config/arbitrum.ts
@@ -5,55 +5,18 @@ interface ArbitrumBridgeConfig {
   }
 }
 
-export const DEFAULT_SUBMISSION_PRICE_PERCENT_INCREASE = 340
-// Temporary workaround for incorrect gas estimation from NodeInterface when there is gas refund
-export const DEFAULT_MAX_GAS_PERCENT_INCREASE = 50
-
-export const ARB_RETRYABLE_TX_ADDRESS =
-  '0x000000000000000000000000000000000000006E'
-export const NODE_INTERFACE_ADDRESS =
-  '0x00000000000000000000000000000000000000C8'
-
-export const GET_SUBMISSION_PRICE_ABI = [
-  `
-  function getSubmissionPrice(uint256 calldataSize)
-    external
-    view
-    returns (uint256, uint256)
-  `,
-]
-export const ESTIMATE_RETRYABLE_TICKET_ABI = [
-  `
-  function estimateRetryableTicket(
-    address sender,
-    uint256 deposit,
-    address destAddr,
-    uint256 l2CallValue,
-    uint256 maxSubmissionCost,
-    address excessFeeRefundAddress,
-    address callValueRefundAddress,
-    uint256 maxGas,
-    uint256 gasPriceBid,
-    bytes data
-  )
-    external
-    view
-    returns (uint256, uint256)
-  `,
-]
-
 const config: ArbitrumBridgeConfig = {
   hardhat: {
-    gatewayRouter: '0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef',
-    inbox: '0x4Dbd4fc535Ac27206064B68FfCf827b0A60BAB3f',
+    gatewayRouter: '0x4c7708168395aEa569453Fc36862D2ffcDaC588c',
+    inbox: '0x6BEbC4925716945D46F0Ec336D5C2564F419682C',
   },
   mainnet: {
     gatewayRouter: '0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef',
     inbox: '0x4Dbd4fc535Ac27206064B68FfCf827b0A60BAB3f',
   },
-  rinkeby: {
-    gatewayRouter: '0x70C143928eCfFaf9F5b406f7f4fC28Dc43d68380',
-    inbox: '0x578BAde599406A8fE3d24Fd7f7211c0911F5B29e',
+  goerli: {
+    gatewayRouter: '0x4c7708168395aEa569453Fc36862D2ffcDaC588c',
+    inbox: '0x6BEbC4925716945D46F0Ec336D5C2564F419682C',
   },
 }
 
---
+++ scripts/demoArbitrumBridge.ts
@@ -0,0 +1,260 @@
+import {
+  providers,
+  Wallet,
+  utils,
+  constants,
+  Contract,
+  BigNumber,
+} from 'ethers'
+import { L1ToL2MessageGasEstimator } from '@arbitrum/sdk/dist/lib/message/L1ToL2MessageGasEstimator'
+import { RetryableDataTools } from '@arbitrum/sdk'
+import { ArbitrumBridgeFacet__factory, ERC20__factory } from '../typechain'
+import { node_url } from '../utils/network'
+import chalk from 'chalk'
+
+const msg = (msg: string) => {
+  console.log(chalk.green(msg))
+}
+
+// Test process
+
+// Bridge Non-Native Asset
+// Approve TEST for LiFiDiamond for swapping
+// Swap TEST -> USDC via uniswap on Goerli
+// Bridge USDC on Goerli -> USDC on Arbitrum Goerli via Arbitrum Native Bridge
+
+// Bridge Native Asset
+// Bridge ETH on Goerli -> ETH on Arbitrum Goerli via Arbitrum Native Bridge
+
+const LIFI_ADDRESS = '0x1231DEB6f5749EF6cE6943a275A1D3E7486F4EaE' // LiFiDiamond address on Goerli
+const USDC_ADDRESS = '0x98339D8C260052B7ad81c28c16C0b98420f2B46a' // USDC address on Goerli
+const TEST_TOKEN_ADDRESS = '0x7ea6eA49B0b0Ae9c5db7907d139D9Cd3439862a1' // TEST Token address on Goerli
+const UNISWAP_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D' // Uniswap router address on Goerli
+const destinationChainId = 421613 // Arbitrum Goerli chain id
+const amountIn = utils.parseEther('1050')
+const amountOut = utils.parseUnits('1000', 6)
+
+async function main() {
+  const l1JsonProvider = new providers.JsonRpcProvider(node_url('goerli'))
+  const l1Provider = new providers.FallbackProvider([l1JsonProvider])
+  const l2JsonProvider = new providers.JsonRpcProvider(
+    node_url('arbitrum_goerli')
+  )
+  const l2Provider = new providers.FallbackProvider([l2JsonProvider])
+
+  const gasEstimator = new L1ToL2MessageGasEstimator(l2Provider)
+
+  let wallet = Wallet.fromMnemonic(<string>process.env.MNEMONIC)
+  wallet = wallet.connect(l1Provider)
+  const walletAddress = await wallet.getAddress()
+
+  const lifi = ArbitrumBridgeFacet__factory.connect(LIFI_ADDRESS, wallet)
+
+  // Bridge Non-Native Asset
+  {
+    const token = ERC20__factory.connect(TEST_TOKEN_ADDRESS, wallet)
+
+    // Setting Swap Data
+    const uniswap = new Contract(UNISWAP_ADDRESS, [
+      'function swapTokensForExactTokens(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts)',
+    ])
+
+    const path = [TEST_TOKEN_ADDRESS, USDC_ADDRESS]
+    const to = LIFI_ADDRESS // should be a checksummed recipient address
+    const deadline = Math.floor(Date.now() / 1000) + 60 * 20 // 20 minutes from the current Unix time
+
+    const swapData = await uniswap.populateTransaction.swapTokensForExactTokens(
+      amountOut,
+      amountIn,
+      path,
+      to,
+      deadline
+    )
+
+    // LIFI Data
+    const bridgeData = {
+      transactionId: utils.randomBytes(32),
+      bridge: 'arbitrum',
+      integrator: 'ACME Devs',
+      referrer: constants.AddressZero,
+      sendingAssetId: USDC_ADDRESS,
+      receiver: walletAddress,
+      minAmount: amountOut,
+      destinationChainId: destinationChainId,
+      hasSourceSwaps: true,
+      hasDestinationCall: false,
+    }
+
+    const depositFunc = (_: any) => {
+      return {
+        data: ArbitrumBridgeFacet__factory.createInterface().encodeFunctionData(
+          'swapAndStartBridgeTokensViaArbitrumBridge',
+          [
+            bridgeData,
+            [
+              {
+                callTo: <string>swapData.to,
+                approveTo: <string>swapData.to,
+                sendingAssetId: TEST_TOKEN_ADDRESS,
+                receivingAssetId: USDC_ADDRESS,
+                fromAmount: amountIn,
+                callData: <string>swapData?.data,
+                requiresDeposit: true,
+              },
+            ],
+            {
+              maxSubmissionCost: 1,
+              maxGas: RetryableDataTools.ErrorTriggeringParams.gasLimit,
+              maxGasPrice:
+                RetryableDataTools.ErrorTriggeringParams.maxFeePerGas,
+            },
+          ]
+        ),
+        to: lifi.address,
+        from: walletAddress,
+        value: BigNumber.from(1).add(
+          RetryableDataTools.ErrorTriggeringParams.gasLimit.mul(
+            RetryableDataTools.ErrorTriggeringParams.maxFeePerGas
+          )
+        ),
+      }
+    }
+
+    // Approve ERC20 for swapping -- USDC -> USDC
+    const allowance = await token.allowance(walletAddress, LIFI_ADDRESS)
+    if (amountIn.gt(allowance)) {
+      await token.approve(LIFI_ADDRESS, amountIn)
+
+      msg('Token approved for swapping')
+    }
+
+    const estimates = await gasEstimator.populateFunctionParams(
+      depositFunc,
+      l1Provider
+    )
+
+    const { maxSubmissionCost, gasLimit, maxFeePerGas } = estimates.estimates
+
+    // Total cost
+    const cost = maxSubmissionCost.add(maxFeePerGas.mul(gasLimit))
+    console.log(cost.toString())
+
+    // Bridge Data
+    const arbitrumData = {
+      maxSubmissionCost: maxSubmissionCost,
+      maxGas: gasLimit,
+      maxGasPrice: maxFeePerGas,
+    }
+
+    // Call LiFi smart contract to start the bridge process -- WITH SWAP
+    await lifi.swapAndStartBridgeTokensViaArbitrumBridge(
+      bridgeData,
+      [
+        {
+          callTo: <string>swapData.to,
+          approveTo: <string>swapData.to,
+          sendingAssetId: TEST_TOKEN_ADDRESS,
+          receivingAssetId: USDC_ADDRESS,
+          fromAmount: amountIn,
+          callData: <string>swapData?.data,
+          requiresDeposit: true,
+        },
+      ],
+      arbitrumData,
+      {
+        gasLimit: '500000',
+        value: cost,
+      }
+    )
+  }
+
+  // Bridge Native Asset
+  {
+    // Bridge amount
+    const amount = utils.parseEther('0.0001')
+
+    // LIFI Data
+    const bridgeData = {
+      transactionId: utils.randomBytes(32),
+      bridge: 'arbitrum',
+      integrator: 'ACME Devs',
+      referrer: constants.AddressZero,
+      sendingAssetId: constants.AddressZero,
+      receiver: walletAddress,
+      minAmount: amount,
+      destinationChainId: destinationChainId,
+      hasSourceSwaps: false,
+      hasDestinationCall: false,
+    }
+
+    const depositFunc = (_: any) => {
+      return {
+        data: ArbitrumBridgeFacet__factory.createInterface().encodeFunctionData(
+          'startBridgeTokensViaArbitrumBridge',
+          [
+            bridgeData,
+            {
+              maxSubmissionCost: 1,
+              maxGas: RetryableDataTools.ErrorTriggeringParams.gasLimit,
+              maxGasPrice:
+                RetryableDataTools.ErrorTriggeringParams.maxFeePerGas,
+            },
+          ]
+        ),
+        to: lifi.address,
+        from: walletAddress,
+        value: amount
+          .add(1)
+          .add(
+            RetryableDataTools.ErrorTriggeringParams.gasLimit.mul(
+              RetryableDataTools.ErrorTriggeringParams.maxFeePerGas
+            )
+          ),
+      }
+    }
+
+    const estimates = await gasEstimator.populateFunctionParams(
+      depositFunc,
+      l1Provider
+    )
+
+    const {
+      maxSubmissionCost,
+      gasLimit: gasLimit,
+      maxFeePerGas: maxGasPrice,
+    } = estimates.estimates
+
+    // Bridge Data
+    const arbitrumData = {
+      maxSubmissionCost: maxSubmissionCost,
+      maxGas: gasLimit,
+      maxGasPrice: maxGasPrice,
+    }
+
+    console.log(
+      maxSubmissionCost.toString(),
+      gasLimit.toString(),
+      maxGasPrice.toString()
+    )
+
+    // Total cost
+    const cost = maxSubmissionCost.add(maxGasPrice.mul(gasLimit))
+
+    // Call LiFi smart contract to start the bridge process -- WITH SWAP
+    await lifi.startBridgeTokensViaArbitrumBridge(bridgeData, arbitrumData, {
+      gasLimit: '500000',
+      value: amount.add(cost),
+    })
+  }
+}
+
+main()
+  .then(() => {
+    console.error('Success')
+    process.exit(0)
+  })
+  .catch((error) => {
+    console.error('error')
+    console.error(error)
+    process.exit(1)
+  })
