52baabd30a8cdae9c1e2ec382f8b04cf37de80b6
Daniel Bläcker <daniel@li.finance>
2022-11-14 12:46:29 +0100

    wip

---
+++ test/solidity/utils/Utilities2.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.0;
+
+import { DSTest } from "ds-test/test.sol";
+import { Vm } from "forge-std/Vm.sol";
+
+//common utilities for forge tests
+contract TestBase is DSTest {
+    Vm internal immutable vm = Vm(HEVM_ADDRESS);
+    bytes32 internal nextUser = keccak256(abi.encodePacked("user address"));
+    address constant deployer = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D; //TODO: change
+
+    // ETH addresses
+    address internal constant UNISWAP_V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+    address internal constant USDC_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address internal constant DAI_ADDRESS = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address internal constant WETH_ADDRESS = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address internal constant USDC_WHALE = 0x72A53cDBBcc1b9efa39c834A540550e23463AAcB;
+    address internal constant DAI_WHALE = 0x5D38B4e4783E34e2301A2a36c39a03c45798C4dD;
+
+    modifier executeAsDeployer() {
+        vm.prank(deployer);
+        _;
+    }
+
+    function getNextUserAddress() external returns (address payable) {
+        //bytes32 to address conversion
+        address payable user = payable(address(uint160(uint256(nextUser))));
+        nextUser = keccak256(abi.encodePacked(nextUser));
+        return user;
+    }
+
+    //#region existing
+    function getNextUserAddress() external returns (address payable) {
+        //bytes32 to address conversion
+        address payable user = payable(address(uint160(uint256(nextUser))));
+        nextUser = keccak256(abi.encodePacked(nextUser));
+        return user;
+    }
+
+    //create users with 100 ether balance
+    function createUsers(uint256 userNum) external returns (address payable[] memory) {
+        address payable[] memory users = new address payable[](userNum);
+        for (uint256 i = 0; i < userNum; i++) {
+            address payable user = this.getNextUserAddress();
+            vm.deal(user, 100 ether);
+            users[i] = user;
+        }
+        return users;
+    }
+
+    //move block.number forward by a given number of blocks
+    function mineBlocks(uint256 numBlocks) external {
+        uint256 targetBlock = block.number + numBlocks;
+        vm.roll(targetBlock);
+    }
+    //#endregion
+}
