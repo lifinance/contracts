5a9a02f8c2aa4364592184eab5b08b5583a232ac
Ed Zynda <ezynda3@gmail.com>
2022-11-18 15:15:33 +0300

    deploy receiver fixes

--- scripts/update-periphery.sh
+++ scripts/update-periphery.sh
@@ -5,6 +5,7 @@ source .env
 load() {
 
 NETWORK=$(cat ./networks | gum filter --placeholder "Network")
+CONTRACTS=$(gum choose --no-limit erc20Proxy axelarExecutor executor receiver feeCollector)
 
 ADDRS="deployments/$NETWORK.json"
 
@@ -17,27 +18,27 @@ FEECOLLECTOR=$(jq -r '.FeeCollector // "0x"' $ADDRS)
 
 echo "Diamond: $DIAMOND"
 
-if [ "$ERC20PROXY" != "0x" ]; then
+if [[ "$ERC20PROXY" != "0x"  && " ${CONTRACTS[*]} " =~ "erc20Proxy" ]]; then
   echo "Updating ERC20Proxy $ERC20PROXY"
   register $NETWORK $DIAMOND 'ERC20Proxy' $ERC20PROXY
 fi
 
-if [ "$AXELAREXECUTOR" != "0x" ]; then
+if [[ "$AXELAREXECUTOR" != "0x" && " ${CONTRACTS[*]}" =~ "axelarExecutor" ]]; then
   echo "Updating AxelarExecutor $AXELAREXECUTOR"
   register $NETWORK $DIAMOND 'AxelarExecutor' $AXELAREXECUTOR
 fi
 
-if [ "$EXECUTOR" != "0x" ]; then
+if [[ "$EXECUTOR" != "0x" && " ${CONTRACTS[*]}" =~ "executor" ]]; then
   echo "Updating Executor $EXECUTOR"
   register $NETWORK $DIAMOND 'Executor' $EXECUTOR
 fi
 
-if [ "$RECEIVER" != "0x" ]; then
+if [[ "$RECEIVER" != "0x" && " ${CONTRACTS[*]}" =~ "receiver" ]]; then
   echo "Updating Receiver $RECEIVER"
   register $NETWORK $DIAMOND 'Receiver' $RECEIVER
 fi
 
-if [ "$FEECOLLECTOR" != "0x" ]; then
+if [[ "$FEECOLLECTOR" != "0x" && " ${CONTRACTS[*]}" =~ "feeCollector" ]]; then
   echo "Updating FeeCollector $FEECOLLECTOR"
   register $NETWORK $DIAMOND 'FeeCollector' $FEECOLLECTOR
 fi
