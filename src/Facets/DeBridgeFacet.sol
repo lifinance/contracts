0a7b2cdf65dd614fb5a2a135077dd6ff8f2c45ef
Satoshi Naoki <satoshi@li.finance>
2022-11-23 21:35:39 +1000

    DeBridge facet contract

---
+++ src/Facets/DeBridgeFacet.sol
@@ -0,0 +1,132 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ILiFi } from "../Interfaces/ILiFi.sol";
+import { IDeBridgeGate } from "../Interfaces/IDeBridgeGate.sol";
+import { LibAsset } from "../Libraries/LibAsset.sol";
+import { LibSwap } from "../Libraries/LibSwap.sol";
+import { ReentrancyGuard } from "../Helpers/ReentrancyGuard.sol";
+import { SwapperV2 } from "../Helpers/SwapperV2.sol";
+import { Validatable } from "../Helpers/Validatable.sol";
+import { LibDiamond } from "../Libraries/LibDiamond.sol";
+
+/// @title DeBridge Facet
+/// @author LI.FI (https://li.fi)
+/// @notice Provides functionality for bridging through DeBridge Protocol
+contract DeBridgeFacet is ILiFi, ReentrancyGuard, SwapperV2, Validatable {
+    /// Storage ///
+
+    bytes32 internal constant NAMESPACE = keccak256("com.lifi.facets.debridge");
+
+    /// @notice The contract address of the spoke pool on the source chain.
+    IDeBridgeGate private immutable deBridgeGate;
+
+    /// Types ///
+
+    /// @param permit deadline + signature for approving the spender by signature.
+    /// @param useAssetFee use assets fee for pay protocol fix (work only for specials token)
+    /// @param referralCode Referral code
+    /// @param autoParams Auto params for external call in target network
+    //chainFees.fixedNativeFee == 0 ? globalFixedNativeFee : chainFees.fixedNativeFee;
+    struct DeBridgeData {
+        bytes permit;
+        bool useAssetFee;
+        uint256 nativeFee;
+        uint32 referralCode;
+        IDeBridgeGate.SubmissionAutoParamsTo autoParams;
+    }
+
+    /// Constructor ///
+
+    /// @notice Initialize the contract.
+    /// @param _deBridgeGate The contract address of the DeBridgeGate on the source chain.
+    constructor(IDeBridgeGate _deBridgeGate) {
+        deBridgeGate = _deBridgeGate;
+    }
+
+    /// External Methods ///
+
+    /// @notice Bridges tokens via DeBridge
+    /// @param _bridgeData the core information needed for bridging
+    /// @param _deBridgeData data specific to DeBridge
+    function startBridgeTokensViaDeBridge(ILiFi.BridgeData calldata _bridgeData, DeBridgeData calldata _deBridgeData)
+        external
+        payable
+        refundExcessNative(payable(msg.sender))
+        validateBridgeData(_bridgeData)
+        doesNotContainSourceSwaps(_bridgeData)
+        doesNotContainDestinationCalls(_bridgeData)
+        nonReentrant
+    {
+        LibAsset.depositAsset(_bridgeData.sendingAssetId, _bridgeData.minAmount);
+        _startBridge(_bridgeData, _deBridgeData);
+    }
+
+    /// @notice Performs a swap before bridging via DeBridge
+    /// @param _bridgeData the core information needed for bridging
+    /// @param _swapData an array of swap related data for performing swaps before bridging
+    /// @param _deBridgeData data specific to DeBridge
+    function swapAndStartBridgeTokensViaDeBridge(
+        ILiFi.BridgeData memory _bridgeData,
+        LibSwap.SwapData[] calldata _swapData,
+        DeBridgeData memory _deBridgeData
+    )
+        external
+        payable
+        nonReentrant
+        refundExcessNative(payable(msg.sender))
+        containsSourceSwaps(_bridgeData)
+        doesNotContainDestinationCalls(_bridgeData)
+        validateBridgeData(_bridgeData)
+    {
+        _bridgeData.minAmount = _depositAndSwap(
+            _bridgeData.transactionId,
+            _bridgeData.minAmount,
+            _swapData,
+            payable(msg.sender),
+            _deBridgeData.nativeFee
+        );
+
+        if (LibAsset.isNativeAsset(_bridgeData.sendingAssetId)) {
+            _bridgeData.minAmount -= _deBridgeData.nativeFee;
+        }
+
+        _startBridge(_bridgeData, _deBridgeData);
+    }
+
+    /// Internal Methods ///
+
+    /// @dev Contains the business logic for the bridge via DeBridge
+    /// @param _bridgeData the core information needed for bridging
+    /// @param _deBridgeData data specific to DeBridge
+    function _startBridge(ILiFi.BridgeData memory _bridgeData, DeBridgeData memory _deBridgeData) internal {
+        bool isNative = LibAsset.isNativeAsset(_bridgeData.sendingAssetId);
+        uint256 nativeAssetAmount = _deBridgeData.nativeFee;
+
+        if (isNative) {
+            nativeAssetAmount += _bridgeData.minAmount;
+        } else {
+            LibAsset.maxApproveERC20(IERC20(_bridgeData.sendingAssetId), address(deBridgeGate), _bridgeData.minAmount);
+        }
+
+        deBridgeGate.send{ value: nativeAssetAmount }(
+            _bridgeData.sendingAssetId,
+            _bridgeData.minAmount,
+            _bridgeData.destinationChainId,
+            abi.encodePacked(_bridgeData.receiver),
+            _deBridgeData.permit,
+            _deBridgeData.useAssetFee,
+            _deBridgeData.referralCode,
+            abi.encode(
+                _deBridgeData.autoParams.executionFee,
+                _deBridgeData.autoParams.flags,
+                abi.encodePacked(_deBridgeData.autoParams.fallbackAddress),
+                _deBridgeData.autoParams.data
+            )
+        );
+
+        emit LiFiTransferStarted(_bridgeData);
+    }
+}
---
+++ src/Interfaces/IDeBridgeGate.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+interface IDeBridgeGate {
+    /// @param executionFee Fee paid to the transaction executor.
+    /// @param flags Flags set specific flows for call data execution.
+    /// @param fallbackAddress Receiver of the tokens if the call fails.
+    /// @param data Message/Call data to be passed to the receiver
+    ///             on the destination chain during the external call execution.
+    struct SubmissionAutoParamsTo {
+        uint256 executionFee;
+        uint256 flags;
+        address fallbackAddress;
+        bytes data;
+    }
+
+    /// @param fixedNativeFee Transfer fixed fee.
+    /// @param isSupported Whether the chain for the asset is supported.
+    /// @param transferFeeBps Transfer fee rate nominated in basis points (1/10000)
+    ///                       of transferred amount.
+    struct ChainSupportInfo {
+        uint256 fixedNativeFee;
+        bool isSupported;
+        uint16 transferFeeBps;
+    }
+
+    /// @dev Fallback fixed fee in native asset, used if a chain fixed fee is set to 0
+    function globalFixedNativeFee() external returns (uint256);
+
+    /// @dev Whether the chain for the asset is supported to send
+    function getChainToConfig(uint256) external returns (ChainSupportInfo memory);
+
+    /// @dev This method is used for the transfer of assets.
+    ///      It locks an asset in the smart contract in the native chain
+    ///      and enables minting of deAsset on the secondary chain.
+    /// @param _tokenAddress Asset identifier.
+    /// @param _amount Amount to be transferred (note: the fee can be applied).
+    /// @param _chainIdTo Chain id of the target chain.
+    /// @param _receiver Receiver address.
+    /// @param _permit deadline + signature for approving the spender by signature.
+    /// @param _useAssetFee use assets fee for pay protocol fix (work only for specials token)
+    /// @param _referralCode Referral code
+    /// @param _autoParams Auto params for external call in target network
+    function send(
+        address _tokenAddress,
+        uint256 _amount,
+        uint256 _chainIdTo,
+        bytes memory _receiver,
+        bytes memory _permit,
+        bool _useAssetFee,
+        uint32 _referralCode,
+        bytes calldata _autoParams
+    ) external payable;
+}
