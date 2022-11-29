19afdaf8e3d05abc4935729822b6fc0dac70d381
Daniel Bläcker <daniel@li.finance>
2022-11-29 15:21:39 +0100

    polygon bridge facet tests updated

--- test/solidity/Facets/HyphenFacet.t.sol
+++ test/solidity/Facets/HyphenFacet.t.sol
@@ -21,12 +21,7 @@ contract TestHyphenFacet is HyphenFacet {
 
 contract HyphenFacetTest is TestBase {
     // These values are for Polygon
-    address internal constant USDC_ADDRESS = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
-    address internal constant USDC_HOLDER = 0xD6216fC19DB775Df9774a6E33526131dA7D19a2c;
-    address internal constant WMATIC_ADDRESS = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
-    address internal constant WMATIC_HOLDER = 0x01aeFAC4A308FbAeD977648361fBAecFBCd380C7;
     address internal constant HYPHEN_ROUTER = 0x2A5c2568b10A0E826BfA892Cf21BA7218310180b;
-    address internal constant UNISWAP_V2_ROUTER = 0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff;
     // -----
 
     TestHyphenFacet internal hyphenFacet;
