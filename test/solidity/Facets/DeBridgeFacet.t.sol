3a70f403c4a458db31a518c664845dd06f0a429a
Satoshi Naoki <satoshi@li.finance>
2022-12-13 22:35:58 +0900

    update DeBridge facet foundry test with base tests

--- test/solidity/Facets/DeBridgeFacet.t.sol
+++ test/solidity/Facets/DeBridgeFacet.t.sol
@@ -1,17 +1,9 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.17;
 
-import { DSTest } from "ds-test/test.sol";
-import { DiamondTest, LiFiDiamond } from "../utils/DiamondTest.sol";
-import { Vm } from "forge-std/Vm.sol";
+import { ILiFi, LibAllowList, LibSwap, TestBaseFacet, console } from "../utils/TestBaseFacet.sol";
 import { DeBridgeFacet } from "lifi/Facets/DeBridgeFacet.sol";
-import { ILiFi } from "lifi/Interfaces/ILiFi.sol";
-import { LibSwap } from "lifi/Libraries/LibSwap.sol";
-import { LibAllowList } from "lifi/Libraries/LibAllowList.sol";
-import { ERC20 } from "solmate/tokens/ERC20.sol";
-import { UniswapV2Router02 } from "../utils/Interfaces.sol";
 import { IDeBridgeGate } from "lifi/Interfaces/IDeBridgeGate.sol";
-import "lifi/Errors/GenericErrors.sol";
 
 // Stub DeBridgeFacet Contract
 contract TestDeBridgeFacet is DeBridgeFacet {
@@ -26,43 +18,46 @@ contract TestDeBridgeFacet is DeBridgeFacet {
     }
 }
 
-contract DeBridgeFacetTest is DSTest, DiamondTest {
+contract DeBridgeFacetTest is TestBaseFacet {
     // These values are for Mainnet
-    address internal constant USDC_ADDRESS = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    address internal constant USDC_HOLDER = 0xaD0135AF20fa82E106607257143d0060A7eB5cBf;
-    address internal constant DAI_ADDRESS = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address internal constant DAI_HOLDER = 0x4943b0C9959dcf58871A799dfB71becE0D97c9f4;
     address internal constant DEBRIDGE_GATE = 0x43dE2d77BF8027e25dBD179B491e8d64f38398aA;
-    address internal constant UNISWAP_V2_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-    uint256 internal constant DSTCHAIN_ID = 56;
+    uint256 internal constant DST_CHAIN_ID = 56;
     uint256 public constant REVERT_IF_EXTERNAL_FAIL = 1;
-    // -----
 
-    Vm internal immutable vm = Vm(HEVM_ADDRESS);
-    LiFiDiamond internal diamond;
     TestDeBridgeFacet internal deBridgeFacet;
-    UniswapV2Router02 internal uniswap;
-    ERC20 internal usdc;
-    ERC20 internal dai;
-    ILiFi.BridgeData internal validBridgeData;
-    DeBridgeFacet.DeBridgeData internal validDeBridgeData;
-    uint256 internal nativeFee;
-    uint256 internal executionFee;
+    DeBridgeFacet.DeBridgeData internal deBridgeData;
 
-    function fork() internal {
-        string memory rpcUrl = vm.envString("ETH_NODE_URI_MAINNET");
-        uint256 blockNumber = 15876510;
-        vm.createSelectFork(rpcUrl, blockNumber);
+    function initiateBridgeTxWithFacet(bool isNative) internal override {
+        if (isNative) {
+            deBridgeFacet.startBridgeTokensViaDeBridge{ value: bridgeData.minAmount + addToMessageValue }(
+                bridgeData,
+                deBridgeData
+            );
+        } else {
+            deBridgeFacet.startBridgeTokensViaDeBridge{ value: addToMessageValue }(bridgeData, deBridgeData);
+        }
+    }
+
+    function initiateSwapAndBridgeTxWithFacet(bool isNative) internal override {
+        if (isNative) {
+            deBridgeFacet.swapAndStartBridgeTokensViaDeBridge{ value: swapData[0].fromAmount + addToMessageValue }(
+                bridgeData,
+                swapData,
+                deBridgeData
+            );
+        } else {
+            deBridgeFacet.swapAndStartBridgeTokensViaDeBridge{ value: addToMessageValue }(
+                bridgeData,
+                swapData,
+                deBridgeData
+            );
+        }
     }
 
     function setUp() public {
-        fork();
+        initTestBase();
 
-        diamond = createDiamond();
         deBridgeFacet = new TestDeBridgeFacet(IDeBridgeGate(DEBRIDGE_GATE));
-        usdc = ERC20(USDC_ADDRESS);
-        dai = ERC20(DAI_ADDRESS);
-        uniswap = UniswapV2Router02(UNISWAP_V2_ROUTER);
 
         bytes4[] memory functionSelectors = new bytes4[](4);
         functionSelectors[0] = deBridgeFacet.startBridgeTokensViaDeBridge.selector;
@@ -76,28 +71,22 @@ contract DeBridgeFacetTest is DSTest, DiamondTest {
 
         deBridgeFacet.addDex(address(uniswap));
         deBridgeFacet.setFunctionApprovalBySignature(uniswap.swapExactTokensForTokens.selector);
+        deBridgeFacet.setFunctionApprovalBySignature(uniswap.swapExactTokensForETH.selector);
         deBridgeFacet.setFunctionApprovalBySignature(uniswap.swapETHForExactTokens.selector);
+        deBridgeFacet.setFunctionApprovalBySignature(uniswap.swapTokensForExactETH.selector);
 
-        validBridgeData = ILiFi.BridgeData({
-            transactionId: "",
-            bridge: "debridge",
-            integrator: "",
-            referrer: address(0),
-            sendingAssetId: USDC_ADDRESS,
-            receiver: USDC_HOLDER,
-            minAmount: 10 * 10**usdc.decimals(),
-            destinationChainId: DSTCHAIN_ID,
-            hasSourceSwaps: false,
-            hasDestinationCall: false
-        });
+        setFacetAddressInTestBase(address(deBridgeFacet), "DeBridgeFacet");
 
-        IDeBridgeGate.ChainSupportInfo memory chainConfig = IDeBridgeGate(DEBRIDGE_GATE).getChainToConfig(DSTCHAIN_ID);
-        nativeFee = chainConfig.fixedNativeFee == 0
+        bridgeData.bridge = "debridge";
+        bridgeData.minAmount = defaultUSDCAmount;
+
+        IDeBridgeGate.ChainSupportInfo memory chainConfig = IDeBridgeGate(DEBRIDGE_GATE).getChainToConfig(DST_CHAIN_ID);
+        uint256 nativeFee = addToMessageValue = chainConfig.fixedNativeFee == 0
             ? IDeBridgeGate(DEBRIDGE_GATE).globalFixedNativeFee()
             : chainConfig.fixedNativeFee;
-        executionFee = 10**usdc.decimals();
+        uint256 executionFee = 1 * 10**usdc.decimals();
 
-        validDeBridgeData = DeBridgeFacet.DeBridgeData(
+        deBridgeData = DeBridgeFacet.DeBridgeData(
             "",
             nativeFee,
             false,
@@ -105,138 +94,38 @@ contract DeBridgeFacetTest is DSTest, DiamondTest {
             DeBridgeFacet.SubmissionAutoParamsTo(
                 executionFee,
                 REVERT_IF_EXTERNAL_FAIL,
-                abi.encodePacked(DAI_HOLDER),
+                abi.encodePacked(USER_RECEIVER),
                 ""
             )
         );
     }
 
-    function testRevertToBridgeTokensWhenSendingAmountIsZero() public {
-        vm.startPrank(USDC_HOLDER);
-
-        usdc.approve(address(deBridgeFacet), 10_000 * 10**usdc.decimals());
-
-        ILiFi.BridgeData memory bridgeData = validBridgeData;
-        bridgeData.minAmount = 0;
-
-        vm.expectRevert(InvalidAmount.selector);
-        deBridgeFacet.startBridgeTokensViaDeBridge(bridgeData, validDeBridgeData);
-
-        vm.stopPrank();
+    function testBase_Revert_BridgeWithInvalidDestinationCallFlag() public override {
+        // facet supports destination call
     }
 
-    function testRevertToBridgeTokensWhenReceiverIsZeroAddress() public {
-        vm.startPrank(USDC_HOLDER);
+    function testBase_CanBridgeTokens_fuzzed(uint256 amount) public override {
+        vm.startPrank(USER_SENDER);
 
-        usdc.approve(address(deBridgeFacet), 10_000 * 10**usdc.decimals());
+        // amount should be greater than execution fee
+        vm.assume(amount > 1 && amount < 100_000);
+        amount = amount * 10**usdc.decimals();
 
-        ILiFi.BridgeData memory bridgeData = validBridgeData;
-        bridgeData.receiver = address(0);
+        logFilePath = "./test/logs/"; // works but is not really a proper file
+        // logFilePath = "./test/logs/fuzz_test.txt"; // throws error "failed to write to "....../test/logs/fuzz_test.txt": No such file or directory"
 
-        vm.expectRevert(InvalidReceiver.selector);
-        deBridgeFacet.startBridgeTokensViaDeBridge(bridgeData, validDeBridgeData);
+        vm.writeLine(logFilePath, vm.toString(amount));
+        // approval
+        usdc.approve(_facetTestContractAddress, amount);
 
-        vm.stopPrank();
-    }
+        bridgeData.sendingAssetId = ADDRESS_USDC;
+        bridgeData.minAmount = amount;
 
-    function testRevertToBridgeTokensWhenSenderHasNoEnoughAmount() public {
-        vm.startPrank(USDC_HOLDER);
+        //prepare check for events
+        vm.expectEmit(true, true, true, true, _facetTestContractAddress);
+        emit LiFiTransferStarted(bridgeData);
 
-        usdc.approve(address(deBridgeFacet), 10_000 * 10**usdc.decimals());
-
-        usdc.transfer(DAI_HOLDER, usdc.balanceOf(USDC_HOLDER));
-
-        vm.expectRevert(abi.encodeWithSelector(InsufficientBalance.selector, 10 * 10**usdc.decimals(), 0));
-        deBridgeFacet.startBridgeTokensViaDeBridge(validBridgeData, validDeBridgeData);
-
-        vm.stopPrank();
-    }
-
-    function testRevertToBridgeTokensWhenSendingNoEnoughNativeAsset() public {
-        vm.startPrank(USDC_HOLDER);
-
-        ILiFi.BridgeData memory bridgeData = validBridgeData;
-        bridgeData.sendingAssetId = address(0);
-        bridgeData.minAmount = 3e18;
-
-        vm.expectRevert(InvalidAmount.selector);
-        deBridgeFacet.startBridgeTokensViaDeBridge{ value: 2e18 }(bridgeData, validDeBridgeData);
-
-        vm.stopPrank();
-    }
-
-    function testRevertToBridgeTokensWhenInformationMismatch() public {
-        vm.startPrank(USDC_HOLDER);
-
-        usdc.approve(address(deBridgeFacet), 10_000 * 10**usdc.decimals());
-
-        ILiFi.BridgeData memory bridgeData = validBridgeData;
-        bridgeData.hasSourceSwaps = true;
-
-        vm.expectRevert(InformationMismatch.selector);
-        deBridgeFacet.startBridgeTokensViaDeBridge(bridgeData, validDeBridgeData);
-
-        vm.stopPrank();
-    }
-
-    function testCanBridgeERC20Tokens() public {
-        vm.startPrank(USDC_HOLDER);
-        usdc.approve(address(deBridgeFacet), 10_000 * 10**usdc.decimals());
-
-        deBridgeFacet.startBridgeTokensViaDeBridge{ value: nativeFee }(validBridgeData, validDeBridgeData);
-
-        vm.stopPrank();
-    }
-
-    function testCanBridgeNativeTokens() public {
-        vm.startPrank(USDC_HOLDER);
-
-        ILiFi.BridgeData memory bridgeData = validBridgeData;
-        bridgeData.sendingAssetId = address(0);
-        bridgeData.minAmount = 3e18;
-
-        deBridgeFacet.startBridgeTokensViaDeBridge{ value: 3e18 + nativeFee }(bridgeData, validDeBridgeData);
-        vm.stopPrank();
-    }
-
-    function testCanSwapAndBridgeTokens() public {
-        vm.startPrank(DAI_HOLDER);
-
-        dai.approve(address(deBridgeFacet), 10_000 * 10**dai.decimals());
-
-        // Swap USDC to DAI
-        address[] memory path = new address[](2);
-        path[0] = DAI_ADDRESS;
-        path[1] = USDC_ADDRESS;
-
-        uint256 amountOut = 1000 * 10**usdc.decimals();
-
-        // Calculate DAI amount
-        uint256[] memory amounts = uniswap.getAmountsIn(amountOut, path);
-        uint256 amountIn = amounts[0];
-        LibSwap.SwapData[] memory swapData = new LibSwap.SwapData[](1);
-        swapData[0] = LibSwap.SwapData(
-            address(uniswap),
-            address(uniswap),
-            DAI_ADDRESS,
-            USDC_ADDRESS,
-            amountIn,
-            abi.encodeWithSelector(
-                uniswap.swapExactTokensForTokens.selector,
-                amountIn,
-                amountOut,
-                path,
-                address(deBridgeFacet),
-                block.timestamp + 20 minutes
-            ),
-            true
-        );
-
-        ILiFi.BridgeData memory bridgeData = validBridgeData;
-        bridgeData.hasSourceSwaps = true;
-
-        deBridgeFacet.swapAndStartBridgeTokensViaDeBridge{ value: nativeFee }(bridgeData, swapData, validDeBridgeData);
-
+        initiateBridgeTxWithFacet(false);
         vm.stopPrank();
     }
 }
