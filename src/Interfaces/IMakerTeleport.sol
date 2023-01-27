f9b4fbfbd8affbc2228b05c00c11446323f71ed2
Satoshi Naoki <satoshi@li.finance>
2023-01-27 11:19:21 +0900

    add IMakerTeleport interface

---
+++ src/Interfaces/IMakerTeleport.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+interface IMakerTeleport {
+    /// @notice Initiate DAI transfer.
+    /// @param targetDomain Domain of destination chain.
+    /// @param receiver Receiver address.
+    /// @param amount The amount of DAI to transfer.
+    function initiateTeleport(
+        bytes32 targetDomain,
+        address receiver,
+        uint128 amount
+    ) external;
+}
