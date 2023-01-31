9248db67a13ecf87d31954633ffdaa197d464ade
Satoshi Naoki <satoshi@li.finance>
2023-01-31 10:32:46 +0900

    add maker teleport facet deploy and update scripts

---
+++ script/UpdateMakerTeleportFacet.s.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import { UpdateScriptBase } from "./utils/UpdateScriptBase.sol";
+import { stdJson } from "forge-std/StdJson.sol";
+import { DiamondCutFacet, IDiamondCut } from "lifi/Facets/DiamondCutFacet.sol";
+import { MakerTeleportFacet } from "lifi/Facets/MakerTeleportFacet.sol";
+
+contract DeployScript is UpdateScriptBase {
+    using stdJson for string;
+
+    function run() public returns (address[] memory facets) {
+        string memory path = string.concat(root, "/deployments/", network, ".", fileSuffix, "json");
+        string memory json = vm.readFile(path);
+        address facet = json.readAddress(".MakerTeleportFacet");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        // MakerTeleport
+        if (loupe.facetFunctionSelectors(facet).length == 0) {
+            bytes4[] memory exclude;
+            cut.push(
+                IDiamondCut.FacetCut({
+                    facetAddress: address(facet),
+                    action: IDiamondCut.FacetCutAction.Add,
+                    functionSelectors: getSelectors("MakerTeleportFacet", exclude)
+                })
+            );
+            cutter.diamondCut(cut, address(0), "");
+        }
+
+        facets = loupe.facetAddresses();
+
+        vm.stopBroadcast();
+    }
+}
