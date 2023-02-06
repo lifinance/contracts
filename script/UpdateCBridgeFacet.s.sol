4d5108f774361000f8cb84c8fb0409ade4dddc9a
Daniel Bläcker <daniel@li.finance>
2023-02-06 10:42:06 +0700

    re-added deployment scripts for CBridgeFacet

--- script/UpdateCBridgeFacet.s.sol
+++ script/UpdateCBridgeFacet.s.sol
@@ -4,40 +4,33 @@ pragma solidity ^0.8.17;
 import { UpdateScriptBase } from "./utils/UpdateScriptBase.sol";
 import { stdJson } from "forge-std/StdJson.sol";
 import { DiamondCutFacet, IDiamondCut } from "lifi/Facets/DiamondCutFacet.sol";
-import {CelerIMFacet} from "lifi/Facets/CelerIMFacet.sol";
+import { CBridgeFacet } from "lifi/Facets/CBridgeFacet.sol";
 
 contract DeployScript is UpdateScriptBase {
-    using stdJson for string;
+using stdJson for string;
 
-    function run() public returns (address[] memory facets) {
-        string memory path = string.concat(
-            root,
-            "/deployments/",
-            network,
-            ".",
-            fileSuffix,
-            "json"
+function run() public returns (address[] memory facets) {
+    string memory path = string.concat(root, "/deployments/", network, ".", fileSuffix, "json");
+    string memory json = vm.readFile(path);
+    address facet = json.readAddress(".CBridgeFacet");
+
+    vm.startBroadcast(deployerPrivateKey);
+
+    // CBridge
+    if (loupe.facetFunctionSelectors(facet).length == 0) {
+        bytes4[] memory exclude;
+        cut.push(
+        IDiamondCut.FacetCut({
+            facetAddress: address(facet),
+            action: IDiamondCut.FacetCutAction.Add,
+            functionSelectors: getSelectors("CBridgeFacet", exclude)
+            })
         );
-        string memory json = vm.readFile(path);
-        address facet = json.readAddress(".CelerIMFacet");
+        cutter.diamondCut(cut, address(0), "");
+    }
 
-        vm.startBroadcast(deployerPrivateKey);
+    facets = loupe.facetAddresses();
 
-        // CBridge
-        if (loupe.facetFunctionSelectors(facet).length == 0) {
-            bytes4[] memory exclude;
-            cut.push(
-                IDiamondCut.FacetCut({
-                    facetAddress: address(facet),
-                    action: IDiamondCut.FacetCutAction.Add,
-                    functionSelectors: getSelectors("CelerIMFacet", exclude)
-                })
-            );
-            cutter.diamondCut(cut, address(0), "");
-        }
-
-        facets = loupe.facetAddresses();
-
-        vm.stopBroadcast();
+    vm.stopBroadcast();
     }
 }
