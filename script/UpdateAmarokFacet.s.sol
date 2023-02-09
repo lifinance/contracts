b01d7dcd68a85a87b0456d20c6dcb2d2ab09e9e2
Daniel Bläcker <daniel@li.finance>
2023-02-09 12:09:57 +0700

    finalized

--- script/UpdateAmarokFacet.s.sol
+++ script/UpdateAmarokFacet.s.sol
@@ -15,36 +15,20 @@ contract DeployScript is UpdateScriptBase {
     function run() public returns (address[] memory facets) {
         address facet = json.readAddress(".AmarokFacet");
 
-        console.log("msg.sender1: ", msg.sender);
         vm.startBroadcast(deployerPrivateKey);
 
         // add Amarok facet to diamond
-//        if (loupe.facetFunctionSelectors(facet).length == 0) {
-//            bytes4[] memory exclude;
-//            cut.push(
-//                IDiamondCut.FacetCut({
-//                    facetAddress: address(facet),
-//                    action: IDiamondCut.FacetCutAction.Add,
-//                    functionSelectors: getSelectors("AmarokFacet", exclude)
-//                })
-//            );
-//            cutter.diamondCut(cut, address(0), "");
-//        }
-
-        console.log("deployerPrivateKey: ", deployerPrivateKey);
-        console.log("msg.sender2: ", msg.sender);
-        console.log("address(this): ", address(this));
-        console.log("diamond: ", diamond);
-        console.log("owner: ", OwnershipFacet(diamond).owner());
-//
-//        AmarokFacet(diamond).setAmarokDomain(uint256(1), uint32(6648936));
-//        AmarokFacet(diamond).setAmarokDomain(uint256(10), uint32(1869640809));
-//        AmarokFacet(diamond).setAmarokDomain(uint256(56), uint32(6450786));
-//        AmarokFacet(diamond).setAmarokDomain(uint256(56), uint32(6450786));
-//        AmarokFacet(diamond).setAmarokDomain(uint256(100), uint32(6778479));
-//        AmarokFacet(diamond).setAmarokDomain(uint256(137), uint32(1886350457));
-//        AmarokFacet(diamond).setAmarokDomain(uint256(1284), uint32(1650811245));
-//        AmarokFacet(diamond).setAmarokDomain(uint256(42161), uint32(1634886255));
+        if (loupe.facetFunctionSelectors(facet).length == 0) {
+            bytes4[] memory exclude;
+            cut.push(
+                IDiamondCut.FacetCut({
+                    facetAddress: address(facet),
+                    action: IDiamondCut.FacetCutAction.Add,
+                    functionSelectors: getSelectors("AmarokFacet", exclude)
+                })
+            );
+            cutter.diamondCut(cut, address(0), "");
+        }
 
         facets = loupe.facetAddresses();
 
