9c7f9310091780fe263df444877022df9cb42f83
h3xept <leo@li.finance>
2023-02-23 12:00:47 +0100

    fix: tests

--- test/solidity/Facets/GetGasFacet.t.sol
+++ test/solidity/Facets/GetGasFacet.t.sol
@@ -21,25 +21,25 @@ contract TestGetGasFacet is GetGasFacet {
 
 contract GetGasFacetTest is TestBaseFacet {
 
-    TestGetGasFacet internal gasUpFacet;
+    TestGetGasFacet internal getGasFacet;
     ILiFi.BridgeData internal validBridgeData;
 
     function setUp() public {
         initTestBase();
-        gasUpFacet = new TestGetGasFacet();
+        getGasFacet = new TestGetGasFacet();
 
         ServiceFeeCollector feeCollector = new ServiceFeeCollector(address(this));
         PeripheryRegistryFacet peripheryRegistry = new PeripheryRegistryFacet();
 
         bytes4[] memory functionSelectors = new bytes4[](4);
-        functionSelectors[0] = gasUpFacet
+        functionSelectors[0] = getGasFacet
             .startBridgeTokensViaGetGas
             .selector;
-        functionSelectors[1] = gasUpFacet
+        functionSelectors[1] = getGasFacet
             .swapAndStartBridgeTokensViaGetGas
             .selector;
-        functionSelectors[2] = gasUpFacet.addDex.selector;
-        functionSelectors[3] = gasUpFacet
+        functionSelectors[2] = getGasFacet.addDex.selector;
+        functionSelectors[3] = getGasFacet
             .setFunctionApprovalBySignature
             .selector;
 
@@ -48,39 +48,39 @@ contract GetGasFacetTest is TestBaseFacet {
             .registerPeripheryContract
             .selector;
 
-        addFacet(diamond, address(gasUpFacet), functionSelectors);
+        addFacet(diamond, address(getGasFacet), functionSelectors);
         addFacet(diamond, address(peripheryRegistry), peripheryRegistryFunctionSelectors);
         
-        gasUpFacet = TestGetGasFacet(address(diamond));
+        getGasFacet = TestGetGasFacet(address(diamond));
         peripheryRegistry = PeripheryRegistryFacet(address(diamond));
 
-        gasUpFacet.addDex(address(uniswap));
-        gasUpFacet.setFunctionApprovalBySignature(
+        getGasFacet.addDex(address(uniswap));
+        getGasFacet.setFunctionApprovalBySignature(
             uniswap.swapExactTokensForTokens.selector
         );
-        gasUpFacet.setFunctionApprovalBySignature(
+        getGasFacet.setFunctionApprovalBySignature(
             uniswap.swapTokensForExactETH.selector
         );
 
 
-        peripheryRegistry.registerPeripheryContract("SERVICE_FEE_COLLECTOR", address(feeCollector));
-        setFacetAddressInTestBase(address(gasUpFacet), "GetGasFacet");
+        peripheryRegistry.registerPeripheryContract("ServiceFeeCollector", address(feeCollector));
+        setFacetAddressInTestBase(address(getGasFacet), "GetGasFacet");
 
-        vm.makePersistent(address(gasUpFacet));
+        vm.makePersistent(address(getGasFacet));
         vm.makePersistent(address(peripheryRegistry));
 
         // adjust bridgeData
-        bridgeData.bridge = "gasUp";
+        bridgeData.bridge = "getGas";
         bridgeData.destinationChainId = 100;
     }
 
     function initiateBridgeTxWithFacet(bool isNative) internal override {
         if (isNative) {
-            gasUpFacet.startBridgeTokensViaGetGas{
+            getGasFacet.startBridgeTokensViaGetGas{
                 value: bridgeData.minAmount
             }(bridgeData);
         } else {
-            gasUpFacet.startBridgeTokensViaGetGas(bridgeData);
+            getGasFacet.startBridgeTokensViaGetGas(bridgeData);
         }
     }
 
@@ -89,11 +89,11 @@ contract GetGasFacetTest is TestBaseFacet {
         override
     {
         if (isNative) {
-            gasUpFacet.swapAndStartBridgeTokensViaGetGas{
+            getGasFacet.swapAndStartBridgeTokensViaGetGas{
                 value: swapData[0].fromAmount
             }(bridgeData, swapData);
         } else {
-            gasUpFacet.swapAndStartBridgeTokensViaGetGas(
+            getGasFacet.swapAndStartBridgeTokensViaGetGas(
                 bridgeData,
                 swapData
             );
