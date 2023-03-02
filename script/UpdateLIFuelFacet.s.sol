6d288de5fb018dad92d0e0a8860613fdf0a9b063
Leonardo Cascianelli <leo@li.finance>
2023-03-02 12:17:47 +0100

    chore: renamed getgas to lifuel (#223)

--- script/UpdateLIFuelFacet.s.sol
+++ script/UpdateLIFuelFacet.s.sol
@@ -4,12 +4,19 @@ pragma solidity ^0.8.17;
 import { UpdateScriptBase } from "./utils/UpdateScriptBase.sol";
 import { stdJson } from "forge-std/StdJson.sol";
 import { DiamondCutFacet, IDiamondCut } from "lifi/Facets/DiamondCutFacet.sol";
+<<<<<<<< HEAD:script/UpdatePeripheryRegistryFacet.s.sol
 import { PeripheryRegistryFacet } from "lifi/Facets/PeripheryRegistryFacet.sol";
+========
+import { OwnershipFacet } from "lifi/Facets/OwnershipFacet.sol";
+import { LIFuelFacet } from "lifi/Facets/LIFuelFacet.sol";
+import {DSTest} from "ds-test/test.sol";
+>>>>>>>> cf6b904 (chore: renamed getgas to lifuel (#223)):script/UpdateLIFuelFacet.s.sol
 
 contract DeployScript is UpdateScriptBase {
     using stdJson for string;
 
     function run() public returns (address[] memory facets) {
+<<<<<<<< HEAD:script/UpdatePeripheryRegistryFacet.s.sol
         string memory path = string.concat(
             root,
             "/deployments/",
@@ -20,6 +27,9 @@ contract DeployScript is UpdateScriptBase {
         );
         string memory json = vm.readFile(path);
         address facet = json.readAddress(".PeripheryRegistryFacet");
+========
+        address facet = json.readAddress(".LIFuelFacet");
+>>>>>>>> cf6b904 (chore: renamed getgas to lifuel (#223)):script/UpdateLIFuelFacet.s.sol
 
         vm.startBroadcast(deployerPrivateKey);
 
@@ -30,10 +40,14 @@ contract DeployScript is UpdateScriptBase {
                 IDiamondCut.FacetCut({
                     facetAddress: address(facet),
                     action: IDiamondCut.FacetCutAction.Add,
+<<<<<<<< HEAD:script/UpdatePeripheryRegistryFacet.s.sol
                     functionSelectors: getSelectors(
                         "PeripheryRegistryFacet",
                         exclude
                     )
+========
+                    functionSelectors: getSelectors("LIFuelFacet", exclude)
+>>>>>>>> cf6b904 (chore: renamed getgas to lifuel (#223)):script/UpdateLIFuelFacet.s.sol
                 })
             );
             cutter.diamondCut(cut, address(0), "");
---
+++ test/solidity/Facets/LIFuelFacet.t.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.17;
+
+import { ILiFi, LibSwap, LibAllowList, TestBaseFacet, console, ERC20 } from "../utils/TestBaseFacet.sol";
+import { ServiceFeeCollector } from "lifi/Periphery/ServiceFeeCollector.sol";
+import { LIFuelFacet } from "lifi/Facets/LIFuelFacet.sol";
+import { OnlyContractOwner, InvalidConfig, NotInitialized, AlreadyInitialized, InvalidAmount } from "src/Errors/GenericErrors.sol";
+import { DiamondTest, LiFiDiamond } from "../utils/DiamondTest.sol";
+import { PeripheryRegistryFacet } from "lifi/Facets/PeripheryRegistryFacet.sol";
+
+// Stub LIFuelFacet Contract
+contract TestLIFuelFacet is LIFuelFacet {
+    function addDex(address _dex) external {
+        LibAllowList.addAllowedContract(_dex);
+    }
+
+    function setFunctionApprovalBySignature(bytes4 _signature) external {
+        LibAllowList.addAllowedSelector(_signature);
+    }
+}
+
+contract LIFuelFacetTest is TestBaseFacet {
+
+    TestLIFuelFacet internal lifuelFacet;
+    ILiFi.BridgeData internal validBridgeData;
+
+    function setUp() public {
+        initTestBase();
+        lifuelFacet = new TestLIFuelFacet();
+
+        ServiceFeeCollector feeCollector = new ServiceFeeCollector(address(this));
+        PeripheryRegistryFacet peripheryRegistry = new PeripheryRegistryFacet();
+
+        bytes4[] memory functionSelectors = new bytes4[](4);
+        functionSelectors[0] = lifuelFacet
+            .startBridgeTokensViaLIFuel
+            .selector;
+        functionSelectors[1] = lifuelFacet
+            .swapAndStartBridgeTokensViaLIFuel
+            .selector;
+        functionSelectors[2] = lifuelFacet.addDex.selector;
+        functionSelectors[3] = lifuelFacet
+            .setFunctionApprovalBySignature
+            .selector;
+
+        bytes4[] memory peripheryRegistryFunctionSelectors = new bytes4[](1);
+        peripheryRegistryFunctionSelectors[0] = peripheryRegistry
+            .registerPeripheryContract
+            .selector;
+
+        addFacet(diamond, address(lifuelFacet), functionSelectors);
+        addFacet(diamond, address(peripheryRegistry), peripheryRegistryFunctionSelectors);
+        
+        lifuelFacet = TestLIFuelFacet(address(diamond));
+        peripheryRegistry = PeripheryRegistryFacet(address(diamond));
+
+        lifuelFacet.addDex(address(uniswap));
+        lifuelFacet.setFunctionApprovalBySignature(
+            uniswap.swapExactTokensForTokens.selector
+        );
+        lifuelFacet.setFunctionApprovalBySignature(
+            uniswap.swapTokensForExactETH.selector
+        );
+
+
+        peripheryRegistry.registerPeripheryContract("ServiceFeeCollector", address(feeCollector));
+        setFacetAddressInTestBase(address(lifuelFacet), "LIFuelFacet");
+
+        vm.makePersistent(address(lifuelFacet));
+        vm.makePersistent(address(peripheryRegistry));
+
+        // adjust bridgeData
+        bridgeData.bridge = "lifuel";
+        bridgeData.destinationChainId = 100;
+    }
+
+    function initiateBridgeTxWithFacet(bool isNative) internal override {
+        if (isNative) {
+            lifuelFacet.startBridgeTokensViaLIFuel{
+                value: bridgeData.minAmount
+            }(bridgeData);
+        } else {
+            lifuelFacet.startBridgeTokensViaLIFuel(bridgeData);
+        }
+    }
+
+    function initiateSwapAndBridgeTxWithFacet(bool isNative)
+        internal
+        override
+    {
+        if (isNative) {
+            lifuelFacet.swapAndStartBridgeTokensViaLIFuel{
+                value: swapData[0].fromAmount
+            }(bridgeData, swapData);
+        } else {
+            lifuelFacet.swapAndStartBridgeTokensViaLIFuel(
+                bridgeData,
+                swapData
+            );
+        }
+    }
+
+    function testBase_Revert_BridgeWithInvalidDestinationCallFlag()
+    public
+    override
+    {
+        console.log("Not applicable for LIFuelFacet");
+    }
+
+}
