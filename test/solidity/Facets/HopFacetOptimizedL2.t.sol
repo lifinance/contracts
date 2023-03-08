f08fb1bfe7b901c949f35704973f9a1ff8293731
Satoshi Naoki <satoshi@li.finance>
2023-03-08 11:11:01 +0900

    update CircleBridgeFacet and HopFacetOptimizedL2 foundry test with custom rpc specification

--- test/solidity/Facets/HopFacetOptimizedL2.t.sol
+++ test/solidity/Facets/HopFacetOptimizedL2.t.sol
@@ -35,7 +35,7 @@ contract HopFacetOptimizedL2Test is TestBaseFacet {
 
     function setUp() public {
         // Custom Config
-        customRpcUrlForForking = vm.envString("ETH_NODE_URI_POLYGON");
+        customRpcUrlForForking = "ETH_NODE_URI_POLYGON";
         customBlockNumberForForking = 38461246;
         ADDRESS_USDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
         ADDRESS_DAI = 0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063;
