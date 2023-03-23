670f8813d7fac74257b5ebdb1d169f1d556990e4
Ed Zynda <ezynda3@gmail.com>
2023-03-23 15:36:39 -0700

    refactor LibBytes

--- src/Facets/SquidFacet.sol
+++ src/Facets/SquidFacet.sol
@@ -9,7 +9,7 @@ import { LibSwap } from "../Libraries/LibSwap.sol";
 import { ReentrancyGuard } from "../Helpers/ReentrancyGuard.sol";
 import { SwapperV2 } from "../Helpers/SwapperV2.sol";
 import { Validatable } from "../Helpers/Validatable.sol";
-import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+import { LibBytes } from "../Libraries/LibBytes.sol";
 import { InformationMismatch } from "../Errors/GenericErrors.sol";
 
 /// @title Squid Facet
@@ -154,7 +154,7 @@ contract SquidFacet is ILiFi, ReentrancyGuard, SwapperV2, Validatable {
                 _bridgeData.sendingAssetId,
                 _bridgeData.minAmount,
                 _squidData.destinationChain,
-                Strings.toHexString(uint160(_bridgeData.receiver), 20),
+                LibBytes.toHexString(uint160(_bridgeData.receiver), 20),
                 _squidData.bridgedTokenSymbol,
                 _squidData.sourceCalls
             );
