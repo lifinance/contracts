84aa8f4c19fda3362203049867f7c5175f5c3329
Satoshi Naoki <satoshi@li.finance>
2023-04-20 11:14:37 +0900

    implement OFTWrapper facet

---
+++ src/Facets/OFTWrapperFacet.sol
@@ -0,0 +1,318 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import { ILiFi } from "../Interfaces/ILiFi.sol";
+import { IOFTWrapper } from "../Interfaces/IOFTWrapper.sol";
+import { LibAsset, IERC20 } from "../Libraries/LibAsset.sol";
+import { LibDiamond } from "../Libraries/LibDiamond.sol";
+import { ReentrancyGuard } from "../Helpers/ReentrancyGuard.sol";
+import { AlreadyInitialized, NotInitialized } from "../Errors/GenericErrors.sol";
+import { SwapperV2, LibSwap } from "../Helpers/SwapperV2.sol";
+import { Validatable } from "../Helpers/Validatable.sol";
+
+/// @title OFTWrapper Facet
+/// @author LI.FI (https://li.fi)
+/// @notice Provides functionality for bridging through OFTWrapper
+/// @custom:version 1.0.0
+contract OFTWrapperFacet is ILiFi, ReentrancyGuard, SwapperV2, Validatable {
+    /// Storage ///
+
+    bytes32 internal constant NAMESPACE = keccak256("com.lifi.facets.oft");
+
+    /// @notice The contract address of the OFTWrapper on the source chain.
+    IOFTWrapper private immutable oftWrapper;
+
+    /// Types ///
+
+    enum TokenType {
+        OFT,
+        OFTV2,
+        OFTFeeV2,
+        ProxyOFTV2,
+        ProxyOFTFeeV2
+    }
+
+    struct ChainIdConfig {
+        uint256 chainId;
+        uint16 layerZeroChainId;
+    }
+
+    struct Storage {
+        mapping(uint256 => uint16) layerZeroChainId;
+        bool initialized;
+    }
+
+    struct OFTWrapperData {
+        TokenType tokenType;
+        bytes32 receiver;
+        uint256 minAmount;
+        uint256 lzFee;
+        bytes adapterParams;
+    }
+
+    /// Errors ///
+
+    error UnknownLayerZeroChain();
+
+    /// Events ///
+
+    event OFTWrapperInitialized(ChainIdConfig[] chainIdConfigs);
+
+    event LayerZeroChainIdSet(
+        uint256 indexed chainId,
+        uint16 layerZeroChainId
+    );
+
+    /// Constructor ///
+
+    /// @notice Initialize the contract.
+    /// @param _oftWrapper The contract address of the OFTWrapper on the source chain.
+    constructor(IOFTWrapper _oftWrapper) {
+        oftWrapper = _oftWrapper;
+    }
+
+    /// Init ///
+
+    /// @notice Initialize local variables for the OFTWrapper Facet.
+    /// @param chainIdConfigs Chain Id configuration data.
+    function initOFTWrapper(ChainIdConfig[] calldata chainIdConfigs) external {
+        LibDiamond.enforceIsContractOwner();
+
+        Storage storage sm = getStorage();
+
+        if (sm.initialized) {
+            revert AlreadyInitialized();
+        }
+
+        for (uint256 i = 0; i < chainIdConfigs.length; i++) {
+            sm.layerZeroChainId[chainIdConfigs[i].chainId] = chainIdConfigs[i]
+                .layerZeroChainId;
+        }
+
+        sm.initialized = true;
+
+        emit OFTWrapperInitialized(chainIdConfigs);
+    }
+
+    /// External Methods ///
+
+    /// @notice Bridges tokens via OFT Wrapper.
+    /// @param _bridgeData The core information needed for bridging.
+    /// @param _oftWrapperData Data specific to OFT Wrapper.
+    function startBridgeTokensViaOFTWrapper(
+        ILiFi.BridgeData calldata _bridgeData,
+        OFTWrapperData calldata _oftWrapperData
+    )
+        external
+        payable
+        nonReentrant
+        refundExcessNative(payable(msg.sender))
+        validateBridgeData(_bridgeData)
+        doesNotContainSourceSwaps(_bridgeData)
+        doesNotContainDestinationCalls(_bridgeData)
+        noNativeAsset(_bridgeData)
+    {
+        LibAsset.depositAsset(
+            _bridgeData.sendingAssetId,
+            _bridgeData.minAmount
+        );
+
+        _startBridge(_bridgeData, _oftWrapperData);
+    }
+
+    /// @notice Performs a swap before bridging via OFT Wrapper.
+    /// @param _bridgeData The core information needed for bridging.
+    /// @param _swapData An array of swap related data for performing swaps before bridging.
+    /// @param _oftWrapperData Data specific to OFT Wrapper.
+    function swapAndStartBridgeTokensViaOFTWrapper(
+        ILiFi.BridgeData memory _bridgeData,
+        LibSwap.SwapData[] calldata _swapData,
+        OFTWrapperData calldata _oftWrapperData
+    )
+        external
+        payable
+        nonReentrant
+        refundExcessNative(payable(msg.sender))
+        validateBridgeData(_bridgeData)
+        containsSourceSwaps(_bridgeData)
+        doesNotContainDestinationCalls(_bridgeData)
+        noNativeAsset(_bridgeData)
+    {
+        _bridgeData.minAmount = _depositAndSwap(
+            _bridgeData.transactionId,
+            _bridgeData.minAmount,
+            _swapData,
+            payable(msg.sender),
+            _oftWrapperData.lzFee
+        );
+
+        _startBridge(_bridgeData, _oftWrapperData);
+    }
+
+    /// @notice Get fee estimation.
+    /// @param _bridgeData The core information needed for bridging.
+    /// @param _oftWrapperData Data specific to OFT Wrapper.
+    function estimateSendFee(
+        ILiFi.BridgeData memory _bridgeData,
+        OFTWrapperData calldata _oftWrapperData
+    ) external view returns (uint256 nativeFee, uint256 zroFee) {
+        if (_oftWrapperData.tokenType == TokenType.OFT) {
+            return
+                oftWrapper.estimateSendFee(
+                    _bridgeData.sendingAssetId,
+                    getOFTLayerZeroChainId(_bridgeData.destinationChainId),
+                    abi.encodePacked(_bridgeData.receiver),
+                    _bridgeData.minAmount,
+                    false,
+                    _oftWrapperData.adapterParams,
+                    IOFTWrapper.FeeObj(0, address(0), "")
+                );
+        } else {
+            return
+                oftWrapper.estimateSendFeeV2(
+                    _bridgeData.sendingAssetId,
+                    getOFTLayerZeroChainId(_bridgeData.destinationChainId),
+                    _oftWrapperData.receiver,
+                    _bridgeData.minAmount,
+                    false,
+                    _oftWrapperData.adapterParams,
+                    IOFTWrapper.FeeObj(0, address(0), "")
+                );
+        }
+    }
+
+    /// Internal Methods ///
+
+    /// @dev Contains the business logic for the bridge via OFT Wrapper
+    /// @param _bridgeData The core information needed for bridging
+    /// @param _oftWrapperData Data specific to OFT Wrapper
+    function _startBridge(
+        ILiFi.BridgeData memory _bridgeData,
+        OFTWrapperData calldata _oftWrapperData
+    ) internal {
+        LibAsset.maxApproveERC20(
+            IERC20(_bridgeData.sendingAssetId),
+            address(oftWrapper),
+            _bridgeData.minAmount
+        );
+
+        if (_oftWrapperData.tokenType == TokenType.OFT) {
+            oftWrapper.sendOFT{ value: _oftWrapperData.lzFee }(
+                _bridgeData.sendingAssetId,
+                getOFTLayerZeroChainId(_bridgeData.destinationChainId),
+                abi.encodePacked(_oftWrapperData.receiver),
+                _bridgeData.minAmount,
+                _oftWrapperData.minAmount,
+                payable(msg.sender),
+                address(0),
+                _oftWrapperData.adapterParams,
+                IOFTWrapper.FeeObj(0, address(0), "")
+            );
+        } else if (_oftWrapperData.tokenType == TokenType.OFTV2) {
+            oftWrapper.sendOFTV2{ value: _oftWrapperData.lzFee }(
+                _bridgeData.sendingAssetId,
+                getOFTLayerZeroChainId(_bridgeData.destinationChainId),
+                _oftWrapperData.receiver,
+                _bridgeData.minAmount,
+                _oftWrapperData.minAmount,
+                IOFTWrapper.LzCallParams(
+                    payable(msg.sender),
+                    address(0),
+                    _oftWrapperData.adapterParams
+                ),
+                IOFTWrapper.FeeObj(0, address(0), "")
+            );
+        } else if (_oftWrapperData.tokenType == TokenType.OFTFeeV2) {
+            oftWrapper.sendOFTFeeV2{ value: _oftWrapperData.lzFee }(
+                _bridgeData.sendingAssetId,
+                getOFTLayerZeroChainId(_bridgeData.destinationChainId),
+                _oftWrapperData.receiver,
+                _bridgeData.minAmount,
+                _oftWrapperData.minAmount,
+                IOFTWrapper.LzCallParams(
+                    payable(msg.sender),
+                    address(0),
+                    _oftWrapperData.adapterParams
+                ),
+                IOFTWrapper.FeeObj(0, address(0), "")
+            );
+        } else if (_oftWrapperData.tokenType == TokenType.ProxyOFTV2) {
+            oftWrapper.sendProxyOFTV2{ value: _oftWrapperData.lzFee }(
+                _bridgeData.sendingAssetId,
+                getOFTLayerZeroChainId(_bridgeData.destinationChainId),
+                _oftWrapperData.receiver,
+                _bridgeData.minAmount,
+                _oftWrapperData.minAmount,
+                IOFTWrapper.LzCallParams(
+                    payable(msg.sender),
+                    address(0),
+                    _oftWrapperData.adapterParams
+                ),
+                IOFTWrapper.FeeObj(0, address(0), "")
+            );
+        } else if (_oftWrapperData.tokenType == TokenType.ProxyOFTFeeV2) {
+            oftWrapper.sendProxyOFTFeeV2{ value: _oftWrapperData.lzFee }(
+                _bridgeData.sendingAssetId,
+                getOFTLayerZeroChainId(_bridgeData.destinationChainId),
+                _oftWrapperData.receiver,
+                _bridgeData.minAmount,
+                _oftWrapperData.minAmount,
+                IOFTWrapper.LzCallParams(
+                    payable(msg.sender),
+                    address(0),
+                    _oftWrapperData.adapterParams
+                ),
+                IOFTWrapper.FeeObj(0, address(0), "")
+            );
+        }
+
+        emit LiFiTransferStarted(_bridgeData);
+    }
+
+    /// Mappings management ///
+
+    /// @notice Sets the Layer zero chain Id for a given chain Id.
+    /// @param _chainId Chain Id.
+    /// @param _layerZeroChainId Layer zero chain Id.
+    /// @dev This is used to map a chain Id to its Layer zero chain Id.
+    function setOFTLayerZeroChainId(
+        uint256 _chainId,
+        uint16 _layerZeroChainId
+    ) external {
+        LibDiamond.enforceIsContractOwner();
+        Storage storage sm = getStorage();
+
+        if (!sm.initialized) {
+            revert NotInitialized();
+        }
+
+        sm.layerZeroChainId[_chainId] = _layerZeroChainId;
+
+        emit LayerZeroChainIdSet(_chainId, _layerZeroChainId);
+    }
+
+    /// @notice Gets the Layer zero chain Id for a given chain Id.
+    /// @param _chainId Chain Id.
+    /// @return layerZeroChainId Layer zero chain Id.
+    function getOFTLayerZeroChainId(
+        uint256 _chainId
+    ) private view returns (uint16 layerZeroChainId) {
+        Storage storage sm = getStorage();
+        layerZeroChainId = sm.layerZeroChainId[_chainId];
+
+        if (layerZeroChainId == 0) {
+            revert UnknownLayerZeroChain();
+        }
+
+        return layerZeroChainId;
+    }
+
+    /// @dev fetch local storage
+    function getStorage() private pure returns (Storage storage s) {
+        bytes32 namespace = NAMESPACE;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            s.slot := namespace
+        }
+    }
+}
