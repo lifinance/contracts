a2d76a8aa63a0b1168108d6274df250d43f419e7
Ed Zynda <ezynda3@gmail.com>
2023-04-20 13:37:33 +0300

    chore: update tests

--- src/Facets/AcrossFacet.sol
+++ src/Facets/AcrossFacet.sol
@@ -13,7 +13,7 @@ import { Validatable } from "../Helpers/Validatable.sol";
 /// @title Across Facet
 /// @author LI.FI (https://li.fi)
 /// @notice Provides functionality for bridging through Across Protocol
-/// @custom:version 1.0.0
+/// @custom:version 1.0.1
 contract AcrossFacet is ILiFi, ReentrancyGuard, SwapperV2, Validatable {
     /// Storage ///
 
