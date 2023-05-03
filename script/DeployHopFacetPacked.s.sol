f5d47c16273870c52ed7b11d9904a4d28f8bb33e
Ed Zynda <ezynda3@gmail.com>
2023-05-03 18:26:18 +0300

    feat: update deploy script

--- script/DeployHopFacetPacked.s.sol
+++ script/DeployHopFacetPacked.s.sol
@@ -3,17 +3,47 @@ pragma solidity ^0.8.17;
 
 import { DeployScriptBase } from "./utils/DeployScriptBase.sol";
 import { HopFacetPacked } from "lifi/Facets/HopFacetPacked.sol";
+import { stdJson } from "forge-std/StdJson.sol";
 
 contract DeployScript is DeployScriptBase {
+    using stdJson for string;
+
+    string internal json;
+    string internal path;
+
+    struct Config {
+        address ammWrapper;
+        address bridge;
+        string name;
+        address token;
+    }
+
     constructor() DeployScriptBase("HopFacetPacked") {}
 
     function run()
         public
         returns (HopFacetPacked deployed, bytes memory constructorArgs)
     {
+        path = string.concat(root, "/config/hop.json");
+        json = vm.readFile(path);
+        bytes memory rawConfig = json.parseRaw(
+            string.concat(".", network, ".tokens")
+        );
+        Config[] memory configs = abi.decode(rawConfig, (Config[]));
+
+        address ammWrapper;
+        for (uint256 i = 0; i < configs.length; i++) {
+            if (
+                keccak256(abi.encodePacked(configs[i].name)) ==
+                keccak256(abi.encodePacked("ETH"))
+            ) {
+                ammWrapper = configs[i].ammWrapper;
+            }
+        }
+
         vm.startBroadcast(deployerPrivateKey);
 
-        constructorArgs = abi.encode(deployerAddress);
+        constructorArgs = abi.encode(deployerAddress, ammWrapper);
 
         if (isDeployed()) {
             return (HopFacetPacked(predicted), constructorArgs);
