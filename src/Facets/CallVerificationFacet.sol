251a609d449db54472204244b2a2bfc1941f1159
Ed Zynda <ezynda3@gmail.com>
2023-05-10 14:51:26 +0300

    Implement calldata verification

---
+++ src/Facets/CallVerificationFacet.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import { ILiFi } from "../Interfaces/ILiFi.sol";
+import { LibSwap } from "../Libraries/LibSwap.sol";
+
+/// @title A title that should describe the contract/interface
+/// @author Li.Finance (https://li.finance)
+/// @notice Provides functionality for verifying calldata
+/// @custom:version 0.0.1
+contract CallVerificationFacet {
+    /// @notice Extracts the bridge data from the calldata
+    /// @param data The calldata to extract the bridge data from
+    /// @return bridgeData The bridge data extracted from the calldata
+    function extractBridgeData(bytes calldata data)
+        external
+        pure
+        returns (ILiFi.BridgeData memory)
+    {
+        ILiFi.BridgeData memory bridgeData;
+        bridgeData = abi.decode(data[4:], (ILiFi.BridgeData));
+        return bridgeData;
+    }
+
+    /// @notice Extracts the swap data from the calldata
+    /// @param data The calldata to extract the swap data from
+    /// @return swapData The swap data extracted from the calldata
+    function extractSwapData(bytes calldata data)
+        external
+        pure
+        returns (LibSwap.SwapData[] memory)
+    {
+        LibSwap.SwapData[] memory swapData;
+        (, swapData) = abi.decode(
+            data[4:],
+            (ILiFi.BridgeData, LibSwap.SwapData[])
+        );
+        return swapData;
+    }
+
+    /// @notice Extracts the bridge data and swap data from the calldata
+    /// @param data The calldata to extract the bridge data and swap data from
+    /// @return bridgeData The bridge data extracted from the calldata
+    function extractData(bytes calldata data)
+        external
+        pure
+        returns (ILiFi.BridgeData memory, LibSwap.SwapData[] memory)
+    {
+        ILiFi.BridgeData memory bridgeData;
+        LibSwap.SwapData[] memory swapData;
+        (bridgeData, swapData) = abi.decode(
+            data[4:],
+            (ILiFi.BridgeData, LibSwap.SwapData[])
+        );
+        return (bridgeData, swapData);
+    }
+
+    /// @notice Extracts the main parameters from the calldata
+    /// @param data The calldata to extract the main parameters from
+    /// @return receiver The receiver extracted from the calldata
+    function extractMainParameters(bytes calldata data)
+        external
+        pure
+        returns (
+            address,
+            uint256,
+            uint256,
+            ILiFi.BridgeData memory
+        )
+    {
+        ILiFi.BridgeData memory bridgeData;
+        (bridgeData) = abi.decode(data[4:], (ILiFi.BridgeData));
+        return (
+            bridgeData.receiver,
+            bridgeData.minAmount,
+            bridgeData.destinationChainId,
+            bridgeData
+        );
+    }
+
+    /// @notice Validates the calldata
+    /// @param data The calldata to validate
+    /// @param receiver The receiver to validate
+    /// @param sendingAssetId The sending asset id to validate
+    /// @param amount The amount to validate
+    /// @param destinationChainId The destination chain id to validate
+    /// @return isValid Whether the calldata is validate
+    function validateCalldata(
+        bytes calldata data,
+        address receiver,
+        address sendingAssetId,
+        uint256 amount,
+        uint256 destinationChainId
+    ) external pure returns (bool) {
+        ILiFi.BridgeData memory bridgeData;
+        (bridgeData) = abi.decode(data[4:], (ILiFi.BridgeData));
+        return
+            (bridgeData.receiver == address(0xfff) ||
+                bridgeData.receiver == receiver) &&
+            bridgeData.sendingAssetId == sendingAssetId &&
+            bridgeData.minAmount == amount &&
+            bridgeData.destinationChainId == destinationChainId;
+    }
+}
---
+++ test/solidity/Facets/CallVerificationFacet.sol
@@ -0,0 +1,142 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import { CallVerificationFacet } from "lifi/Facets/CallVerificationFacet.sol";
+import { HyphenFacet } from "lifi/Facets/HyphenFacet.sol";
+import { ILiFi } from "lifi/Interfaces/ILiFi.sol";
+import { LibSwap } from "lifi/Libraries/LibSwap.sol";
+import { TestBase } from "../utils/TestBase.sol";
+
+contract CallVerificationFacetTest is TestBase {
+    CallVerificationFacet internal callVerificationFacet;
+
+    function setUp() public {
+        callVerificationFacet = new CallVerificationFacet();
+        bridgeData = ILiFi.BridgeData({
+            transactionId: keccak256("id"),
+            bridge: "acme",
+            integrator: "acme",
+            referrer: address(0),
+            sendingAssetId: address(123),
+            receiver: address(456),
+            minAmount: 1 ether,
+            destinationChainId: 137,
+            hasSourceSwaps: false,
+            hasDestinationCall: false
+        });
+
+        swapData.push(
+            LibSwap.SwapData({
+                callTo: address(uniswap),
+                approveTo: address(uniswap),
+                sendingAssetId: address(123),
+                receivingAssetId: address(456),
+                fromAmount: 1 ether,
+                callData: abi.encodePacked("calldata"),
+                requiresDeposit: true
+            })
+        );
+    }
+
+    function test_CanExtractBridgeData() public {
+        bytes memory callData = abi.encodeWithSelector(
+            HyphenFacet.startBridgeTokensViaHyphen.selector,
+            bridgeData
+        );
+
+        bytes memory fullCalldata = bytes.concat(callData, "extra stuff"); // Add extra bytes because Hyphen does not have call specific data
+        ILiFi.BridgeData memory returnedData = callVerificationFacet
+            .extractBridgeData(fullCalldata);
+
+        checkBridgeData(returnedData);
+    }
+
+    function test_CanExtractSwapData() public {
+        bytes memory callData = abi.encodeWithSelector(
+            HyphenFacet.swapAndStartBridgeTokensViaHyphen.selector,
+            bridgeData,
+            swapData
+        );
+
+        bytes memory fullCalldata = bytes.concat(callData, "extra stuff"); // Add extra bytes because Hyphen does not have call specific data
+        LibSwap.SwapData[] memory returnedData = callVerificationFacet
+            .extractSwapData(fullCalldata);
+
+        checkSwapData(returnedData);
+    }
+
+    function test_CatExtractBridgeAndSwapData() public {
+        bytes memory callData = abi.encodeWithSelector(
+            HyphenFacet.swapAndStartBridgeTokensViaHyphen.selector,
+            bridgeData,
+            swapData
+        );
+
+        bytes memory fullCalldata = bytes.concat(callData, "extra stuff"); // Add extra bytes because Hyphen does not have call specific data
+        (
+            ILiFi.BridgeData memory returnedBridgeData,
+            LibSwap.SwapData[] memory returnedSwapData
+        ) = callVerificationFacet.extractData(fullCalldata);
+
+        checkBridgeData(returnedBridgeData);
+        checkSwapData(returnedSwapData);
+    }
+
+    function test_CanExtractMainParameters() public {
+        bytes memory callData = abi.encodeWithSelector(
+            HyphenFacet.startBridgeTokensViaHyphen.selector,
+            bridgeData
+        );
+
+        bytes memory fullCalldata = bytes.concat(callData, "extra stuff"); // Add extra bytes because Hyphen does not have call specific data
+        (
+            address returnedReceiver,
+            uint256 returnedMinAmount,
+            uint256 returnedDestinationChainId,
+            ILiFi.BridgeData memory returnedBridgeData
+        ) = callVerificationFacet.extractMainParameters(fullCalldata);
+
+        checkBridgeData(returnedBridgeData);
+        assertEq(returnedReceiver, bridgeData.receiver);
+        assertEq(returnedMinAmount, bridgeData.minAmount);
+        assertEq(returnedDestinationChainId, bridgeData.destinationChainId);
+    }
+
+    function test_CanValidateCalldata() public {
+        bytes memory callData = abi.encodeWithSelector(
+            HyphenFacet.startBridgeTokensViaHyphen.selector,
+            bridgeData
+        );
+
+        bytes memory fullCalldata = bytes.concat(callData, "extra stuff"); // Add extra bytes because Hyphen does not have call specific data
+        bool validCall = callVerificationFacet.validateCalldata(
+            fullCalldata,
+            bridgeData.receiver,
+            bridgeData.sendingAssetId,
+            bridgeData.minAmount,
+            bridgeData.destinationChainId
+        );
+        bool invalidCall = callVerificationFacet.validateCalldata(
+            fullCalldata,
+            address(0xb33f),
+            bridgeData.sendingAssetId,
+            bridgeData.minAmount,
+            bridgeData.destinationChainId
+        );
+        assertTrue(validCall);
+        assertFalse(invalidCall);
+    }
+
+    function checkBridgeData(ILiFi.BridgeData memory data) internal {
+        assertTrue(data.transactionId == bridgeData.transactionId);
+        assertEq(data.bridge, bridgeData.bridge);
+        assertEq(data.integrator, bridgeData.integrator);
+    }
+
+    function checkSwapData(LibSwap.SwapData[] memory data) internal {
+        assertTrue(data[0].callTo == swapData[0].callTo);
+        assertTrue(data[0].approveTo == swapData[0].approveTo);
+        assertTrue(data[0].sendingAssetId == swapData[0].sendingAssetId);
+        assertTrue(data[0].receivingAssetId == swapData[0].receivingAssetId);
+    }
+}
