74487554763ea3d6dc2622585eb2ccc06ecb1242
Daniel Bläcker <d.blaecker@gmail.com>
2023-06-01 14:56:23 +0200

    added script to transfer ownership of periphery contracts

--- script/tasks/diamondMakeImmutable.sh
+++ script/tasks/diamondMakeImmutable.sh
@@ -25,7 +25,7 @@ diamondMakeImmutable() {
 
 
   attempts=1
-
+  # PART 1 - SETTER FUNCTION REMOVAL IN FACETS ------------------------------------------------------------------------
   # remove all function selectors that will be unusable in the immutable diamond
   while [ $attempts -lt 11 ]
   do
@@ -65,6 +65,46 @@ diamondMakeImmutable() {
   echo "Press button to continue."
   read
 
+  # PART 2 - TRANSFER OWNERSHIP OF PERIPHERY CONTRACTS  ----------------------------------------------------------------
+	attempts=1
+
+  while [ $attempts -lt 11 ]
+  do
+    echo "Trying to transfer ownership of periphery contracts now - attempt ${attempts}"
+    # try to execute call
+    RAW_RETURN_DATA=$(NETWORK=$NETWORK FILE_SUFFIX=$FILE_SUFFIX PRIVATE_KEY=$(getPrivateKey "$ENVIRONMENT") forge script script/tasks/solidity/transferOwnershipOfPeripheryContractsForImmutable.s.sol -f $NETWORK -vvvv --json --silent --broadcast --verify --skip-simulation --legacy --tc DeployScript)
+    RETURN_CODE=$?
+
+    # print return data only if debug mode is activated
+    echoDebug "RAW_RETURN_DATA: $RAW_RETURN_DATA"
+
+    # check return data for error message (regardless of return code as this is not 100% reliable)
+    if [[ $RAW_RETURN_DATA == *"\"logs\":[]"* && $RAW_RETURN_DATA == *"\"returns\":{}"* ]]; then
+      # try to extract error message and throw error
+      ERROR_MESSAGE=$(echo "$RAW_RETURN_DATA" | sed -n 's/.*0\\0\\0\\0\\0\(.*\)\\0\".*/\1/p')
+      if [[ $ERROR_MESSAGE == "" ]]; then
+        error "execution of script failed. Could not extract error message. RAW_RETURN_DATA: $RAW_RETURN_DATA"
+      else
+        error "execution of script failed with message: $ERROR_MESSAGE"
+      fi
+
+    # check the return code the last call
+    elif [ $RETURN_CODE -eq 0 ]; then
+      break  # exit the loop if the operation was successful
+    fi
+
+    attempts=$((attempts+1))  # increment attempts
+    sleep 1  # wait for 1 second before trying the operation again
+  done
+
+  if [ $attempts -eq 11 ]
+  then
+      echo "Failed to transfer ownership of periphery contractsfor g"
+      exit 1
+  fi
+
+
+  # PART 3 - TRANSFER DIAMOND OWNERSHIP & REMOVE DIAMONDCUT FACET  -----------------------------------------------------
   gum style \
 	--foreground 212 --border-foreground 213 --border double \
 	--align center --width 50 --margin "1 2" --padding "2 4" \
---
+++ script/tasks/solidity/transferOwnershipOfPeripheryContractsForImmutable.s.sol
@@ -0,0 +1,149 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import { Script, console } from "forge-std/Script.sol";
+import { stdJson } from "forge-std/Script.sol";
+import { TransferrableOwnership } from "lifi/Helpers/TransferrableOwnership.sol";
+import { ERC20Proxy } from "lifi/Periphery/ERC20Proxy.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { Executor } from "lifi/Periphery/Executor.sol";
+import { FeeCollector } from "lifi/Periphery/FeeCollector.sol";
+import { Receiver } from "lifi/Periphery/Receiver.sol";
+import { RelayerCelerIM } from "lifi/Periphery/RelayerCelerIM.sol";
+import { ServiceFeeCollector } from "lifi/Periphery/ServiceFeeCollector.sol";
+import { PeripheryRegistryFacet } from "lifi/Facets/PeripheryRegistryFacet.sol";
+
+contract DeployScript is Script{
+    using stdJson for string;
+
+    string internal path;
+    string internal json;
+    string internal globalConfigJson;
+    uint256 internal deployerPrivateKey;
+    string internal network;
+    string internal fileSuffix;
+    string internal root;
+    address internal diamond;
+    address internal contractAddress;
+
+    constructor() {
+        deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
+        root = vm.projectRoot();
+        network = vm.envString("NETWORK");
+        fileSuffix = vm.envString("FILE_SUFFIX");
+
+        path = string.concat(
+            root,
+            "/deployments/",
+            network,
+            ".",
+            fileSuffix,
+            "json"
+        );
+        json = vm.readFile(path);
+        diamond = json.readAddress(".LiFiDiamondImmutable");
+    }
+
+
+    function run() public {
+        vm.startBroadcast(deployerPrivateKey);
+        console.log("in script transferOwnershipOfPeripheryContractsForImmutable");
+
+        // get correct path of diamond log
+        path = string.concat(
+            root,
+            "/deployments/",
+            network,
+            ".diamond.immutable.",
+            fileSuffix,
+            "json"
+        );
+
+        // read file into json variable
+        json = vm.readFile(path);
+
+        // get correct path of diamond log
+        path = string.concat(root, "/config/global.json");
+
+        // read file into json variable
+        globalConfigJson = vm.readFile(path);
+        address refundWalletAddress = json.readAddress(".refundWallet");
+        address withdrawWalletAddress = json.readAddress(".withdrawWallet");
+
+        // ------- ERC20Proxy
+        if (PeripheryRegistryFacet(diamond).getPeripheryContract("ERC20Proxy") != address(0)) {
+            console.log("ERC20Proxy now");
+            // get contract address
+            contractAddress = json.readAddress(".Periphery.ERC20Proxy");
+            address executor = json.readAddress(".Periphery.Executor");
+            console.log("contractAddress: ", contractAddress);
+
+            // set Executor contract as authorized caller
+            ERC20Proxy(contractAddress).setAuthorizedCaller(executor, true);
+
+            // renounceOwnership
+            console.log("before");
+            Ownable(contractAddress).renounceOwnership();
+            console.log("after");
+        }
+
+
+        // ------- FeeCollector
+        if (PeripheryRegistryFacet(diamond).getPeripheryContract("FeeCollector") != address(0)) {
+            console.log("FeeCollector now");
+            // get contract address
+            contractAddress = json.readAddress(".Periphery.FeeCollector");
+            console.log("contractAddress: ", contractAddress);
+
+            // transfer ownership to withdraw wallet
+            // TODO: write script to confirm ownership transfers by wallet
+            console.log("before");
+            TransferrableOwnership(contractAddress).transferOwnership(withdrawWalletAddress);
+            console.log("after");
+        }
+
+        // ------- Receiver
+        if (PeripheryRegistryFacet(diamond).getPeripheryContract("Receiver") != address(0)) {
+            console.log("Receiver now");
+            // get contract address
+            contractAddress = json.readAddress(".Periphery.Receiver");
+            console.log("contractAddress: ", contractAddress);
+
+            // transfer ownership to refund wallet
+            // TODO: write script to confirm ownership transfers by wallet
+            console.log("before");
+            TransferrableOwnership(contractAddress).transferOwnership(refundWalletAddress);
+            console.log("after");
+        }
+
+        // ------- RelayerCelerIM
+        if (PeripheryRegistryFacet(diamond).getPeripheryContract("RelayerCelerIM") != address(0)) {
+            console.log("RelayerCelerIM now");
+            // get contract address
+            contractAddress = json.readAddress(".Periphery.RelayerCelerIM");
+            console.log("contractAddress: ", contractAddress);
+
+            // transfer ownership to refund wallet
+            // TODO: write script to confirm ownership transfers by wallet
+            console.log("before");
+            TransferrableOwnership(contractAddress).transferOwnership(refundWalletAddress);
+            console.log("after");
+        }
+
+        // ------- ServiceFeeCollector
+        if (PeripheryRegistryFacet(diamond).getPeripheryContract("ServiceFeeCollector") != address(0)) {
+            console.log("ServiceFeeCollector now");
+            // get contract address
+            contractAddress = json.readAddress(".Periphery.ServiceFeeCollector");
+            console.log("contractAddress: ", contractAddress);
+
+            // transfer ownership to withdraw wallet
+            // TODO: write script to confirm ownership transfers by wallet
+            console.log("before");
+            TransferrableOwnership(contractAddress).transferOwnership(withdrawWalletAddress);
+            console.log("after");
+        }
+
+        vm.stopBroadcast();
+    }
+}
