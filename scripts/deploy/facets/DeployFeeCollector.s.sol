5be1820aa7e43ce375c006b6967f93cd17ddf550
Daniel Bläcker <d.blaecker@gmail.com>
2023-06-08 09:17:14 +0200

    changed ownership of FeeCollector contract

--- scripts/deploy/facets/DeployFeeCollector.s.sol
+++ scripts/deploy/facets/DeployFeeCollector.s.sol
@@ -3,17 +3,33 @@ pragma solidity ^0.8.17;
 
 import { DeployScriptBase } from "./utils/DeployScriptBase.sol";
 import { FeeCollector } from "lifi/Periphery/FeeCollector.sol";
+import { stdJson } from "forge-std/Script.sol";
 
 contract DeployScript is DeployScriptBase {
+    using stdJson for string;
+    string internal globalConfigPath;
+    string internal globalConfigJson;
+
     constructor() DeployScriptBase("FeeCollector") {}
 
     function run()
         public
         returns (FeeCollector deployed, bytes memory constructorArgs)
     {
+        // get path of global config file
+        globalConfigPath = string.concat(root, "/config/global.json");
+
+        // read file into json variable
+        globalConfigJson = vm.readFile(globalConfigPath);
+
+        // extract refundWallet address
+        address withdrawWalletAddress = globalConfigJson.readAddress(
+            ".withdrawWallet"
+        );
+
         vm.startBroadcast(deployerPrivateKey);
 
-        constructorArgs = abi.encode(deployerAddress);
+        constructorArgs = abi.encode(withdrawWalletAddress);
 
         if (isDeployed()) {
             return (FeeCollector(predicted), constructorArgs);
