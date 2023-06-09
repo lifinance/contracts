969a5cd1ba3640ffdbe72aacf7c8f9ae3c54c5cd
Max Klenk <max@klenk.biz>
2023-06-09 21:56:43 +0200

    feat: update approved token in hopFacetOptimized inside the diamond

---
+++ scripts/tasks/solidity/UpdateHopFacetOptimizedToken.s.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import { UpdateScriptBase } from "../../deploy/facets/utils/UpdateScriptBase.sol";
+import { stdJson } from "forge-std/StdJson.sol";
+import { DiamondCutFacet, IDiamondCut } from "lifi/Facets/DiamondCutFacet.sol";
+import { HopFacetOptimized } from "lifi/Facets/HopFacetOptimized.sol";
+
+contract DeployScript is UpdateScriptBase {
+    using stdJson for string;
+
+    struct Approval {
+        address a_tokenAddress;
+        address b_contractAddress;
+        string c_tokenName;
+        string d_contractName;
+    }
+
+    address[] internal contractAddresses;
+    address[] internal tokenAddresses;
+
+    function run() public returns (address[] memory facets) {
+        address facet = json.readAddress(".LiFiDiamond"); // HopFacetOptimized is registered in the Diamond
+
+        // load config
+        path = string.concat(root, "/config/hop.json");
+        json = vm.readFile(path);
+        bytes memory rawApprovals = json.parseRaw(
+            string.concat(".", network, ".approvals")
+        );
+        Approval[] memory approvals = abi.decode(rawApprovals, (Approval[]));
+
+        // Loop through all items and split them in arrays
+        for (uint256 i = 0; i < approvals.length; i++) {
+            contractAddresses.push(approvals[i].b_contractAddress);
+            tokenAddresses.push(approvals[i].a_tokenAddress);
+        }
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Call Facet directly to update standalone version
+        HopFacetOptimized(facet).setApprovalForBridges(
+            contractAddresses,
+            tokenAddresses
+        );
+
+        facets = loupe.facetAddresses();
+
+        vm.stopBroadcast();
+    }
+}
