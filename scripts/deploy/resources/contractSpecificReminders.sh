5aabad9d3a53d047b5765c9254250ad87dbe68a3
Daniel Bläcker <d.blaecker@gmail.com>
2023-06-15 15:06:54 +0200

    fixed logging for celerIM / relayerCelerIM

--- scripts/deploy/resources/contractSpecificReminders.sh
+++ scripts/deploy/resources/contractSpecificReminders.sh
@@ -3,7 +3,5 @@
 # This file contains contract-specific information to keep in mind when deploying this contract
 # e.g. precondition requirements (contract XY must be deployed before, etc.)
 
-
-CelerIMFacet="RelayerCelerIM needs to be deployed prior to deploying this facet (CelerIMFacet)"
-
-RelayerCelerIM="Please check after deployment that the RelayerCelerIM contract contains the correct diamond address"
+# EXAMPLE:
+# CelerIMFacet="RelayerCelerIM needs to be deployed prior to deploying this facet (CelerIMFacet)"
