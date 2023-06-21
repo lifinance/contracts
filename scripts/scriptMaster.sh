d839fc073326b32ced1570d82ac6afe5cae09e5c
Daniel Bläcker <d.blaecker@gmail.com>
2023-06-21 12:39:42 +0200

    some adjustments and target state staging entries removed

--- scripts/scriptMaster.sh
+++ scripts/scriptMaster.sh
@@ -99,7 +99,7 @@ scriptMaster() {
       "6) Batch update _targetState.json file" \
       "7) Verify all unverified contracts" \
       "8) Review deploy status (vs. target state)" \
-      "9) Create updated target state from Google Docs (updates PRODUCTION only)" \
+      "9) Create updated target state from Google Docs (updates STAGING or PRODUCTION depending on your settings)" \
       "10) Update all diamond log files" \
       "11) Propose upgrade TX to Gnosis SAFE"
   )
