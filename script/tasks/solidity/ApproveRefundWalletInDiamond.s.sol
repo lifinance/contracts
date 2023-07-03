33a6fc88c0f8072f11eef002212b75b575a8f915
Daniel Bläcker <d.blaecker@gmail.com>
2023-07-03 15:56:46 +0200

    review comments implemented

--- script/tasks/solidity/ApproveRefundWalletInDiamond.s.sol
+++ script/tasks/solidity/ApproveRefundWalletInDiamond.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import { UpdateScriptBase, console } from "../../deploy/facets/utils/UpdateScriptBase.sol";
+import { UpdateScriptBase } from "../../deploy/facets/utils/UpdateScriptBase.sol";
 import { stdJson } from "forge-std/StdJson.sol";
 
 contract DeployScript is UpdateScriptBase {
