ff27e384bc9d440ee1256848a9116368e1cdd838
Satoshi Naoki <satoshi@li.finance>
2023-07-11 22:47:27 +0900

    update hardhat configuration with zksync

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,14 +1,20 @@
 import 'dotenv/config'
 import fs from 'fs'
+import path from 'path'
+import '@matterlabs/hardhat-zksync-solc'
+import '@matterlabs/hardhat-zksync-deploy'
+import '@matterlabs/hardhat-zksync-verify'
 import '@nomiclabs/hardhat-ethers'
 import '@nomiclabs/hardhat-etherscan'
-import { HardhatUserConfig } from 'hardhat/types'
 import '@typechain/hardhat'
+import 'hardhat-deploy'
 import 'hardhat-preprocessor'
-import path from 'path'
 import { node_url, accounts } from './utils/network'
+import { HardhatUserConfig } from 'hardhat/types'
 
 require('./tasks/generateDiamondABI.ts')
+
+const PKEY = process.env.PRIVATE_KEY || null
 
 function getRemappings() {
   return fs
@@ -32,34 +38,62 @@ const config: HardhatUserConfig = {
       },
     ],
   },
+  zksolc: {
+    version: '1.3.5',
+    compilerSource: 'binary',
+    settings: {},
+  },
   namedAccounts: {
     deployer: 0,
     simpleERC20Beneficiary: 1,
   },
   networks: {
     hardhat: {
-      chainId: 1337,
-      initialBaseFeePerGas: 0, // to fix : https://github.com/sc-forks/solidity-coverage/issues/652, see https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136
-      // process.env.HARDHAT_FORK will specify the network that the fork is made from.
-      // this line ensure the use of the corresponding accounts
-      accounts: accounts(process.env.HARDHAT_FORK),
-      forking: process.env.HARDHAT_FORK
-        ? {
-            // TODO once PR merged : network: process.env.HARDHAT_FORK,
-            url: node_url(process.env.HARDHAT_FORK),
-            blockNumber: process.env.HARDHAT_FORK_NUMBER
-              ? parseInt(process.env.HARDHAT_FORK_NUMBER)
-              : undefined,
-          }
-        : undefined,
+      zksync: true,
     },
+    goerli: {
+      url: node_url('goerli'),
+    },
+    zksyncGoerli: {
+      url: node_url('zksync_goerli'),
+      accounts: PKEY ? [PKEY] : accounts(),
+      chainId: 280,
+      companionNetworks: {
+        hub: 'goerli',
+      },
+      zksync: true,
+      ethNetwork: 'goerli',
+      verifyURL:
+        'https://zksync2-testnet-explorer.zksync.dev/contract_verification',
+      verify: {
+        etherscan: {
+          apiKey: process.env.ETHERSCAN_API_KEY!,
+          apiUrl: 'https://zksync2-testnet.zkscan.io',
+        },
+      },
+    },
+  },
+  etherscan: {
+    apiKey: {
+      goerli: process.env.ETHERSCAN_API_KEY!,
+    },
+    customChains: [
+      {
+        network: 'zksyncGoerli',
+        chainId: 280,
+        urls: {
+          apiURL: 'https://zksync2-testnet.zkscan.io/api',
+          browserURL: 'https://zksync2-testnet.zkscan.io',
+        },
+      },
+    ],
   },
   preprocess: {
     eachLine: (hre) => ({
       transform: (line: string, sourceInfo: { absolutePath: string }) => {
         if (line.match(/^\s*import /i)) {
           for (const [from, to] of getRemappings()) {
-            if (line.includes(from)) {
+            if (line.includes(from) && !to.includes('node_modules')) {
               line = line.replace(
                 from,
                 `${path
