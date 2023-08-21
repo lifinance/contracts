688928f204f5c84a38670442cdc128a8ca8e9615
Ed Zynda <ezynda3@gmail.com>
2023-08-21 19:02:19 +0300

    Remove unused items from deploys script

--- script/deploy/facets/DeployStandardizedCallFacet.s.sol
+++ script/deploy/facets/DeployStandardizedCallFacet.s.sol
@@ -2,47 +2,21 @@
 pragma solidity ^0.8.17;
 
 import { DeployScriptBase } from "./utils/DeployScriptBase.sol";
-import { stdJson } from "forge-std/Script.sol";
 import { StandardizedCallFacet } from "lifi/Facets/StandardizedCallFacet.sol";
 
 contract DeployScript is DeployScriptBase {
-    using stdJson for string;
-
     constructor() DeployScriptBase("StandardizedCallFacetFacet") {}
 
-    function run()
-        public
-        returns (StandardizedCallFacet deployed, bytes memory constructorArgs)
-    {
-        string memory path = string.concat(
-            vm.projectRoot(),
-            "/config/standardizedCallFacet.json"
-        );
-        string memory json = vm.readFile(path);
-        address example = json.readAddress(
-            string.concat(".", network, ".example")
-        );
-
-        constructorArgs = abi.encode(example);
-
+    function run() public returns (StandardizedCallFacet deployed) {
         vm.startBroadcast(deployerPrivateKey);
 
         if (isDeployed()) {
-            return (
-                StandardizedCallFacet(payable(predicted)),
-                constructorArgs
-            );
+            return (StandardizedCallFacet(payable(predicted)));
         }
 
         deployed = StandardizedCallFacet(
             payable(
-                factory.deploy(
-                    salt,
-                    bytes.concat(
-                        type(StandardizedCallFacet).creationCode,
-                        constructorArgs
-                    )
-                )
+                factory.deploy(salt, type(StandardizedCallFacet).creationCode)
             )
         );
 
