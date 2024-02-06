79a5b2d50f6a80571591cda2f0390feef769a0fe
Daniel Bläcker <d.blaecker@gmail.com>
2024-02-06 16:23:02 +0700

    feat: updates Across config file with values generated by script incl tokensToApprove

---
+++ script/deploy/facets/UpdateAcrossFacetPacked.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import { UpdateScriptBase } from "./utils/UpdateScriptBase.sol";
+import { stdJson } from "forge-std/StdJson.sol";
+import { AcrossFacetPacked } from "lifi/Facets/AcrossFacetPacked.sol";
+
+contract DeployScript is UpdateScriptBase {
+    using stdJson for string;
+
+    function run()
+        public
+        returns (address[] memory facets, bytes memory cutData)
+    {
+        return update("AcrossFacetPacked");
+    }
+
+    function getExcludes() internal view override returns (bytes4[] memory) {
+        AcrossFacetPacked across;
+        bytes4[] memory excludes = new bytes4[](6);
+        excludes[0] = across.cancelOwnershipTransfer.selector;
+        excludes[1] = across.transferOwnership.selector;
+        excludes[2] = across.confirmOwnershipTransfer.selector;
+        excludes[3] = across.owner.selector;
+        excludes[4] = across.pendingOwner.selector;
+        excludes[5] = across.setApprovalForBridge.selector;
+
+        return excludes;
+    }
+}
---
+++ test/solidity/Facets/AcrossFacetPacked.t.sol
@@ -0,0 +1,549 @@
+// // SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "ds-test/test.sol";
+import { AcrossFacet } from "lifi/Facets/AcrossFacet.sol";
+import { AcrossFacetPacked } from "lifi/Facets/AcrossFacetPacked.sol";
+import { IAcrossSpokePool } from "lifi/Interfaces/IAcrossSpokePool.sol";
+import { LibAsset, IERC20 } from "lifi/Libraries/LibAsset.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { TestBase } from "../utils/TestBase.sol";
+import { ERC20 } from "solmate/tokens/ERC20.sol";
+import { LiFiDiamond } from "../utils/DiamondTest.sol";
+
+contract AcrossFacetPackedTest is TestBase {
+    using SafeERC20 for IERC20;
+
+    address internal constant ACROSS_SPOKE_POOL =
+        0x5c7BCd6E7De5423a257D81B442095A1a6ced35C5;
+    address internal ADDRESS_USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+
+    IAcrossSpokePool internal across;
+    ERC20 internal usdt;
+    AcrossFacetPacked internal acrossFacetPacked;
+    AcrossFacetPacked internal acrossStandAlone;
+    AcrossFacet.AcrossData internal validAcrossData;
+
+    bytes32 transactionId;
+    uint64 destinationChainId;
+
+    uint256 amountNative;
+    bytes packedNativeCalldata;
+
+    uint256 amountUSDT;
+    bytes packedUSDTCalldata;
+
+    uint256 amountUSDC;
+    bytes packedUSDCCalldata;
+
+    event LiFiAcrossTransfer(bytes8 _transactionId);
+
+    function setUp() public {
+        customBlockNumberForForking = 19145375;
+
+        initTestBase();
+
+        usdt = ERC20(ADDRESS_USDT);
+
+        /// Prepare AcrossFacetPacked (as facet & as standalone contract to test both modes)
+        diamond = createDiamond();
+        across = IAcrossSpokePool(ACROSS_SPOKE_POOL);
+        acrossFacetPacked = new AcrossFacetPacked(
+            across,
+            ADDRESS_WETH,
+            address(this)
+        );
+        acrossStandAlone = new AcrossFacetPacked(
+            across,
+            ADDRESS_WETH,
+            address(this)
+        );
+
+        bytes4[] memory functionSelectors = new bytes4[](9);
+        functionSelectors[0] = acrossFacetPacked.setApprovalForBridge.selector;
+        functionSelectors[1] = acrossFacetPacked
+            .startBridgeTokensViaAcrossNativePacked
+            .selector;
+        functionSelectors[2] = acrossFacetPacked
+            .startBridgeTokensViaAcrossNativeMin
+            .selector;
+        functionSelectors[3] = acrossFacetPacked
+            .startBridgeTokensViaAcrossERC20Packed
+            .selector;
+        functionSelectors[4] = acrossFacetPacked
+            .startBridgeTokensViaAcrossERC20Min
+            .selector;
+        functionSelectors[5] = acrossFacetPacked
+            .encode_startBridgeTokensViaAcrossNativePacked
+            .selector;
+        functionSelectors[6] = acrossFacetPacked
+            .encode_startBridgeTokensViaAcrossERC20Packed
+            .selector;
+        functionSelectors[7] = acrossFacetPacked
+            .decode_startBridgeTokensViaAcrossNativePacked
+            .selector;
+        functionSelectors[8] = acrossFacetPacked
+            .decode_startBridgeTokensViaAcrossERC20Packed
+            .selector;
+
+        // add facet to diamond
+        addFacet(diamond, address(acrossFacetPacked), functionSelectors);
+        acrossFacetPacked = AcrossFacetPacked(payable(address(diamond)));
+
+        /// Prepare parameters
+        transactionId = "someID";
+        destinationChainId = 137;
+
+        // define valid AcrossData
+        validAcrossData = AcrossFacet.AcrossData({
+            relayerFeePct: 0,
+            quoteTimestamp: uint32(block.timestamp),
+            message: "",
+            maxCount: type(uint256).max
+        });
+
+        vm.label(ACROSS_SPOKE_POOL, "SpokePool");
+        vm.label(ADDRESS_USDT, "USDT_TOKEN");
+
+        // Native params
+        amountNative = 1 ether;
+        packedNativeCalldata = acrossFacetPacked
+            .encode_startBridgeTokensViaAcrossNativePacked(
+                transactionId,
+                USER_RECEIVER,
+                destinationChainId,
+                validAcrossData.relayerFeePct,
+                validAcrossData.quoteTimestamp,
+                validAcrossData.maxCount,
+                validAcrossData.message
+            );
+
+        // usdt params
+        amountUSDT = 100 * 10 ** usdt.decimals();
+        packedUSDTCalldata = acrossFacetPacked
+            .encode_startBridgeTokensViaAcrossERC20Packed(
+                transactionId,
+                USER_RECEIVER,
+                ADDRESS_USDT,
+                amountUSDT,
+                destinationChainId,
+                validAcrossData.relayerFeePct,
+                validAcrossData.quoteTimestamp,
+                validAcrossData.message,
+                validAcrossData.maxCount
+            );
+        deal(ADDRESS_USDT, USER_SENDER, amountUSDT);
+
+        // usdc params
+        amountUSDC = 100 * 10 ** usdc.decimals();
+        packedUSDCCalldata = acrossFacetPacked
+            .encode_startBridgeTokensViaAcrossERC20Packed(
+                transactionId,
+                USER_RECEIVER,
+                ADDRESS_USDC,
+                amountUSDC,
+                destinationChainId,
+                validAcrossData.relayerFeePct,
+                validAcrossData.quoteTimestamp,
+                validAcrossData.message,
+                validAcrossData.maxCount
+            );
+
+        // Prepare approvals
+        address[] memory tokens = new address[](2);
+        tokens[0] = ADDRESS_USDT;
+        tokens[1] = ADDRESS_USDC;
+
+        // set token approvals for standalone contract via admin function
+        acrossStandAlone.setApprovalForBridge(tokens);
+
+        // set token approvals for facet via cheatcode (in production we will do this via script)
+        vm.startPrank(address(acrossFacetPacked));
+        LibAsset.maxApproveERC20(
+            IERC20(ADDRESS_USDT),
+            ACROSS_SPOKE_POOL,
+            type(uint256).max
+        );
+        usdc.approve(ACROSS_SPOKE_POOL, type(uint256).max);
+        vm.stopPrank();
+    }
+
+    function test_canBridgeNativeTokensViaPackedFunction_Facet() public {
+        vm.startPrank(USER_SENDER);
+        // check that event is emitted correctly
+        vm.expectEmit(true, true, true, true, address(diamond));
+        emit LiFiAcrossTransfer(bytes8(transactionId));
+
+        // call facet through diamond
+        (bool success, ) = address(diamond).call{ value: amountNative }(
+            packedNativeCalldata
+        );
+        if (!success) {
+            revert();
+        }
+        vm.stopPrank();
+    }
+
+    function test_canBridgeNativeTokensViaPackedFunction_Standalone() public {
+        vm.startPrank(USER_SENDER);
+        // check that event is emitted correctly
+        vm.expectEmit(true, true, true, true, address(acrossStandAlone));
+        emit LiFiAcrossTransfer(bytes8(transactionId));
+
+        // call facet through diamond
+        (bool success, ) = address(acrossStandAlone).call{
+            value: amountNative
+        }(packedNativeCalldata);
+        if (!success) {
+            revert();
+        }
+        vm.stopPrank();
+    }
+
+    function test_canBridgeNativeTokensViaMinFunction_Facet() public {
+        vm.startPrank(USER_SENDER);
+        // check that event is emitted correctly
+        vm.expectEmit(true, true, true, true, address(diamond));
+        emit LiFiAcrossTransfer(bytes8(transactionId));
+
+        // call facet through diamond
+        acrossFacetPacked.startBridgeTokensViaAcrossNativeMin{
+            value: amountNative
+        }(
+            transactionId,
+            USER_RECEIVER,
+            destinationChainId,
+            validAcrossData.relayerFeePct,
+            validAcrossData.quoteTimestamp,
+            validAcrossData.message,
+            validAcrossData.maxCount
+        );
+
+        vm.stopPrank();
+    }
+
+    function test_canBridgeNativeTokensViaMinFunction_Standalone() public {
+        vm.startPrank(USER_SENDER);
+        // check that event is emitted correctly
+        vm.expectEmit(true, true, true, true, address(acrossStandAlone));
+        emit LiFiAcrossTransfer(bytes8(transactionId));
+
+        // call facet through diamond
+        acrossStandAlone.startBridgeTokensViaAcrossNativeMin{
+            value: amountNative
+        }(
+            transactionId,
+            USER_RECEIVER,
+            destinationChainId,
+            validAcrossData.relayerFeePct,
+            validAcrossData.quoteTimestamp,
+            validAcrossData.message,
+            validAcrossData.maxCount
+        );
+
+        vm.stopPrank();
+    }
+
+    function test_canBridgeERC20TokensViaPackedFunction_Facet_USDC() public {
+        vm.startPrank(USER_SENDER);
+
+        // approve diamond to spend sender's tokens
+        IERC20(ADDRESS_USDC).safeApprove(address(diamond), amountUSDC);
+
+        // check that event is emitted correctly
+        vm.expectEmit(true, true, true, true, address(diamond));
+        emit LiFiAcrossTransfer(bytes8(transactionId));
+
+        // call facet through diamond
+        (bool success, ) = address(diamond).call(packedUSDCCalldata);
+        if (!success) {
+            revert();
+        }
+        vm.stopPrank();
+    }
+
+    function test_canBridgeERC20TokensViaPackedFunction_Facet_USDT() public {
+        vm.startPrank(USER_SENDER);
+
+        // approve diamond to spend sender's tokens
+        IERC20(ADDRESS_USDT).safeApprove(address(diamond), amountUSDT * 100);
+
+        // check that event is emitted correctly
+        vm.expectEmit(true, true, true, true, address(diamond));
+        emit LiFiAcrossTransfer(bytes8(transactionId));
+
+        // call facet through diamond
+        (bool success, ) = address(diamond).call(packedUSDTCalldata);
+        if (!success) {
+            revert();
+        }
+        vm.stopPrank();
+    }
+
+    function test_canBridgeERC20TokensViaPackedFunction_Standalone_USDC()
+        public
+    {
+        vm.startPrank(USER_SENDER);
+
+        // approve diamond to spend sender's tokens
+        IERC20(ADDRESS_USDC).safeApprove(
+            address(acrossStandAlone),
+            amountUSDC
+        );
+
+        // check that event is emitted correctly
+        vm.expectEmit(true, true, true, true, address(acrossStandAlone));
+        emit LiFiAcrossTransfer(bytes8(transactionId));
+
+        // call facet through diamond
+        (bool success, ) = address(acrossStandAlone).call(packedUSDCCalldata);
+        if (!success) {
+            revert();
+        }
+        vm.stopPrank();
+    }
+
+    function test_canBridgeERC20TokensViaPackedFunction_Standalone_USDT()
+        public
+    {
+        vm.startPrank(USER_SENDER);
+
+        // approve diamond to spend sender's tokens
+        IERC20(ADDRESS_USDT).safeApprove(
+            address(acrossStandAlone),
+            amountUSDT
+        );
+        // usdt.approve(address(diamond), amountUSDT);
+
+        // check that event is emitted correctly
+        vm.expectEmit(true, true, true, true, address(acrossStandAlone));
+        emit LiFiAcrossTransfer(bytes8(transactionId));
+
+        // call facet through diamond
+        (bool success, ) = address(acrossStandAlone).call(packedUSDTCalldata);
+        if (!success) {
+            revert();
+        }
+        vm.stopPrank();
+    }
+
+    function test_canBridgeERC20TokensViaMinFunction_Facet_USDC() public {
+        vm.startPrank(USER_SENDER);
+
+        // approve diamond to spend sender's tokens
+        usdc.approve(address(diamond), amountUSDC);
+
+        // check that event is emitted correctly
+        vm.expectEmit(true, true, true, true, address(diamond));
+        emit LiFiAcrossTransfer(bytes8(transactionId));
+
+        // call facet through diamond
+        acrossFacetPacked.startBridgeTokensViaAcrossERC20Min(
+            transactionId,
+            ADDRESS_USDC,
+            amountUSDC,
+            USER_RECEIVER,
+            destinationChainId,
+            validAcrossData.relayerFeePct,
+            validAcrossData.quoteTimestamp,
+            validAcrossData.message,
+            validAcrossData.maxCount
+        );
+
+        vm.stopPrank();
+    }
+
+    function test_canBridgeERC20TokensViaMinFunction_Facet_USDT() public {
+        vm.startPrank(USER_SENDER);
+
+        // approve diamond to spend sender's tokens
+        IERC20(ADDRESS_USDT).safeApprove(address(diamond), amountUSDT);
+
+        // check that event is emitted correctly
+        vm.expectEmit(true, true, true, true, address(diamond));
+        emit LiFiAcrossTransfer(bytes8(transactionId));
+
+        // call facet through diamond
+        acrossFacetPacked.startBridgeTokensViaAcrossERC20Min(
+            transactionId,
+            ADDRESS_USDT,
+            amountUSDT,
+            USER_RECEIVER,
+            destinationChainId,
+            validAcrossData.relayerFeePct,
+            validAcrossData.quoteTimestamp,
+            validAcrossData.message,
+            validAcrossData.maxCount
+        );
+
+        vm.stopPrank();
+    }
+
+    function test_canBridgeERC20TokensViaMinFunction_Standalone_USDC() public {
+        vm.startPrank(USER_SENDER);
+
+        // approve diamond to spend sender's tokens
+        usdc.approve(address(acrossStandAlone), amountUSDC);
+
+        // check that event is emitted correctly
+        vm.expectEmit(true, true, true, true, address(acrossStandAlone));
+        emit LiFiAcrossTransfer(bytes8(transactionId));
+
+        // call facet through diamond
+        acrossStandAlone.startBridgeTokensViaAcrossERC20Min(
+            transactionId,
+            ADDRESS_USDC,
+            amountUSDC,
+            USER_RECEIVER,
+            destinationChainId,
+            validAcrossData.relayerFeePct,
+            validAcrossData.quoteTimestamp,
+            validAcrossData.message,
+            validAcrossData.maxCount
+        );
+
+        vm.stopPrank();
+    }
+
+    function test_canBridgeERC20TokensViaMinFunction_Standalone_USDT() public {
+        vm.startPrank(USER_SENDER);
+
+        // approve diamond to spend sender's tokens
+        IERC20(ADDRESS_USDT).safeApprove(
+            address(acrossStandAlone),
+            amountUSDT
+        );
+
+        // check that event is emitted correctly
+        vm.expectEmit(true, true, true, true, address(acrossStandAlone));
+        emit LiFiAcrossTransfer(bytes8(transactionId));
+
+        // call facet through diamond
+        acrossStandAlone.startBridgeTokensViaAcrossERC20Min(
+            transactionId,
+            ADDRESS_USDT,
+            amountUSDT,
+            USER_RECEIVER,
+            destinationChainId,
+            validAcrossData.relayerFeePct,
+            validAcrossData.quoteTimestamp,
+            validAcrossData.message,
+            validAcrossData.maxCount
+        );
+
+        vm.stopPrank();
+    }
+
+    function assertEqAcrossData(
+        AcrossFacet.AcrossData memory original,
+        AcrossFacet.AcrossData memory decoded
+    ) public {
+        assertEq(original.relayerFeePct == decoded.relayerFeePct, true);
+        assertEq(original.quoteTimestamp == decoded.quoteTimestamp, true);
+        assertEq(
+            keccak256(abi.encode(original.message)) ==
+                keccak256(abi.encode(decoded.message)),
+            true
+        );
+        assertEq(original.relayerFeePct == decoded.relayerFeePct, true);
+    }
+
+    function assertEqBridgeData(BridgeData memory original) public {
+        assertEq(original.transactionId == transactionId, true);
+        assertEq(original.receiver == USER_RECEIVER, true);
+        assertEq(original.destinationChainId == destinationChainId, true);
+    }
+
+    function test_canEncodeAndDecodeNativePackedCalldata() public {
+        (
+            BridgeData memory bridgeData,
+            AcrossFacet.AcrossData memory acrossData
+        ) = acrossFacetPacked.decode_startBridgeTokensViaAcrossNativePacked(
+                packedNativeCalldata
+            );
+
+        // validate bridgeData
+        assertEqBridgeData(bridgeData);
+
+        // validate acrossData
+        assertEqAcrossData(validAcrossData, acrossData);
+    }
+
+    function test_canEncodeAndDecodeERC20PackedCalldata() public {
+        (
+            BridgeData memory bridgeData,
+            AcrossFacet.AcrossData memory acrossData
+        ) = acrossFacetPacked.decode_startBridgeTokensViaAcrossERC20Packed(
+                packedUSDCCalldata
+            );
+
+        // validate bridgeData
+        assertEqBridgeData(bridgeData);
+        assertEq(bridgeData.minAmount == amountUSDC, true);
+        assertEq(bridgeData.sendingAssetId == ADDRESS_USDC, true);
+
+        // validate acrossData
+        assertEqAcrossData(validAcrossData, acrossData);
+    }
+
+    function test_revert_cannotEncodeDestinationChainIdAboveUint32Max_Native()
+        public
+    {
+        uint64 invalidDestinationChainId = uint64(type(uint32).max) + 1;
+
+        vm.expectRevert(
+            "destinationChainId value passed too big to fit in uint32"
+        );
+
+        acrossFacetPacked.encode_startBridgeTokensViaAcrossNativePacked(
+            transactionId,
+            USER_RECEIVER,
+            invalidDestinationChainId,
+            validAcrossData.relayerFeePct,
+            validAcrossData.quoteTimestamp,
+            validAcrossData.maxCount,
+            validAcrossData.message
+        );
+    }
+
+    function test_revert_cannotEncodeDestinationChainIdAboveUint32Max_ERC20()
+        public
+    {
+        uint64 invalidDestinationChainId = uint64(type(uint32).max) + 1;
+
+        // USDC
+        vm.expectRevert(
+            "destinationChainId value passed too big to fit in uint32"
+        );
+
+        acrossFacetPacked.encode_startBridgeTokensViaAcrossERC20Packed(
+            transactionId,
+            USER_RECEIVER,
+            ADDRESS_USDC,
+            amountUSDC,
+            invalidDestinationChainId,
+            validAcrossData.relayerFeePct,
+            validAcrossData.quoteTimestamp,
+            validAcrossData.message,
+            validAcrossData.maxCount
+        );
+    }
+
+    function test_revert_cannotUseMinAmountAboveUint128Max_ERC20() public {
+        uint256 invalidMinAmount = uint256(type(uint128).max) + 1;
+
+        vm.expectRevert("minAmount value passed too big to fit in uint128");
+
+        acrossFacetPacked.encode_startBridgeTokensViaAcrossERC20Packed(
+            transactionId,
+            USER_RECEIVER,
+            ADDRESS_USDT,
+            invalidMinAmount,
+            destinationChainId,
+            validAcrossData.relayerFeePct,
+            validAcrossData.quoteTimestamp,
+            validAcrossData.message,
+            validAcrossData.maxCount
+        );
+    }
+}
