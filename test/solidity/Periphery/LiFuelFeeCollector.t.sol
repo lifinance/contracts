db01cb730626b9e69cb52958276083c4d990722c
Ed Zynda <ezynda3@gmail.com>
2024-02-12 15:33:13 +0300

    Revert "Update fee collectors ServiceFeeCollector v1.0.1, LiFuelFeeCollector v1.0.1, LIFuelFacet v1.0.1 (#585)"
    This reverts commit 69f3337773a14d463a8aee0ebb83e27b36152d78.

--- test/solidity/Periphery/LiFuelFeeCollector.t.sol
+++ test/solidity/Periphery/LiFuelFeeCollector.t.sol
@@ -50,11 +50,7 @@ contract LiFuelFeeCollectorTest is DSTest {
         uint256 fee = 0.015 ether;
 
         // Act
-        feeCollector.collectNativeGasFees{ value: fee }(
-            fee,
-            137,
-            address(0xb33f)
-        );
+        feeCollector.collectNativeGasFees{ value: fee }(137, address(0xb33f));
 
         // Assert
         assert(address(feeCollector).balance == fee);
@@ -92,11 +88,7 @@ contract LiFuelFeeCollectorTest is DSTest {
             137,
             address(0xb33f)
         );
-        feeCollector.collectNativeGasFees{ value: fee }(
-            fee,
-            137,
-            address(0xb33f)
-        );
+        feeCollector.collectNativeGasFees{ value: fee }(137, address(0xb33f));
         uint256 startingTokenBalance = feeToken.balanceOf(address(this));
         uint256 startingETHBalance = address(this).balance;
 
