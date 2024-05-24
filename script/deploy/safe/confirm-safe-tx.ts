328d1d38099290b388b4053d364b7ce20456bde3
Ed Zynda <ezynda3@users.noreply.github.com>
2024-05-24 12:33:38 +0300

    Add convenience scripts for dealing with SAFE transactions (#645)
    * Upgrade gnosis safe libs
    
    * Fix API urls
    
    * add safe addresses
    
    * add optional rpcUrl
    
    * rename
    
    * implement confirm transactions on safe
    
    * refactor
    
    * add method for adding owners to a safe
    
    * fix typo
    
    * update deps
    
    * add scripts to package.json
    
    * revert
    
    * add mantle
    
    * use alias for ethers v6
    
    * add blast safe details

---
+++ script/deploy/safe/confirm-safe-tx.ts
@@ -0,0 +1,141 @@
+import { defineCommand, runMain } from 'citty'
+import { type SafeApiKitConfig } from '@safe-global/api-kit'
+import { Abi, Chain, Hex, decodeFunctionData, parseAbi } from 'viem'
+import Safe, { EthersAdapter } from '@safe-global/protocol-kit'
+import SafeApiKit from '@safe-global/api-kit'
+import { ethers } from 'ethers6'
+import consola from 'consola'
+import * as chains from 'viem/chains'
+import { safeAddresses, safeApiUrls } from './config'
+
+const ABI_LOOKUP_URL = `https://api.openchain.xyz/signature-database/v1/lookup?function=%SELECTOR%&filter=true`
+
+const chainMap: Record<string, Chain> = {}
+for (const [k, v] of Object.entries(chains)) {
+  // @ts-ignore
+  chainMap[k] = v
+}
+
+const main = defineCommand({
+  meta: {
+    name: 'propose-to-safe',
+    description: 'Propose a transaction to a Gnosis Safe',
+  },
+  args: {
+    network: {
+      type: 'string',
+      description: 'Network name',
+      required: true,
+    },
+    rpcUrl: {
+      type: 'string',
+      description: 'RPC URL',
+    },
+    privateKey: {
+      type: 'string',
+      description: 'Private key of the signer',
+      required: true,
+    },
+  },
+  async run({ args }) {
+    const chain: Chain = chainMap[args.network]
+
+    const config: SafeApiKitConfig = {
+      chainId: BigInt(chain.id),
+      txServiceUrl: safeApiUrls[args.network.toLowerCase()],
+    }
+
+    const safeService = new SafeApiKit(config)
+
+    const safeAddress = safeAddresses[args.network.toLowerCase()]
+
+    const rpcUrl = args.rpcUrl || chain.rpcUrls.default.http[0]
+    const provider = new ethers.JsonRpcProvider(rpcUrl)
+    const signer = new ethers.Wallet(args.privateKey, provider)
+
+    const ethAdapter = new EthersAdapter({
+      ethers,
+      signerOrProvider: signer,
+    })
+
+    const protocolKit = await Safe.create({
+      ethAdapter,
+      safeAddress: safeAddress,
+    })
+
+    const txs = await safeService.getPendingTransactions(safeAddress)
+    if (!txs.results.length) {
+      consola.success('No pending transactions')
+      return
+    }
+
+    for (const tx of txs.results.sort((a, b) => {
+      if (a.nonce < b.nonce) return -1
+      if (a.nonce > b.nonce) return 1
+      return 0
+    })) {
+      let abi
+      let abiInterface: Abi
+      let decoded
+      if (tx.data) {
+        const selector = tx.data.substring(0, 10)
+        const url = ABI_LOOKUP_URL.replace('%SELECTOR%', selector)
+        const response = await fetch(url)
+        const data = await response.json()
+        if (
+          data.ok &&
+          data.result &&
+          data.result.function &&
+          data.result.function[selector]
+        ) {
+          abi = data.result.function[selector][0].name
+          const fullAbiString = `function ${abi}`
+          abiInterface = parseAbi([fullAbiString])
+          decoded = decodeFunctionData({
+            abi: abiInterface,
+            data: tx.data as Hex,
+          })
+        }
+      }
+
+      consola.info('Method:', abi)
+      consola.info('Decoded Data:', JSON.stringify(decoded, null, 2))
+      consola.info('Nonce:', tx.nonce)
+      consola.info('To:', tx.to)
+      consola.info('Value:', tx.value)
+      consola.info('Data:', tx.data)
+      consola.info('Proposer:', tx.proposer)
+      consola.info('Safe Tx Hash:', tx.safeTxHash)
+
+      const ok = await consola.prompt('Confirm Transaction?', {
+        type: 'confirm',
+      })
+
+      if (!ok) {
+        continue
+      }
+
+      const action = await consola.prompt('Action', {
+        type: 'select',
+        options: ['Sign & Execute Later', 'Execute Now'],
+      })
+
+      const txToConfirm = await safeService.getTransaction(tx.safeTxHash)
+
+      if (action === 'Sign & Execute Later') {
+        consola.info('Signing transaction', tx.safeTxHash)
+        await protocolKit.signTransaction(txToConfirm)
+        consola.success('Transaction signed', tx.safeTxHash)
+      }
+
+      if (action === 'Execute Now') {
+        consola.info('Executing transaction', tx.safeTxHash)
+        const exec = await protocolKit.executeTransaction(txToConfirm)
+        await exec.transactionResponse?.wait()
+        consola.success('Transaction executed', tx.safeTxHash)
+      }
+    }
+  },
+})
+
+runMain(main)
