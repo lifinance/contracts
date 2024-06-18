ce401a8f15e5164a03ee7cdf3d7771b538b41e8c
Daniel Bläcker <d.blaecker@gmail.com>
2024-06-18 10:56:36 +0700

    removed unused import (audit issue #10)

--- src/Periphery/ReceiverStargateV2.sol
+++ src/Periphery/ReceiverStargateV2.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.17;
 
 import { IERC20, SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { ReentrancyGuard } from "../Helpers/ReentrancyGuard.sol";
 import { LibSwap } from "../Libraries/LibSwap.sol";
 import { LibAsset } from "../Libraries/LibAsset.sol";
 import { OFTComposeMsgCodec } from "../Libraries/OFTComposeMsgCodec.sol";
