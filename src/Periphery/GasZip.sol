aea3404bb398851b6ac70b59e43f5fdee80514ad
Daniela Chybisova <iron.melmel@gmail.com>
2024-06-25 16:28:03 +0200

    chore: remove unused errors

--- src/Periphery/GasZip.sol
+++ src/Periphery/GasZip.sol
@@ -23,10 +23,7 @@ contract GasZip {
     IGasZip public immutable gasZipRouter;
 
     /// Errors ///
-    error SwapFailed(address, address);
-    error GasZipFailed(uint256);
     error TransferFailed();
-    error InboundTokenDisallowed();
 
     /// Events ///
 
