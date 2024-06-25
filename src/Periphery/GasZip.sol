11f2132f3393f7c9fb9e86c25c6f080132539f3d
Daniela Chybisova <iron.melmel@gmail.com>
2024-06-25 14:24:41 +0200

    chore: import

--- src/Periphery/GasZip.sol
+++ src/Periphery/GasZip.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { LibAsset } from "../Libraries/LibAsset.sol";
 import { LibSwap } from "../Libraries/LibSwap.sol";
 import { TransferrableOwnership } from "../Helpers/TransferrableOwnership.sol";
