ff3000b9b15089b74ce134ee2679c915d3a2d8d7
Ed Zynda <ezynda3@gmail.com>
2024-07-04 17:23:32 +0300

    Deploy to staging on Arbitrum

---
+++ script/deploy/facets/DeployIntentFactory.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import { DeployScriptBase } from "./utils/DeployScriptBase.sol";
+import { IntentFactory } from "lifi/Periphery/IntentFactory.sol";
+import { Intent } from "lifi/Helpers/Intent.sol";
+
+contract DeployScript is DeployScriptBase {
+    Intent implementation;
+
+    constructor() DeployScriptBase("IntentFactory") {}
+
+    function run()
+        public
+        returns (IntentFactory deployed, bytes memory constructorArgs)
+    {
+        implementation = new Intent();
+        constructorArgs = getConstructorArgs();
+
+        deployed = IntentFactory(deploy(type(IntentFactory).creationCode));
+    }
+
+    function getConstructorArgs()
+        internal
+        view
+        override
+        returns (bytes memory)
+    {
+        return abi.encode(address(implementation));
+    }
+}
