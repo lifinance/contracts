bbc21edbf4f7b2ef785955486d65281327a61da4
Daniel Bläcker <d.blaecker@gmail.com>
2024-07-25 14:37:17 +0700

    test

--- script/tasks/diamondEMERGENCYPauseGitHub.sh
+++ script/tasks/diamondEMERGENCYPauseGitHub.sh
@@ -11,109 +11,107 @@ source ./script/helperFunctions.sh
 
 # Define function to handle each network operation
 function handleNetwork() {
+  echo ""
+  echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> start network $1 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
   local NETWORK=$1
 
-  if [[ $NETWORK == "bsc-testnet" ]]; then
-    echo "skipping bsc-testnet"
-    return 0
-  fi
+  # skip any non-prod networks
+  case "$NETWORK" in
+    "bsc-testnet" | "localanvil" | "sepolia" | "mumbai" | "lineatest")
+      echo "skipping $NETWORK"
+      return 0
+      ;;
+  esac
 
-  if [[ $NETWORK == "localanvil" ]]; then
-    echo "skipping localanvil"
-    return 0
-  fi
-  if [[ $NETWORK == "sepolia" ]]; then
-    echo "skipping sepolia"
-    return 0
-  fi
 
   echo "now starting with network $NETWORK"
-    DEPLOYER=$(cast wallet address "$PRIVATE_KEY_PAUSER_WALLET")
-  echo "DEPLOYER_ADDRESS1: $DEPLOYER_ADDRESS"
+  DEPLOYER=$(cast wallet address "$PRIVATE_KEY_PAUSER_WALLET")
+  echo "DEPLOYER_ADDRESS1: $DEPLOYER"
 
   # get RPC URL for given network
   RPC_KEY="ETH_NODE_URI_$(tr '[:lower:]' '[:upper:]' <<<"$NETWORK")"
   # RPC_URL="$ETH_NODE_URI_$(tr '[:lower:]' '[:upper:]' <<<"$NETWORK")"
   RPC_KEY_VAR_NAME=$(echo "$RPC_KEY" | tr '-' '_')  # Replace dashes with underscores in the variable name
-  eval "RPC_KEY_VALUE=\$$RPC_KEY_VAR_NAME"           # Use eval to get the value
-  echo "RPC_KEY: $RPC_KEY"
-  echo "Value: $RPC_KEY_VALUE"
+  eval "RPC_URL=\$$RPC_KEY_VAR_NAME"           # Use eval to get the value
+  # echo "RPC_KEY: $RPC_KEY"
+  # echo "Value: $RPC_URL"
 
   echo "[$NETWORK] RPC_URL: $RPC_URL"
 
+  # get diamond address for this network
   DIAMOND_ADDRESS=$(getContractAddressFromDeploymentLogs "$NETWORK" "production" "LiFiDiamond")
-  echo "[$NETWORK] DIAMOND_ADDRESS: $DEPLOYER"
-
   if [[ $? -ne 0 ]]; then
     error "[network: $NETWORK] could not find diamond address in PROD deploy log. Cannot continue for this network."
     return 1
   fi
+  echo "[$NETWORK] DIAMOND_ADDRESS: $DEPLOYER"
 
   # logging for debug purposes
   echo ""
   echo "in function handleNetwork"
   echo "NETWORK=$NETWORK"
-  echo "ACTION=$ACTION"
   echo "RPC_URL=$RPC_URL"
   echo "DIAMOND_ADDRESS=$DIAMOND_ADDRESS"
-  echo "FACET_CONTRACT_NAME=$FACET_CONTRACT_NAME"
   echo ""
 
+  # make sure pauserWallet is registered in this diamond and matches with the private key of the pauser wallet
+
+  # pause the diamond
 
 
+  # execute the requested action
+  local ATTEMPTS=1
+  while [ $ATTEMPTS -le "$MAX_ATTEMPTS_PER_SCRIPT_EXECUTION" ]; do
+    echoDebug "[network: $NETWORK] pausing diamond $DIAMOND_ADDRESS now from PauserWallet: $DEPLOYER"
+    cast send "$DIAMOND_ADDRESS" "pauseDiamond()" --private-key "$PRIVATE_KEY_PAUSER_WALLET" --rpc-url "$RPC_URL" --legacy >/dev/null
 
 
-  # # execute the requested action
-  # local ATTEMPTS=1
-  # while [ $ATTEMPTS -le "$MAX_ATTEMPTS_PER_SCRIPT_EXECUTION" ]; do
-  #   echoDebug "[network: $NETWORK] pausing diamond $DIAMOND_ADDRESS now from PauserWallet: $DEPLOYER"
-  #   cast send "$DIAMOND_ADDRESS" "pauseDiamond()" --private-key "$PRIVATE_KEY_PAUSER_WALLET" --rpc-url "$RPC_URL" --legacy >/dev/null
+    echo "[info] trying to pause the diamond now - attempt ${ATTEMPTS} (max attempts: $MAX_ATTEMPTS_PER_SCRIPT_EXECUTION)"
 
+    # if a facet address is given, remove that facet, otherwise pause the diamond
+    if [ -z "$FACET_CONTRACT_NAME"  ]; then
+      if [ "$ACTION" == "pause the diamond contract entirely" ]; then
 
-  #   echo "[info] trying to pause the diamond now - attempt ${ATTEMPTS} (max attempts: $MAX_ATTEMPTS_PER_SCRIPT_EXECUTION)"
+      else
+        echoDebug "[network: $NETWORK] proposing an unpause transaction to diamond owner multisig now"
 
-  #   # if a facet address is given, remove that facet, otherwise pause the diamond
-  #   if [ -z "$FACET_CONTRACT_NAME"  ]; then
-  #     if [ "$ACTION" == "pause the diamond contract entirely" ]; then
+        local CALLDATA=$(cast calldata "unpauseDiamond(address[])" "$BLACKLIST")
+        ts-node script/deploy/safe/propose-to-safe.ts --to "$DIAMOND_ADDRESS" --calldata "$CALLDATA" --network "$NETWORK" --rpcUrl $RPC_URL --privateKey "$SAFE_SIGNER_PRIVATE_KEY"
+      fi
+    else
+      echoDebug "[network: $NETWORK] removing $FACET_CONTRACT_NAME now"
 
-  #     else
-  #       echoDebug "[network: $NETWORK] proposing an unpause transaction to diamond owner multisig now"
+      # get facet address
+      FACET_ADDRESS=$(getContractAddressFromDeploymentLogs "$NETWORK" "production" "$FACET_CONTRACT_NAME")
 
-  #       local CALLDATA=$(cast calldata "unpauseDiamond(address[])" "$BLACKLIST")
-  #       ts-node script/deploy/safe/propose-to-safe.ts --to "$DIAMOND_ADDRESS" --calldata "$CALLDATA" --network "$NETWORK" --rpcUrl $RPC_URL --privateKey "$SAFE_SIGNER_PRIVATE_KEY"
-  #     fi
-  #   else
-  #     echoDebug "[network: $NETWORK] removing $FACET_CONTRACT_NAME now"
+      if [[ $? -ne 0 ]]; then
+        error "[network: $NETWORK] could not find address for facet $FACET_CONTRACT_NAME in PROD deploy log. Cannot continue for this network."
+        return 1
+      fi
 
-  #     # get facet address
-  #     FACET_ADDRESS=$(getContractAddressFromDeploymentLogs "$NETWORK" "production" "$FACET_CONTRACT_NAME")
+      cast send "$DIAMOND_ADDRESS" "removeFacet(address)" "$FACET_ADDRESS" --private-key "$PRIVATE_KEY_PAUSER_WALLET" --rpc-url "$RPC_URL" --legacy
+    fi
 
-  #     if [[ $? -ne 0 ]]; then
-  #       error "[network: $NETWORK] could not find address for facet $FACET_CONTRACT_NAME in PROD deploy log. Cannot continue for this network."
-  #       return 1
-  #     fi
+    # check the return code of the last call
+    if [ $? -eq 0 ]; then
+      break # exit the loop if the operation was successful
+    fi
 
-  #     cast send "$DIAMOND_ADDRESS" "removeFacet(address)" "$FACET_ADDRESS" --private-key "$PRIVATE_KEY_PAUSER_WALLET" --rpc-url "$RPC_URL" --legacy
-  #   fi
+    ATTEMPTS=$((ATTEMPTS + 1)) # increment ATTEMPTS
+    sleep 1                    # wait for 1 second before trying the operation again
+  done
 
-  #   # check the return code of the last call
-  #   if [ $? -eq 0 ]; then
-  #     break # exit the loop if the operation was successful
-  #   fi
-
-  #   ATTEMPTS=$((ATTEMPTS + 1)) # increment ATTEMPTS
-  #   sleep 1                    # wait for 1 second before trying the operation again
-  # done
-
-  # # check if call was executed successfully or used all ATTEMPTS
-  # if [ $ATTEMPTS -gt "$MAX_ATTEMPTS_PER_SCRIPT_EXECUTION" ]; then
-  #   error "[network: $NETWORK] failed to $ACTION on network $NETWORK (diamond address: $DIAMOND_ADDRESS)"
-  #   return 1
-  # fi
+  # check if call was executed successfully or used all ATTEMPTS
+  if [ $ATTEMPTS -gt "$MAX_ATTEMPTS_PER_SCRIPT_EXECUTION" ]; then
+    error "[network: $NETWORK] failed to $ACTION on network $NETWORK (diamond address: $DIAMOND_ADDRESS)"
+    return 1
+  fi
 
   # success "[network: $NETWORK] successfully executed action '$ACTION'"
   # echo ""
+    echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< end network $NETWORK <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
   return 0
+
 }
 
 
