1faf3c830929d5c6a58426be805b0e4cbf119b7d
Daniel <77058885+0xDEnYO@users.noreply.github.com>
2024-08-07 02:23:13 +0700

    Fund wallet on all chains script (#728)
    * temp - DO NOT PUSH
    
    * adds script to fund wallet on all chains using gas zip
    
    * cleanup

--- utils/network.ts
+++ utils/network.ts
@@ -1,4 +1,8 @@
 import 'dotenv/config'
+import * as fs from 'fs'
+import path from 'path'
+import { Chain } from 'viem'
+import * as chains from 'viem/chains'
 export function node_url(networkName: string): string {
   if (networkName) {
     const uri = process.env['ETH_NODE_URI_' + networkName.toUpperCase()]
@@ -45,4 +49,46 @@ export function getMnemonic(networkName?: string): string {
 
 export function accounts(networkName?: string): { mnemonic: string } {
   return { mnemonic: getMnemonic(networkName) }
+}
+
+// get a list of all networks from our ./networks file
+export function getAllNetworks(): string[] {
+  try {
+    // Read file contents
+    const fileContents = fs.readFileSync(
+      path.join(__dirname, '../networks'),
+      'utf-8'
+    )
+
+    // Split the contents by new lines to get an array of network names
+    const networkNames = fileContents
+      .split('\n')
+      .map((name) => name.trim())
+      .filter((name) => name !== '')
+
+    return networkNames
+  } catch (error) {
+    console.error(`Error reading file: ${JSON.stringify(error, null, 2)}`)
+    return []
+  }
+}
+
+// viem chain handling
+const chainNameMappings: Record<string, string> = {
+  zksync: 'zkSync',
+  polygonzkevm: 'polygonZkEvm',
+}
+
+const chainMap: Record<string, Chain> = {}
+for (const [k, v] of Object.entries(chains)) {
+  // @ts-ignore
+  chainMap[k] = v
+}
+
+export const getViemChainForNetworkName = (network: string): Chain => {
+  const chainName = chainNameMappings[network] || network
+  const chain: Chain = chainMap[chainName]
+
+  if (!chain) throw new Error(`Viem chain not found for network ${network}`)
+  return chain
 }
