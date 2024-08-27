9785369b0ea4902d7c9d6c0bc0bbaf764a8e84a0
Ed Zynda <ezynda3@gmail.com>
2024-08-27 15:15:54 +0300

    Cleanup and comments

--- script/demoScripts/demoPermit2Proxy.ts
+++ script/demoScripts/demoPermit2Proxy.ts
@@ -26,16 +26,19 @@ const main = defineCommand({
     signerKey: {
       type: 'string',
       description: 'Private key of signer',
+      required: true,
     },
     executorKey: {
       type: 'string',
       description: 'Private key of the executor',
+      required: true,
     },
   },
   async run({ args }) {
     const SIGNER_PRIVATE_KEY = `0x${args.signerKey}` as Hex
     const EXECUTOR_PRIVATE_KEY = `0x${args.executorKey}` as Hex
 
+    // Setup the required ABIs
     const permit2Abi = parseAbi([
       'function nonceBitmap(address owner, uint256 index) external view returns (uint256 nonce)',
     ])
@@ -44,24 +47,23 @@ const main = defineCommand({
       'function callDiamondWithPermit2SignatureSingle(address,bytes,address,((address,uint256),uint256,uint256),bytes) external',
     ])
 
+    // Setup a READ-ONLY client
     const client = createPublicClient({
       chain: arbitrum,
       transport: http(),
     })
 
-    // Account
+    // Setup a signer account
     const account = privateKeyToAccount(SIGNER_PRIVATE_KEY)
 
-    // Get calldata to bridge UNI from LIFI API
+    // Get calldata to bridge USDT from LIFI API
     const url =
       'https://li.quest/v1/quote?fromChain=ARB&toChain=POL&fromToken=0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9&toToken=0xc2132D05D31c914a87C6611C10748AEb04B58e8F&fromAddress=0xb9c0dE368BECE5e76B52545a8E377a4C118f597B&toAddress=0xb9c0dE368BECE5e76B52545a8E377a4C118f597B&fromAmount=5000000'
     const options = { method: 'GET', headers: { accept: 'application/json' } }
-
     const lifiResp = await fetch(url, options)
-
     const calldata = (await lifiResp.json()).transactionRequest.data
 
-    // Get nonce
+    // Get the nonce from the PERMIT2 contract
     const nonce = await client.readContract({
       address: PERMIT2_ADDRESS,
       abi: permit2Abi,
@@ -69,10 +71,10 @@ const main = defineCommand({
       args: [account.address, 0n],
     })
 
-    // Get block
+    // Get lastest block
     const block = await client.getBlock()
 
-    // Pass args and figure out msg hash
+    // Consturct a valid message hash to sign using Permit2Proxy's utility func
     const msgHash = await client.readContract({
       address: PERMIT2_PROXY_ADDRESS,
       abi: permit2ProxyAbi,
@@ -86,25 +88,26 @@ const main = defineCommand({
         block.timestamp + 1200n,
       ],
     })
-
     console.log(msgHash)
 
-    // Sign msg hash
+    // Sign the message hash
     const rsvSig = await sign({ hash: msgHash, privateKey: SIGNER_PRIVATE_KEY })
     const signature = serializeSignature(rsvSig)
-
-    // Call proxy with signature
     console.log(signature)
 
+    // Setup the parameters for the executor to call
     const tokenPermissions = [USDT_ADDRESS, parseUnits('5', 6)]
     const permit = [tokenPermissions, nonce, block.timestamp + 1200n]
 
+    // Instantiate the executor account and a WRITE enabled client
     const executorAccount = privateKeyToAccount(EXECUTOR_PRIVATE_KEY)
     const walletClient = createWalletClient({
       account: executorAccount,
       chain: arbitrum,
       transport: http(),
     })
+
+    // Execute using the Permit2 Proxy
     const tx = await walletClient.writeContract({
       address: PERMIT2_PROXY_ADDRESS,
       abi: permit2ProxyAbi,
