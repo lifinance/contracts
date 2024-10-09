5c04958a72a9c63331f5809bafc1a4bff0149194
Daniel Bläcker <d.blaecker@gmail.com>
2024-10-09 15:16:26 +0700

    use viem helper function to convert address to correct format

--- script/utils/viemScriptHelpers.ts
+++ script/utils/viemScriptHelpers.ts
@@ -1,4 +1,4 @@
-import { Chain, defineChain } from 'viem'
+import { Chain, defineChain, getAddress } from 'viem'
 import * as chains from 'viem/chains'
 import networksConfig from '../../config/networks.json'
 
@@ -45,7 +45,7 @@ export const getViemChainForNetworkName = (networkName: string): Chain => {
       },
     },
     contracts: {
-      multicall3: { address: network.multicallAddress as `0x${string}` },
+      multicall3: { address: getAddress(network.multicallAddress) },
     },
   })
   return chain
