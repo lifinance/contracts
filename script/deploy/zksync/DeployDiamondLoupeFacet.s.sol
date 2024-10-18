bce38b66750674abe0548b7106aae39db257ff34
Ed Zynda <ezynda3@gmail.com>
2024-10-18 12:42:58 +0300

    Add missing deploy scripts to zksync

---
+++ script/deploy/zksync/DeployDiamondLoupeFacet.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import { DeployScriptBase } from "./utils/DeployScriptBase.sol";
+import { DiamondLoupeFacet } from "lifi/Facets/DiamondLoupeFacet.sol";
+
+contract DeployScript is DeployScriptBase {
+    constructor() DeployScriptBase("DiamondLoupeFacet") {}
+
+    function run() public returns (DiamondLoupeFacet deployed) {
+        deployed = DiamondLoupeFacet(
+            deploy(type(DiamondLoupeFacet).creationCode)
+        );
+    }
+}
