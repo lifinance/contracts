5cc0d08a446f457d6275f54ee596835f0f2c5b47
Michal <michalmironczuk1@gmail.com>
2025-01-23 17:22:58 +0100

    Added LINK tests, refactoring

--- test/solidity/Facets/GlacisFacet.t.sol
+++ test/solidity/Facets/GlacisFacet.t.sol
@@ -5,7 +5,7 @@ import { LibAllowList, TestBaseFacet, ERC20 } from "../utils/TestBaseFacet.sol";
 import { LibSwap } from "lifi/Libraries/LibSwap.sol";
 import { GlacisFacet } from "lifi/Facets/GlacisFacet.sol";
 import { IGlacisAirlift, QuoteSendInfo } from "lifi/Interfaces/IGlacisAirlift.sol";
-import { InsufficientBalance } from "lifi/Errors/GenericErrors.sol";
+import { InsufficientBalance, InvalidReceiver, InvalidAmount, CannotBridgeToSameNetwork } from "lifi/Errors/GenericErrors.sol";
 
 // Stub GlacisFacet Contract
 contract TestGlacisFacet is GlacisFacet {
@@ -20,36 +20,31 @@ contract TestGlacisFacet is GlacisFacet {
     }
 }
 
-contract GlacisFacetTest is TestBaseFacet {
+abstract contract GlacisFacetTestBase is TestBaseFacet {
     GlacisFacet.GlacisData internal validGlacisData;
+    IGlacisAirlift internal airliftContract;
     TestGlacisFacet internal glacisFacet;
-    ERC20 internal wormhole;
-    uint256 internal defaultWORMHOLEAmount;
-    uint256 internal tokenFee;
-
-    IGlacisAirlift internal constant airlift =
-        IGlacisAirlift(0xE0A049955E18CFfd09C826C2c2e965439B6Ab272);
-    address internal ADDRESS_WORMHOLE_TOKEN =
-        0xB0fFa8000886e57F86dd5264b9582b2Ad87b2b91;
+    ERC20 internal srcToken;
+    uint256 internal defaultSrcTokenAmount;
+    uint256 internal destinationChainId;
+    address internal ADDRESS_SRC_TOKEN;
 
     uint256 internal payableAmount = 1 ether;
 
-    function setUp() public {
-        customRpcUrlForForking = "ETH_NODE_URI_ARBITRUM";
-        customBlockNumberForForking = 298446895;
+    function setUp() public virtual {
         initTestBase();
 
-        wormhole = ERC20(ADDRESS_WORMHOLE_TOKEN);
+        srcToken = ERC20(ADDRESS_SRC_TOKEN);
 
-        defaultWORMHOLEAmount = 1_000 * 10 ** wormhole.decimals();
+        defaultSrcTokenAmount = 1_000 * 10 ** srcToken.decimals();
 
         deal(
-            ADDRESS_WORMHOLE_TOKEN,
+            ADDRESS_SRC_TOKEN,
             USER_SENDER,
-            500_000 * 10 ** wormhole.decimals()
+            500_000 * 10 ** srcToken.decimals()
         );
 
-        glacisFacet = new TestGlacisFacet(airlift);
+        glacisFacet = new TestGlacisFacet(airliftContract);
         bytes4[] memory functionSelectors = new bytes4[](4);
         functionSelectors[0] = glacisFacet.startBridgeTokensViaGlacis.selector;
         functionSelectors[1] = glacisFacet
@@ -72,20 +67,30 @@ contract GlacisFacetTest is TestBaseFacet {
         glacisFacet.setFunctionApprovalBySignature(
             uniswap.swapETHForExactTokens.selector
         );
-
-        setFacetAddressInTestBase(address(glacisFacet), "GlacisFacet");
+        _facetTestContractAddress = address(glacisFacet);
+        vm.label(address(glacisFacet), "GlacisFacet");
 
         // adjust bridgeData
         bridgeData.bridge = "glacis";
-        bridgeData.sendingAssetId = ADDRESS_WORMHOLE_TOKEN;
-        bridgeData.minAmount = defaultWORMHOLEAmount;
-        bridgeData.destinationChainId = 10;
+        bridgeData.sendingAssetId = ADDRESS_SRC_TOKEN;
+        bridgeData.minAmount = defaultSrcTokenAmount;
+        bridgeData.destinationChainId = destinationChainId;
+
+        // add liquidity for dex pair DAI-{SOURCE TOKEN}
+        // this is necessary because Glacis does not provide routes for stablecoins like USDT or USDC, forcing us to work with custom tokens that often lack liquidity on V2 dexes
+        addLiquidity(
+            ADDRESS_DAI,
+            ADDRESS_SRC_TOKEN,
+            100_000 * 10 ** ERC20(ADDRESS_DAI).decimals(),
+            100_000 * 10 ** srcToken.decimals()
+        );
 
         // Call `quoteSend` to estimate the required native fee for the transfer.
         // This is necessary to ensure the transaction has sufficient gas for execution.
         // The `payableAmount` parameter simulates the amount of native tokens required for the estimation.
-        QuoteSendInfo memory quoteSendInfo = IGlacisAirlift(address(airlift))
-            .quoteSend(
+        QuoteSendInfo memory quoteSendInfo = IGlacisAirlift(
+            address(airliftContract)
+        ).quoteSend(
                 bridgeData.sendingAssetId,
                 bridgeData.minAmount,
                 bytes32(uint256(uint160(bridgeData.receiver))),
@@ -105,39 +110,40 @@ contract GlacisFacetTest is TestBaseFacet {
         });
     }
 
-    function initiateBridgeTxWithFacet(bool) internal override {
+    function initiateBridgeTxWithFacet(bool) internal virtual override {
         glacisFacet.startBridgeTokensViaGlacis{ value: addToMessageValue }(
             bridgeData,
             validGlacisData
         );
     }
 
-    function initiateSwapAndBridgeTxWithFacet(bool) internal override {
+    function initiateSwapAndBridgeTxWithFacet(bool) internal virtual override {
         glacisFacet.swapAndStartBridgeTokensViaGlacis{
             value: addToMessageValue
         }(bridgeData, swapData, validGlacisData);
     }
 
-    function testBase_CanBridgeNativeTokens() public override {
+    function testBase_CanBridgeNativeTokens() public virtual override {
         // facet does not support bridging of native assets
     }
 
     function testBase_CanBridgeTokens()
         public
+        virtual
         override
         assertBalanceChange(
-            ADDRESS_WORMHOLE_TOKEN,
+            ADDRESS_SRC_TOKEN,
             USER_SENDER,
-            -int256(defaultWORMHOLEAmount)
+            -int256(defaultSrcTokenAmount)
         )
-        assertBalanceChange(ADDRESS_WORMHOLE_TOKEN, USER_RECEIVER, 0)
+        assertBalanceChange(ADDRESS_SRC_TOKEN, USER_RECEIVER, 0)
         assertBalanceChange(ADDRESS_DAI, USER_SENDER, 0)
         assertBalanceChange(ADDRESS_DAI, USER_RECEIVER, 0)
     {
         vm.startPrank(USER_SENDER);
 
         // approval
-        wormhole.approve(address(glacisFacet), bridgeData.minAmount);
+        srcToken.approve(address(glacisFacet), bridgeData.minAmount);
 
         //prepare check for events
         vm.expectEmit(true, true, true, true, address(glacisFacet));
@@ -147,17 +153,22 @@ contract GlacisFacetTest is TestBaseFacet {
         vm.stopPrank();
     }
 
-    function testBase_CanBridgeTokens_fuzzed(uint256 amount) public override {
+    function testBase_CanBridgeTokens_fuzzed(
+        uint256 amount
+    ) public virtual override {
         vm.assume(
-            amount > 0 * 10 ** wormhole.decimals() &&
-                amount < 100_000 * 10 ** wormhole.decimals()
+            amount > 1 * 10 ** srcToken.decimals() &&
+                amount < 100_000 * 10 ** srcToken.decimals()
         );
-        vm.startPrank(USER_SENDER);
         bridgeData.minAmount = amount;
+
+        vm.startPrank(USER_SENDER);
+
         // approval
-        wormhole.approve(address(glacisFacet), bridgeData.minAmount);
-        QuoteSendInfo memory quoteSendInfo = IGlacisAirlift(address(airlift))
-            .quoteSend(
+        srcToken.approve(address(glacisFacet), bridgeData.minAmount);
+        QuoteSendInfo memory quoteSendInfo = IGlacisAirlift(
+            address(airliftContract)
+        ).quoteSend(
                 bridgeData.sendingAssetId,
                 bridgeData.minAmount,
                 bytes32(uint256(uint160(bridgeData.receiver))),
@@ -168,6 +179,7 @@ contract GlacisFacetTest is TestBaseFacet {
         addToMessageValue =
             quoteSendInfo.gmpFee.nativeFee +
             quoteSendInfo.airliftFeeInfo.airliftFee.nativeFee;
+
         //prepare check for events
         vm.expectEmit(true, true, true, true, address(glacisFacet));
         emit LiFiTransferStarted(bridgeData);
@@ -176,29 +188,28 @@ contract GlacisFacetTest is TestBaseFacet {
         vm.stopPrank();
     }
 
-    function testBase_CanSwapAndBridgeNativeTokens() public override {
+    function testBase_CanSwapAndBridgeNativeTokens() public virtual override {
         // facet does not support bridging of native assets
     }
 
-    function setDefaultSwapDataSingleDAItoWORMHOLE() internal virtual {
+    function setDefaultSwapDataSingleDAItoSourceToken() internal virtual {
         delete swapData;
-        // Swap DAI -> WORMHOLE
+        // Swap DAI -> {SOURCE TOKEN}
         address[] memory path = new address[](2);
         path[0] = ADDRESS_DAI;
-        path[1] = ADDRESS_WORMHOLE_TOKEN;
+        path[1] = ADDRESS_SRC_TOKEN;
 
-        uint256 amountOut = defaultWORMHOLEAmount;
+        uint256 amountOut = defaultSrcTokenAmount;
 
         // Calculate DAI amount
         uint256[] memory amounts = uniswap.getAmountsIn(amountOut, path);
         uint256 amountIn = amounts[0];
-
         swapData.push(
             LibSwap.SwapData({
                 callTo: address(uniswap),
                 approveTo: address(uniswap),
                 sendingAssetId: ADDRESS_DAI,
-                receivingAssetId: ADDRESS_WORMHOLE_TOKEN,
+                receivingAssetId: ADDRESS_SRC_TOKEN,
                 fromAmount: amountIn,
                 callData: abi.encodeWithSelector(
                     uniswap.swapExactTokensForTokens.selector,
@@ -215,20 +226,12 @@ contract GlacisFacetTest is TestBaseFacet {
 
     function testBase_CanSwapAndBridgeTokens()
         public
+        virtual
         override
         assertBalanceChange(ADDRESS_DAI, USER_RECEIVER, 0)
-        assertBalanceChange(ADDRESS_WORMHOLE_TOKEN, USER_SENDER, 0)
-        assertBalanceChange(ADDRESS_WORMHOLE_TOKEN, USER_RECEIVER, 0)
+        assertBalanceChange(ADDRESS_SRC_TOKEN, USER_SENDER, 0)
+        assertBalanceChange(ADDRESS_SRC_TOKEN, USER_RECEIVER, 0)
     {
-        // add liquidity for dex pair DAI-WORMHOLE
-        // this is necessary because Glacis does not provide routes for stablecoins like USDT or USDC, forcing us to work with custom tokens that often lack liquidity on V2 dexes
-        addLiquidity(
-            ADDRESS_DAI,
-            ADDRESS_WORMHOLE_TOKEN,
-            100_000 * 10 ** ERC20(ADDRESS_DAI).decimals(),
-            100_000 * 10 ** wormhole.decimals()
-        );
-
         uint256 initialDAIBalance = dai.balanceOf(USER_SENDER);
 
         vm.startPrank(USER_SENDER);
@@ -237,7 +240,7 @@ contract GlacisFacetTest is TestBaseFacet {
         bridgeData.hasSourceSwaps = true;
 
         // reset swap data
-        setDefaultSwapDataSingleDAItoWORMHOLE();
+        setDefaultSwapDataSingleDAItoSourceToken();
 
         // approval
         dai.approve(_facetTestContractAddress, swapData[0].fromAmount);
@@ -250,7 +253,7 @@ contract GlacisFacetTest is TestBaseFacet {
             bridgeData.transactionId,
             address(uniswap),
             ADDRESS_DAI,
-            ADDRESS_WORMHOLE_TOKEN,
+            ADDRESS_SRC_TOKEN,
             swapData[0].fromAmount,
             bridgeData.minAmount,
             block.timestamp
@@ -269,16 +272,75 @@ contract GlacisFacetTest is TestBaseFacet {
         assertEq(USER_SENDER.balance, initialETHBalance - addToMessageValue);
     }
 
-    function testBase_Revert_CallerHasInsufficientFunds() public override {
+    function testBase_Revert_BridgeAndSwapWithInvalidReceiverAddress()
+        public
+        virtual
+        override
+    {
+        vm.startPrank(USER_SENDER);
+        // prepare bridgeData
+        bridgeData.receiver = address(0);
+        bridgeData.hasSourceSwaps = true;
+
+        setDefaultSwapDataSingleDAItoSourceToken();
+
+        vm.expectRevert(InvalidReceiver.selector);
+
+        initiateSwapAndBridgeTxWithFacet(false);
+        vm.stopPrank();
+    }
+
+    function testBase_Revert_SwapAndBridgeWithInvalidAmount()
+        public
+        virtual
+        override
+    {
+        vm.startPrank(USER_SENDER);
+        // prepare bridgeData
+        bridgeData.hasSourceSwaps = true;
+        bridgeData.minAmount = 0;
+
+        setDefaultSwapDataSingleDAItoSourceToken();
+
+        vm.expectRevert(InvalidAmount.selector);
+
+        initiateSwapAndBridgeTxWithFacet(false);
+        vm.stopPrank();
+    }
+
+    function testBase_Revert_SwapAndBridgeToSameChainId()
+        public
+        virtual
+        override
+    {
+        vm.startPrank(USER_SENDER);
+        // prepare bridgeData
+        bridgeData.destinationChainId = block.chainid;
+        bridgeData.hasSourceSwaps = true;
+
+        setDefaultSwapDataSingleDAItoSourceToken();
+        dai.approve(_facetTestContractAddress, swapData[0].fromAmount);
+
+        vm.expectRevert(CannotBridgeToSameNetwork.selector);
+
+        initiateSwapAndBridgeTxWithFacet(false);
+        vm.stopPrank();
+    }
+
+    function testBase_Revert_CallerHasInsufficientFunds()
+        public
+        virtual
+        override
+    {
         vm.startPrank(USER_SENDER);
 
-        wormhole.approve(
+        srcToken.approve(
             address(_facetTestContractAddress),
-            defaultWORMHOLEAmount
+            defaultSrcTokenAmount
         );
 
-        // send all available W balance to different account to ensure sending wallet has no W funds
-        wormhole.transfer(USER_RECEIVER, wormhole.balanceOf(USER_SENDER));
+        // send all available source token balance to different account to ensure sending wallet has no source token funds
+        srcToken.transfer(USER_RECEIVER, srcToken.balanceOf(USER_SENDER));
 
         vm.expectRevert(
             abi.encodeWithSelector(
@@ -290,5 +352,33 @@ contract GlacisFacetTest is TestBaseFacet {
 
         initiateBridgeTxWithFacet(false);
         vm.stopPrank();
+    }
+}
+
+contract GlacisFacetWormholeTest is GlacisFacetTestBase {
+    function setUp() public virtual override {
+        customRpcUrlForForking = "ETH_NODE_URI_ARBITRUM";
+        customBlockNumberForForking = 298468086;
+
+        airliftContract = IGlacisAirlift(
+            0xE0A049955E18CFfd09C826C2c2e965439B6Ab272
+        );
+        ADDRESS_SRC_TOKEN = 0xB0fFa8000886e57F86dd5264b9582b2Ad87b2b91; // address of W token on Arbitrum network
+        destinationChainId = 10;
+        super.setUp();
+    }
+}
+
+contract GlacisFacetLINKTest is GlacisFacetTestBase {
+    function setUp() public virtual override {
+        customRpcUrlForForking = "ETH_NODE_URI_BASE";
+        customBlockNumberForForking = 25427676;
+
+        airliftContract = IGlacisAirlift(
+            0x56E20A6260644CC9F0B7d79a8C8E1e3Fabc15CEA
+        );
+        ADDRESS_SRC_TOKEN = 0x88Fb150BDc53A65fe94Dea0c9BA0a6dAf8C6e196; // address of LINK token on Base network
+        destinationChainId = 34443;
+        super.setUp();
     }
 }
