b138ca3fade4be4743bcb63d2bc7344421ab7ddc
Michal Mironczuk <michalmironczuk1@gmail.com>
2025-03-11 09:43:11 +0100

    Updated scripts

--- script/deploy/facets/UpdateCoreFacets.s.sol
+++ script/deploy/facets/UpdateCoreFacets.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import { UpdateScriptBase, console } from "./utils/UpdateScriptBase.sol";
+import { UpdateScriptBase } from "./utils/UpdateScriptBase.sol";
 import { stdJson } from "forge-std/StdJson.sol";
 import { DiamondCutFacet } from "lifi/Facets/DiamondCutFacet.sol";
 
