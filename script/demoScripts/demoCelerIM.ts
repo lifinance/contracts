76c39b42c8f616b0b2d6c1578a253d4d79deb411
Michal Mironczuk <michalmironczuk1@gmail.com>
2025-03-31 11:29:47 +0200

    Updated demoCelerIM

--- script/demoScripts/demoCelerIM.ts
+++ script/demoScripts/demoCelerIM.ts
@@ -1,36 +1,21 @@
 import { parseUnits, Narrow, zeroAddress } from 'viem'
 import { randomBytes } from 'crypto'
 import dotenv from 'dotenv'
-import erc20Artifact from '../../out/ERC20/ERC20.sol/ERC20.json'
 import celerIMFacetArtifact from '../../out/CelerIMFacetMutable.sol/CelerIMFacetMutable.json'
 import {
   CelerIM,
   ILiFi,
 } from '../../typechain/CelerIMFacetBase.sol/CelerIMFacetBase'
 import { SupportedChain } from './utils/demoScriptChainConfig'
-import {
-  ensureBalance,
-  executeTransaction,
-  setupEnvironment,
-} from './utils/demoScriptHelpers'
+import { executeTransaction, setupEnvironment } from './utils/demoScriptHelpers'
 
 dotenv.config()
 
 // #region ABIs
 
-const ERC20_ABI = erc20Artifact.abi as Narrow<typeof erc20Artifact.abi>
 const CELER_IM_FACET_ABI = celerIMFacetArtifact.abi as Narrow<
   typeof celerIMFacetArtifact.abi
 >
-
-// If you need to import a custom ABI, follow these steps:
-//
-// First, ensure you import the relevant artifact file:
-// import exampleArtifact from '../../out/{example artifact json file}'
-//
-// Then, define the ABI using `Narrow<typeof exampleArtifact.abi>` for proper type inference:
-// const EXAMPLE_ABI = exampleArtifact.abi as Narrow<typeof exampleArtifact.abi>
-//
 
 // #endregion
 
@@ -41,21 +26,14 @@ async function main() {
   const srcChain: SupportedChain = 'arbitrum'
   const destinationChainId = 137 // polygon
 
-  const {
-    client,
-    publicClient,
-    walletAccount,
-    lifiDiamondAddress,
-    lifiDiamondContract,
-  } = await setupEnvironment(srcChain, CELER_IM_FACET_ABI)
+  const { publicClient, walletAccount, lifiDiamondContract } =
+    await setupEnvironment(srcChain, CELER_IM_FACET_ABI)
   const signerAddress = walletAccount.address
 
   const amount = parseUnits('0.0001', 18) // 0.0005 * 1e{source token decimals}
 
   console.info(`Bridge ${amount} native token from ${srcChain} --> Polygon`)
   console.info(`Connected wallet address: ${signerAddress}`)
-
-  // await ensureBalance(address(0), signerAddress, amount)
 
   // === Prepare bridge data ===
   const bridgeData: ILiFi.BridgeDataStruct = {
@@ -72,15 +50,13 @@ async function main() {
     hasDestinationCall: false,
   }
 
-  const messageBusFee = parseUnits('0.0001', 18) // 0.0005 * 1e{source token decimals}
-
   const celerIMData: CelerIM.CelerIMDataStruct = {
     maxSlippage: 5000,
     nonce: 1,
     callTo: zeroAddress,
     callData: '0x',
-    bridgeType: 1, // MsgDataTypes.BridgeSendType.Liquidity
-    messageBusFee: messageBusFee,
+    bridgeType: 4, // MsgDataTypes.BridgeSendType.PegV2Deposit
+    messageBusFee: 0,
   }
 
   // === Start bridging ===
@@ -88,7 +64,7 @@ async function main() {
     () =>
       lifiDiamondContract.write.startBridgeTokensViaCelerIM(
         [bridgeData, celerIMData],
-        { value: amount + BigInt(messageBusFee) }
+        { value: amount }
       ),
     'Starting bridge tokens via CelerIM',
     publicClient,
