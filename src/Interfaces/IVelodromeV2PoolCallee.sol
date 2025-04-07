9adede5d41f0820c4d1308686a704f688e81c8b8
Michal Mironczuk <michalmironczuk1@gmail.com>
2025-04-07 11:49:21 +0200

    Added interface natspec

--- src/Interfaces/IVelodromeV2PoolCallee.sol
+++ src/Interfaces/IVelodromeV2PoolCallee.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+/// @title Interface for VelodromeV2 pool callee
+/// @author LI.FI (https://li.fi)
+/// @custom:version 1.0.0
 interface IVelodromeV2PoolCallee {
     function hook(
         address sender,
