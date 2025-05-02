b1d0a6e248d93fa4edcde3d2978d9a90a4ddfc60
Michal Mironczuk <michalmironczuk1@gmail.com>
2025-05-02 14:19:03 +0200

    added check for native assets in transferFromERC20 (audit issue #5)

--- src/Libraries/LibAsset.sol
+++ src/Libraries/LibAsset.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { LibSwap } from "./LibSwap.sol";
 import { SafeTransferLib } from "solady/utils/SafeTransferLib.sol";
-import { InvalidReceiver, NullAddrIsNotAValidSpender, InvalidAmount } from "../Errors/GenericErrors.sol";
+import { InvalidReceiver, NullAddrIsNotAValidSpender, InvalidAmount, NullAddrIsNotAnERC20Token } from "../Errors/GenericErrors.sol";
 
 /// @title LibAsset
 /// @custom:version 2.0.0
@@ -98,6 +98,11 @@ library LibAsset {
         address recipient,
         uint256 amount
     ) internal {
+        // check if native asset
+        if (isNativeAsset(assetId)) {
+            revert NullAddrIsNotAnERC20Token();
+        }
+
         // make sure a meaningful receiver address was provided
         if (recipient == NULL_ADDRESS) {
             revert InvalidReceiver();
