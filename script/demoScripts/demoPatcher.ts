aae891491d218ce87624d633b56459f0474ae2f9
Ed Zynda <ezynda3@gmail.com>
2025-05-23 16:24:21 +0300

    use correct calc

--- script/demoScripts/demoPatcher.ts
+++ script/demoScripts/demoPatcher.ts
@@ -19,6 +19,7 @@ import {
   http,
   getContract,
   Hex,
+  getCreate2Address,
 } from 'viem'
 import { privateKeyToAccount } from 'viem/accounts'
 import { arbitrum, base } from 'viem/chains'
@@ -39,6 +40,9 @@ const PATCHER_ARBITRUM = '0xE65b50EcF482f97f53557f0E02946aa27f8839EC'
 const COW_SHED_FACTORY = '0x00E989b87700514118Fa55326CD1cCE82faebEF6'
 const COW_SHED_IMPLEMENTATION = '0x2CFFA8cf11B90C9F437567b86352169dF4009F73'
 const VAULT_RELAYER_ARBITRUM = '0xC92E8bdf79f0507f65a392b0ab4667716BFE0110'
+// The creationCode of CoWShedProxy (type(COWShedProxy).creationCode)
+const PROXY_CREATION_CODE =
+  '0x60a034608e57601f61037138819003918201601f19168301916001600160401b038311848410176093578084926040948552833981010312608e57604b602060458360a9565b920160a9565b6080527f360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc556040516102b490816100bd8239608051818181608f01526101720152f35b600080fd5b634e487b7160e01b600052604160045260246000fd5b51906001600160a01b0382168203608e5756fe60806040526004361015610018575b3661019457610194565b6000803560e01c908163025b22bc1461003b575063f851a4400361000e5761010d565b3461010a5760207ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffc36011261010a5773ffffffffffffffffffffffffffffffffffffffff60043581811691828203610106577f0000000000000000000000000000000000000000000000000000000000000000163314600014610101577f360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc557fbc7cd75a20ee27fd9adebab32041f755214dbc6bffa90cc0225b39da2e5c2d3b8280a280f35b61023d565b8380fd5b80fd5b346101645760007ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffc360112610164576020610146610169565b73ffffffffffffffffffffffffffffffffffffffff60405191168152f35b600080fd5b333003610101577f000000000000000000000000000000000000000000000000000000000000000090565b60ff7f68df44b1011761f481358c0f49a711192727fb02c377d697bcb0ea8ff8393ac0541615806101ef575b1561023d5760046040517ff92ee8a9000000000000000000000000000000000000000000000000000000008152fd5b507f400ada75000000000000000000000000000000000000000000000000000000007fffffffff000000000000000000000000000000000000000000000000000000006000351614156101c0565b7f360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc546000808092368280378136915af43d82803e1561027a573d90f35b3d90fdfea2646970667358221220c7c26ff3040b96a28e96d6d27b743972943aeaef81cc821544c5fe1e24f9b17264736f6c63430008190033'
 
 // ABIs
 const ERC20_ABI = erc20Artifact.abi
@@ -68,42 +72,21 @@ class CowShedSdk {
 
   // Compute the deterministic proxy address for a user
   computeProxyAddress(owner: string): `0x${string}` {
-    // This uses CREATE2 to compute the deterministic address
-    const salt = ethers.utils.solidityKeccak256(
-      ['address', 'address'],
-      [owner, this.implementationAddress]
-    )
-
-    const proxyBytecode =
-      '0x' +
-      ethers.utils
-        .solidityPack(
-          ['bytes', 'address'],
-          [
-            '0x3d602d80600a3d3981f3363d3d373d3d3d363d73',
-            this.implementationAddress,
-          ]
-        )
-        .slice(2) +
-      '5af43d82803e903d91602b57fd5bf3'
-
-    const create2Input = ethers.utils.solidityKeccak256(
-      ['bytes'],
+    const salt = ethers.utils.defaultAbiCoder.encode(['address'], [owner])
+    const initCodeHash = ethers.utils.solidityKeccak256(
+      ['bytes', 'bytes'],
       [
-        ethers.utils.solidityPack(
-          ['bytes1', 'address', 'bytes32', 'bytes32'],
-          [
-            '0xff',
-            this.factoryAddress,
-            salt,
-            ethers.utils.keccak256(proxyBytecode),
-          ]
+        PROXY_CREATION_CODE,
+        ethers.utils.defaultAbiCoder.encode(
+          ['address', 'address'],
+          [COW_SHED_IMPLEMENTATION, owner]
         ),
       ]
     )
-
-    return ethers.utils.getAddress(
-      '0x' + create2Input.slice(26)
+    return ethers.utils.getCreate2Address(
+      COW_SHED_FACTORY,
+      salt,
+      initCodeHash
     ) as `0x${string}`
   }
 
