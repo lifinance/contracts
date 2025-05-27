5948f0af0d1671e271c0f80dcb8bdf7361f8f8e2
Daniel Bläcker <d.blaecker@gmail.com>
2025-05-27 19:23:11 +0700

    adjust GenericSwapFacetV3 to refund positive slippage above 1 wei only

--- src/Facets/GenericSwapFacetV3.sol
+++ src/Facets/GenericSwapFacetV3.sol
@@ -551,7 +551,11 @@ contract GenericSwapFacetV3 is ILiFi {
                 address(this)
             );
 
-            if (sendingAssetBalance > 0) {
+            // we decided to change this value from 0 to 1 to have more flexibility with rebasing tokens that
+            // sometimes produce rounding errors. In those cases there might we 1 wei leftover at the end of a swap
+            // but this 1 wei is not transferable, so the tx reverts. We accept that 1 wei dust gets stuck in the contract
+            // with every tx as this does not represent a significant USD value in any relevant token.
+            if (sendingAssetBalance > 1) {
                 sendingAsset.safeTransfer(receiver, sendingAssetBalance);
             }
         }
