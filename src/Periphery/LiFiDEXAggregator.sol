8537d49972c9ce2b2413d463210c08e1ff6f7b91
Michal Mironczuk <michalmironczuk1@gmail.com>
2025-06-05 23:02:28 +0200

    added enosysdexV3SwapCallback callback

--- src/Periphery/LiFiDEXAggregator.sol
+++ src/Periphery/LiFiDEXAggregator.sol
@@ -796,6 +796,23 @@ contract LiFiDEXAggregator is WithdrawablePeriphery {
         uniswapV3SwapCallback(amount0Delta, amount1Delta, data);
     }
 
+    /// @notice Called to `msg.sender` after executing a swap via IEnosysDexV3Pool#swap.
+    /// @dev In the implementation you must pay the pool tokens owed for the swap.
+    /// The caller of this method must be checked to be a EnosysDexV3Pool deployed by the canonical EnosysDexV3Factory.
+    /// amount0Delta and amount1Delta can both be 0 if no tokens were swapped.
+    /// @param amount0Delta The amount of token0 that was sent (negative) or must be received (positive) by the pool by
+    /// the end of the swap. If positive, the callback must send that amount of token0 to the pool.
+    /// @param amount1Delta The amount of token1 that was sent (negative) or must be received (positive) by the pool by
+    /// the end of the swap. If positive, the callback must send that amount of token1 to the pool.
+    /// @param data Any data passed through by the caller via the IEnosysDexV3PoolActions#swap call
+    function enosysdexV3SwapCallback(
+        int256 amount0Delta,
+        int256 amount1Delta,
+        bytes calldata data
+    ) external {
+        uniswapV3SwapCallback(amount0Delta, amount1Delta, data);
+    }
+
     /// @notice Curve pool swap. Legacy pools that don't return amountOut and have native coins are not supported
     /// @param stream [pool, poolType, fromIndex, toIndex, recipient, output token]
     /// @param from Where to take liquidity for swap
