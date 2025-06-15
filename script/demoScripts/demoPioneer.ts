62e0988304ea7f98cbcbb003f9ed57257af98e48
Daniel Bläcker <d.blaecker@gmail.com>
2025-06-15 19:09:18 +0700

    adjust demo script to match latest refactoring

--- script/demoScripts/demoPioneer.ts
+++ script/demoScripts/demoPioneer.ts
@@ -1,18 +1,13 @@
-import { getContract, parseUnits, Narrow, zeroAddress } from 'viem'
-import { randomBytes } from 'crypto'
-import dotenv from 'dotenv'
-import erc20Artifact from '../../out/ERC20/ERC20.sol/ERC20.json'
-import pioneerFacetArtifact from '../../out/PioneerFacet.sol/PioneerFacet.json'
-import { PioneerFacet, ILiFi } from '../../typechain'
-import { SupportedChain, viemChainMap } from './utils/demoScriptChainConfig'
-import {
-  ensureBalance,
-  ensureAllowance,
-  executeTransaction,
-  setupEnvironment,
-} from './utils/demoScriptHelpers'
+import { config } from 'dotenv'
+import { parseUnits, zeroAddress, type Narrow } from 'viem'
 
-dotenv.config()
+import pioneerFacetArtifact from '../../out/PioneerFacet.sol/PioneerFacet.json'
+import type { ILiFi } from '../../typechain'
+import type { SupportedChain } from '../common/types'
+
+import { executeTransaction, setupEnvironment } from './utils/demoScriptHelpers'
+
+config()
 
 // The following transactions show a successful transaction from Arbitrum to Optimism
 // https://arbiscan.io/tx/0x64bddd54b3b6a235a4f85c23f80f6aa26031dbb914b11db8623349cb38b0771a
@@ -20,13 +15,10 @@ dotenv.config()
 
 // #region ABIs
 
-const ERC20_ABI = erc20Artifact.abi as Narrow<typeof erc20Artifact.abi>
 const PIONEER_FACET_ABI = pioneerFacetArtifact.abi as Narrow<
   typeof pioneerFacetArtifact.abi
 >
 // #endregion
-
-dotenv.config()
 
 async function main() {
   // === Set up environment ===
@@ -34,13 +26,8 @@ async function main() {
   const destinationChainId = 10 // OP Mainnet
   const PIONEER_ENDPOINT = 'https://solver-dev.li.fi' as const
 
-  const {
-    client,
-    publicClient,
-    walletAccount,
-    lifiDiamondAddress,
-    lifiDiamondContract,
-  } = await setupEnvironment(srcChain, PIONEER_FACET_ABI)
+  const { client, publicClient, walletAccount, lifiDiamondContract, chain } =
+    await setupEnvironment(srcChain, PIONEER_FACET_ABI)
   const signerAddress = walletAccount.address
 
   // === Contract addresses ===
@@ -69,9 +56,7 @@ async function main() {
       `Insufficient balance. Required: ${amount}, Available: ${balance}`
     )
     process.exit(1)
-  } else {
-    console.info(`Balance: ${balance}`)
-  }
+  } else console.info(`Balance: ${balance}`)
 
   // === In this part put necessary logic usually it's fetching quotes, estimating fees, signing messages etc. ===
 
@@ -83,7 +68,7 @@ async function main() {
     toAsset: string
     toAddress: string
   } = {
-    fromChainId: viemChainMap[srcChain]!.id.toString(),
+    fromChainId: chain.id.toString(),
     fromAsset: SRC_TOKEN_ADDRESS,
     fromAmount: amount.toString(),
     toChainId: destinationChainId.toString(),
@@ -136,11 +121,16 @@ async function main() {
   console.log(bridgeData)
 
   // === Start bridging ===
+  if (!lifiDiamondContract) throw new Error('LiFi Diamond contract not found')
+
   await executeTransaction(
     () =>
-      lifiDiamondContract.write.startBridgeTokensViaPioneer([bridgeData], {
-        value: amount,
-      }),
+      (lifiDiamondContract as any).write.startBridgeTokensViaPioneer(
+        [bridgeData],
+        {
+          value: amount,
+        }
+      ),
     'Starting bridge tokens via Pioneer',
     publicClient,
     true
