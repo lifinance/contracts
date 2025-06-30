285ef06945cf5d5f04c193194e5a07de13f1e2cd
Michal Mironczuk <michalmironczuk1@gmail.com>
2025-06-30 17:59:09 +0200

    test

---
+++ src/Test/ReentrancyVuln.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+error NothingToWithdraw();
+error WithdrawFailed();
+
+contract ReentrancyVuln {
+    mapping(address => uint256) public balances;
+
+    function deposit() external payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() external {
+        uint256 amount = balances[msg.sender];
+        if (amount == 0) revert NothingToWithdraw();
+        (bool sent, ) = msg.sender.call{ value: amount }("");
+        if (!sent) revert WithdrawFailed();
+        balances[msg.sender] = 0;
+    }
+}
