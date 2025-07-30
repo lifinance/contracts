7d283f991df2948077c1a91ea685a2ef56bab568
Daniel Bläcker <d.blaecker@gmail.com>
2025-07-30 11:32:50 +0700

    improve update core facets script (reads core facets dynamically

--- script/deploy/facets/UpdateCoreFacets.s.sol
+++ script/deploy/facets/UpdateCoreFacets.s.sol
@@ -8,153 +8,85 @@ import { DiamondCutFacet } from "lifi/Facets/DiamondCutFacet.sol";
 contract DeployScript is UpdateScriptBase {
     using stdJson for string;
 
+    error FailedToReadCoreFacetsFromConfig();
+
     function run()
         public
         returns (address[] memory facets, bytes memory cutData)
     {
-        address diamondLoupe = _getConfigContractAddress(
-            path,
-            ".DiamondLoupeFacet"
+        // Read core facets dynamically from global.json config
+        string memory globalConfigPath = string.concat(
+            vm.projectRoot(),
+            "/config/global.json"
         );
-        address ownership = _getConfigContractAddress(path, ".OwnershipFacet");
-        address withdraw = _getConfigContractAddress(path, ".WithdrawFacet");
-        address dexMgr = _getConfigContractAddress(path, ".DexManagerFacet");
-        address accessMgr = _getConfigContractAddress(
-            path,
-            ".AccessManagerFacet"
+        string memory globalConfig = vm.readFile(globalConfigPath);
+        string[] memory coreFacets = globalConfig.readStringArray(
+            ".coreFacets"
         );
-        address peripheryRgs = _getConfigContractAddress(
-            path,
-            ".PeripheryRegistryFacet"
-        );
-        address gasZipAddress = _getConfigContractAddress(
-            path,
-            ".GasZipFacet"
-        );
-        address genSwapAddress = _getConfigContractAddress(
-            path,
-            ".GenericSwapFacet"
-        );
-        address genSwapV3Address = _getConfigContractAddress(
-            path,
-            ".GenericSwapFacetV3"
-        );
-        address calldVerifAddress = _getConfigContractAddress(
-            path,
-            ".CalldataVerificationFacet"
-        );
-        address emergencyPauseAddress = _getConfigContractAddress(
-            path,
-            ".EmergencyPauseFacet"
-        );
+
+        emit log("Core facets found in config/global.json: ");
+        emit log_uint(coreFacets.length);
 
         bytes4[] memory exclude;
 
-        // check if the loupe was already added to the diamond
+        // Check if the loupe was already added to the diamond
         bool loupeExists;
         try loupe.facetAddresses() returns (address[] memory) {
-            // if call was successful, loupe exists on diamond already
+            // If call was successful, loupe exists on diamond already
             loupeExists = true;
         } catch {
-            // no need to do anything, just making sure that the flow continues in both cases with try/catch
+            // No need to do anything, just making sure that the flow continues in both cases with try/catch
         }
 
-        // Diamond Loupe
-        bytes4[] memory selectors = getSelectors("DiamondLoupeFacet", exclude);
-
+        // Handle DiamondLoupeFacet separately as it needs special treatment
         if (!loupeExists) {
-            buildInitialCut(selectors, diamondLoupe);
+            address diamondLoupeAddress = _getConfigContractAddress(
+                path,
+                ".DiamondLoupeFacet"
+            );
+            bytes4[] memory loupeSelectors = getSelectors(
+                "DiamondLoupeFacet",
+                exclude
+            );
+
+            buildInitialCut(loupeSelectors, diamondLoupeAddress);
             vm.startBroadcast(deployerPrivateKey);
             if (cut.length > 0) {
                 cutter.diamondCut(cut, address(0), "");
             }
             vm.stopBroadcast();
+
+            // Reset diamond cut variable to remove diamondLoupe information
+            delete cut;
         }
 
-        // reset diamond cut variable to remove diamondLoupe information
-        delete cut;
+        // Process all core facets dynamically
+        for (uint256 i = 0; i < coreFacets.length; i++) {
+            string memory facetName = coreFacets[i];
 
-        // Ownership Facet
-        selectors = getSelectors("OwnershipFacet", exclude);
-        if (loupeExists) {
-            buildDiamondCut(selectors, ownership);
-        } else {
-            buildInitialCut(selectors, ownership);
+            // Skip DiamondLoupeFacet as it was already handled
+            if (
+                keccak256(bytes(facetName)) ==
+                keccak256(bytes("DiamondLoupeFacet"))
+            ) {
+                continue;
+            }
+
+            // Use _getConfigContractAddress which validates the contract exists
+            address facetAddress = _getConfigContractAddress(
+                path,
+                string.concat(".", facetName)
+            );
+            bytes4[] memory selectors = getSelectors(facetName, exclude);
+
+            if (loupeExists) {
+                buildDiamondCut(selectors, facetAddress);
+            } else {
+                buildInitialCut(selectors, facetAddress);
+            }
         }
 
-        // Withdraw Facet
-        selectors = getSelectors("WithdrawFacet", exclude);
-        if (loupeExists) {
-            buildDiamondCut(selectors, withdraw);
-        } else {
-            buildInitialCut(selectors, withdraw);
-        }
-
-        // Dex Manager Facet
-        selectors = getSelectors("DexManagerFacet", exclude);
-        if (loupeExists) {
-            buildDiamondCut(selectors, dexMgr);
-        } else {
-            buildInitialCut(selectors, dexMgr);
-        }
-
-        // Access Manager Facet
-        selectors = getSelectors("AccessManagerFacet", exclude);
-        if (loupeExists) {
-            buildDiamondCut(selectors, accessMgr);
-        } else {
-            buildInitialCut(selectors, accessMgr);
-        }
-
-        // PeripheryRegistry
-        selectors = getSelectors("PeripheryRegistryFacet", exclude);
-        if (loupeExists) {
-            buildDiamondCut(selectors, peripheryRgs);
-        } else {
-            buildInitialCut(selectors, peripheryRgs);
-        }
-
-        // GasZipFacet
-        selectors = getSelectors("GasZipFacet", exclude);
-        if (loupeExists) {
-            buildDiamondCut(selectors, gasZipAddress);
-        } else {
-            buildInitialCut(selectors, gasZipAddress);
-        }
-
-        // GenericSwapFacet
-        selectors = getSelectors("GenericSwapFacet", exclude);
-        if (loupeExists) {
-            buildDiamondCut(selectors, genSwapAddress);
-        } else {
-            buildInitialCut(selectors, genSwapAddress);
-        }
-
-        // GenericSwapFacetV3
-        selectors = getSelectors("GenericSwapFacetV3", exclude);
-        if (loupeExists) {
-            buildDiamondCut(selectors, genSwapV3Address);
-        } else {
-            buildInitialCut(selectors, genSwapV3Address);
-        }
-
-        // CalldataVerificationFacet
-        selectors = getSelectors("CalldataVerificationFacet", exclude);
-        if (loupeExists) {
-            buildDiamondCut(selectors, calldVerifAddress);
-        } else {
-            buildInitialCut(selectors, calldVerifAddress);
-        }
-
-        // EmergencyPauseFacet
-        selectors = getSelectors("EmergencyPauseFacet", exclude);
-        if (loupeExists) {
-            buildDiamondCut(selectors, emergencyPauseAddress);
-        } else {
-            buildInitialCut(selectors, emergencyPauseAddress);
-        }
-
-        // if noBroadcast is activated, we only prepare calldata for sending it to multisig SAFE
+        // If noBroadcast is activated, we only prepare calldata for sending it to multisig SAFE
         if (noBroadcast) {
             if (cut.length > 0) {
                 cutData = abi.encodeWithSelector(
@@ -171,7 +103,6 @@ contract DeployScript is UpdateScriptBase {
         if (cut.length > 0) {
             cutter.diamondCut(cut, address(0), "");
         }
-
         vm.stopBroadcast();
 
         facets = loupe.facetAddresses();
