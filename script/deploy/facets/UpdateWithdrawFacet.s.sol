e68403a73499f53e69ccb0e8ff7d14651234883d
Daniel <77058885+0xDEnYO@users.noreply.github.com>
2025-09-15 19:54:34 +0700

    stETH redeployments (#1377)
    * add helper functions for multichain (and -evm) execution
    
    * add helper scripts for lib asset replacement
    
    * redeployed 2 facets on Soneium
    
    * redeployed 2 contracts to unichain
    
    * redeployed 2 facets to optimism
    
    * redeployed 27 contracts to mainnet
    
    * add new periphery contracts to whitelist
    
    * add update scripts for withdraw facet
    
    * remove unwanted files
    
    * diamond logs updated

---
+++ script/deploy/facets/UpdateWithdrawFacet.s.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.17;
+
+import { UpdateScriptBase } from "./utils/UpdateScriptBase.sol";
+import { stdJson } from "forge-std/StdJson.sol";
+
+contract DeployScript is UpdateScriptBase {
+    using stdJson for string;
+
+    function run()
+        public
+        returns (address[] memory facets, bytes memory cutData)
+    {
+        return update("WithdrawFacet");
+    }
+}
