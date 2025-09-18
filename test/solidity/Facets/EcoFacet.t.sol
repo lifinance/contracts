1a29ffaa391ca5679254e0f68daf9e1a57644987
Carlos Febres <cfebres@eco.com>
2025-09-18 17:16:48 -0400

    feat: update portal contract address

--- test/solidity/Facets/EcoFacet.t.sol
+++ test/solidity/Facets/EcoFacet.t.sol
@@ -25,7 +25,7 @@ contract TestEcoFacet is EcoFacet {
 contract EcoFacetTest is TestBaseFacet {
     TestEcoFacet internal ecoFacet;
     address internal constant PORTAL =
-        0xa9CD3b5603806D5Bdfa81cDFA494D2196D7b2624;
+        0xB5e58A8206473Df3Ab9b8DDd3B0F84c0ba68F8b5;
     uint256 internal constant NATIVE_SOLVER_REWARD = 0.0001 ether;
     uint256 internal constant TOKEN_SOLVER_REWARD = 10 * 10 ** 6; // 10 USDC (6 decimals)
 
