da34c7f4c4691138b5f80c1d476b4d58b0f88d95
Alexander <git@lindgren.xyz>
2025-09-29 12:57:50 +0300

    Missed "LIFI" on update script

--- script/deploy/facets/UpdateLiFiIntentFacet.s.sol
+++ script/deploy/facets/UpdateLiFiIntentFacet.s.sol
@@ -9,6 +9,6 @@ contract DeployScript is UpdateScriptBase {
         public
         returns (address[] memory facets, bytes memory cutData)
     {
-        return update("LIFIIntentEscrowFacet");
+        return update("LiFiIntentEscrowFacet");
     }
 }
