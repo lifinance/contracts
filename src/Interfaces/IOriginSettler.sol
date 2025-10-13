d5af55c4fda936b8e0230bf40ccfa95c0a798a67
Alexander <git@lindgren.xyz>
2025-10-13 11:49:55 +0200

    Remove unused version tag

--- src/Interfaces/IOriginSettler.sol
+++ src/Interfaces/IOriginSettler.sol
@@ -8,7 +8,6 @@ import { StandardOrder } from "./IOpenIntentFramework.sol";
  * @title IOriginSettler
  * @notice Interface for opening origin-settlement orders (with and without sponsor authorization).
  * @author LI.FI (https://li.fi)
- * @custom:version 1.0.0
  */
 interface IOriginSettler {
     function openFor(
