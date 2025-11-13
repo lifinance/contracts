36db3bb68f9bd56e009c274e902160f159aa6c44
Daniel Bläcker <d.blaecker@gmail.com>
2025-11-13 10:02:53 +0700

    update facet formatting

--- src/Interfaces/IOriginSettler.sol
+++ src/Interfaces/IOriginSettler.sol
@@ -4,12 +4,10 @@ pragma solidity ^0.8.17;
 
 import { StandardOrder } from "./IOpenIntentFramework.sol";
 
-/**
- * @title IOriginSettler
- * @notice Interface for opening origin-settlement orders (with and without sponsor authorization).
- * @author LI.FI (https://li.fi)
- * @custom:version 1.0.0
- */
+/// @title IOriginSettler
+/// @notice Interface for opening origin-settlement orders (with and without sponsor authorization).
+/// @author LI.FI (https://li.fi)
+/// @custom:version 1.0.0
 interface IOriginSettler {
     function openFor(
         StandardOrder calldata order,
