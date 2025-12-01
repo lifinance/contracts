7599c43c35b0fb98bb331b12a85bb66ce5225eeb
Daniel Bläcker <D.Blaecker@gmail.com>
2025-12-01 09:59:23 +0700

    fix failing test

--- test/solidity/Facets/PolymerCCTPFacet.t.sol
+++ test/solidity/Facets/PolymerCCTPFacet.t.sol
@@ -402,7 +402,7 @@ contract PolymerCCTPFacetTest is TestBaseFacet {
     }
 
     function test_ChainIdToDomainIdMapping() public {
-        ChainMapping[] memory mappings = new ChainMapping[](19);
+        ChainMapping[] memory mappings = new ChainMapping[](16);
         mappings[0] = ChainMapping({ chainId: 1, domainId: 0 }); // Ethereum
         mappings[1] = ChainMapping({ chainId: 43114, domainId: 1 }); // Avalanche
         mappings[2] = ChainMapping({ chainId: 10, domainId: 2 }); // OP Mainnet
@@ -419,9 +419,6 @@ contract PolymerCCTPFacetTest is TestBaseFacet {
         mappings[13] = ChainMapping({ chainId: 999, domainId: 19 }); // HyperEVM
         mappings[14] = ChainMapping({ chainId: 57073, domainId: 21 }); // Ink
         mappings[15] = ChainMapping({ chainId: 98866, domainId: 22 }); // Plume
-        mappings[16] = ChainMapping({ chainId: 11155111, domainId: 0 }); // Sepolia
-        mappings[17] = ChainMapping({ chainId: 11155420, domainId: 2 }); // OP Sepolia
-        mappings[18] = ChainMapping({ chainId: 84532, domainId: 6 }); // Base Sepolia
 
         for (uint256 i = 0; i < mappings.length; i++) {
             assertEq(
