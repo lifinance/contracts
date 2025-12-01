31f66ef7125800cf1363024f3f7796824b45cfcf
Ed Zynda <ezynda3@gmail.com>
2025-12-01 11:17:47 +0300

    fix

--- src/Periphery/LiFiDEXAggregator.sol
+++ src/Periphery/LiFiDEXAggregator.sol
@@ -2,8 +2,7 @@
 
 pragma solidity ^0.8.17;
 
-import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { IERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Permit.sol";
+import { SafeERC20, IERC20, IERC20Permit } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { SafeTransferLib } from "solady/utils/SafeTransferLib.sol";
 import { WithdrawablePeriphery } from "lifi/Helpers/WithdrawablePeriphery.sol";
 import { IVelodromeV2Pool } from "lifi/Interfaces/IVelodromeV2Pool.sol";
