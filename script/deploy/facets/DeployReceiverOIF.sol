960dfac5216ec4daf5c1ecb7a5ae816630691acb
Alexander <git@lindgren.xyz>
2025-12-05 15:57:39 +0100

    Deployment script

---
+++ script/deploy/facets/DeployReceiverOIF.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import { DeployScriptBase } from "./utils/DeployScriptBase.sol";
+import { stdJson } from "forge-std/Script.sol";
+import { ReceiverOIF } from "lifi/Periphery/ReceiverOIF.sol";
+
+contract DeployScript is DeployScriptBase {
+    using stdJson for string;
+
+    constructor() DeployScriptBase("ReceiverOIF") {}
+
+    function run()
+        public
+        returns (ReceiverOIF deployed, bytes memory constructorArgs)
+    {
+        constructorArgs = getConstructorArgs();
+
+        deployed = ReceiverOIF(deploy(type(ReceiverOIF).creationCode));
+    }
+
+    function getConstructorArgs() internal override returns (bytes memory) {
+        // get path of global config file
+        string memory globalConfigPath = string.concat(
+            root,
+            "/config/global.json"
+        );
+
+        // read file into json variable
+        string memory globalConfigJson = vm.readFile(globalConfigPath);
+
+        // extract refundWallet address
+        address refundWalletAddress = globalConfigJson.readAddress(
+            ".refundWallet"
+        );
+
+        string memory path = string.concat(
+            root,
+            "/config/lifiintentescrow.json"
+        );
+        address outputSettler = _getConfigContractAddress(
+            path,
+            string.concat(".outputSettlerSimple")
+        );
+
+        // get Executor address from deploy log
+        path = string.concat(
+            root,
+            "/deployments/",
+            network,
+            ".",
+            fileSuffix,
+            "json"
+        );
+        address executor = _getConfigContractAddress(path, ".Executor");
+
+        return
+            abi.encode(
+                refundWalletAddress,
+                executor,
+                outputSettler,
+                35000, // This is not enough for the recovery logic, but it means that there is a higher likelihood that complicated swap logic goes through (see docs).
+                400000
+            );
+    }
+}
