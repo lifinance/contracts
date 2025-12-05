8962d006c2cfd04abf61aba61ccdd5231999d3b7
Alexander <git@lindgren.xyz>
2025-12-05 15:27:45 +0100

    LI.FI Receiver draft contract

---
+++ src/Periphery/ReceiverOIF.sol
@@ -0,0 +1,145 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.17;
+
+import { LibSwap } from "../Libraries/LibSwap.sol";
+import { SafeTransferLib } from "solady/utils/SafeTransferLib.sol";
+import { LibAsset } from "../Libraries/LibAsset.sol";
+import { ILiFi } from "../Interfaces/ILiFi.sol";
+import { IExecutor } from "../Interfaces/IExecutor.sol";
+import { WithdrawablePeriphery } from "../Helpers/WithdrawablePeriphery.sol";
+import { UnAuthorized } from "../Errors/GenericErrors.sol";
+
+/**
+ * @notice Callback handling for OIF output processing.
+ */
+interface IOutputCallback {
+    /**
+     * @notice If configured, is called when the output is filled on the output chain.
+     */
+    function outputFilled(
+        bytes32 token,
+        uint256 amount,
+        bytes calldata executionData
+    ) external;
+}
+
+/// @title ReceiverOIF
+/// @author LI.FI (https://li.fi)
+/// @notice Arbitrary execution contract used for cross-chain swaps via OIF.
+/// Anyone can call this contract with fradulently filled intents, incoming outputFilled calls cannot be trusted.
+/// @custom:version 1.0.0
+contract ReceiverOIF is ILiFi, WithdrawablePeriphery, IOutputCallback {
+    /// Errors ///
+    error NotEnoughGas();
+
+    /// Storage ///
+    IExecutor public immutable EXECUTOR;
+    uint256 public immutable MINIMUM_GAS;
+    uint256 public immutable RECOVER_GAS;
+    address public immutable OUTPUT_SETTLER;
+
+    /// Modifiers ///
+    modifier onlyTrustedOutputSettler() {
+        if (msg.sender != OUTPUT_SETTLER) {
+            revert UnAuthorized();
+        }
+        _;
+    }
+
+    /// Constructor
+    constructor(
+        address _owner,
+        address _executor,
+        address _outputSettler,
+        uint256 _minimumGas,
+        uint256 _recoverGas
+    ) WithdrawablePeriphery(_owner) {
+        EXECUTOR = IExecutor(_executor);
+        MINIMUM_GAS = _minimumGas;
+        RECOVER_GAS = _recoverGas;
+        OUTPUT_SETTLER = _outputSettler;
+    }
+
+    /// External Methods ///
+
+    /// @notice Completes an OIF Intent via outputFilled.
+    /// @dev The endpoint is configured to only accept calldata coming from a specfic output settler. This does not prevent people from calling this function with malicious or fradulent data but we can assure that the tokens and amounts have been correctly delivered.
+    // If token is bytes32(0) then the native amount has been delivered before this call and no call value is provided.
+    /// @param token Token identifier for the filled output. If normal ERC20, the 20 least significant bytes contains the address.
+    /// @param amount Token amount
+    /// @param executionData Attached arbitrary callbackData for the output.
+    function outputFilled(
+        bytes32 token,
+        uint256 amount,
+        bytes calldata executionData
+    ) external onlyTrustedOutputSettler {
+        // decode payload
+        (
+            bytes32 transactionId,
+            LibSwap.SwapData[] memory swapData,
+            address receiver
+        ) = abi.decode(executionData, (bytes32, LibSwap.SwapData[], address));
+
+        // execute swap(s)
+        _swapAndCompleteBridgeTokens(
+            transactionId,
+            swapData,
+            address(uint160(uint256(token))),
+            payable(receiver),
+            amount
+        );
+    }
+
+    /// Private Methods ///
+
+    /// @notice Performs a swap before completing a cross-chain transaction
+    /// @param _transactionId the transaction id associated with the operation
+    /// @param _swapData array of data needed for swaps
+    /// @param assetId address of the token received from the source chain
+    /// @param receiver address that will receive tokens in the end
+    /// @param amount amount of token
+    function _swapAndCompleteBridgeTokens(
+        bytes32 _transactionId,
+        LibSwap.SwapData[] memory _swapData,
+        address assetId,
+        address payable receiver,
+        uint256 amount
+    ) private {
+        // If the solver has not provided enough gas, we revert. This makes the intent unfillable (until they try with more gas.)
+        if (gasleft() < MINIMUM_GAS) revert NotEnoughGas();
+
+        bool isNative = LibAsset.isNativeAsset(assetId);
+        if (!isNative) {
+            SafeTransferLib.safeApproveWithRetry(
+                assetId,
+                address(EXECUTOR),
+                amount
+            );
+        }
+        // We will catch OOG so we need to ensure that there is enough gas left that we will run the recover loop.
+        try
+            EXECUTOR.swapAndCompleteBridgeTokens{
+                value: isNative ? amount : 0,
+                gas: gasleft() - RECOVER_GAS
+            }(_transactionId, _swapData, assetId, receiver)
+        {} catch {
+            if (isNative) {
+                SafeTransferLib.safeTransferETH(receiver, amount);
+            } else {
+                SafeTransferLib.safeTransfer(assetId, receiver, amount);
+            }
+
+            emit LiFiTransferRecovered(
+                _transactionId,
+                assetId,
+                receiver,
+                amount,
+                block.timestamp
+            );
+        }
+    }
+
+    /// @notice Native assets are sent alone before the callback is called.
+    // solhint-disable-next-line no-empty-blocks
+    receive() external payable {}
+}
---
+++ test/solidity/Periphery/ReceiverOIF.t.sol
@@ -0,0 +1,348 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity ^0.8.17;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { ReceiverOIF } from "lifi/Periphery/ReceiverOIF.sol";
+import { Executor } from "lifi/Periphery/Executor.sol";
+import { UnAuthorized } from "lifi/Errors/GenericErrors.sol";
+import { ERC20Proxy } from "lifi/Periphery/ERC20Proxy.sol";
+import { MandateOutput } from "lifi/Interfaces/IOpenIntentFramework.sol";
+
+import { TestBase, LibSwap } from "../utils/TestBase.sol";
+
+address constant OUTPUT_SETTLER_COIN = 0x0000000000eC36B683C2E6AC89e9A75989C22a2e;
+
+interface OutputSettler {
+    function fill(
+        bytes32 orderId,
+        MandateOutput calldata output,
+        uint48 fillDeadline,
+        bytes calldata fillerData
+    ) external payable returns (bytes32 fillRecordHash);
+}
+
+contract NonETHReceiver {
+    // this contract cannot receive any ETH due to missing receive function
+}
+
+contract ReceiverOIFTest is TestBase {
+    uint256 internal constant MINIMUM_GAS = 400_000;
+    uint256 internal constant RECOVER_GAS = 100_000;
+
+    Executor internal executor;
+    ERC20Proxy internal erc20Proxy;
+    ReceiverOIF internal receiver;
+
+    bytes32 internal transferId = keccak256("Hello");
+
+    function setUp() public {
+        // Block after deployment.
+        customBlockNumberForForking = 23695990;
+        initTestBase();
+
+        erc20Proxy = new ERC20Proxy(address(this));
+        executor = new Executor(address(erc20Proxy), address(this));
+        receiver = new ReceiverOIF(
+            address(this),
+            address(executor),
+            OUTPUT_SETTLER_COIN,
+            MINIMUM_GAS,
+            RECOVER_GAS
+        );
+    }
+
+    function test_OwnerCanWithdrawERC20Token() public {
+        // fund receiver with ERC20 tokens
+        deal(ADDRESS_DAI, address(receiver), 1000);
+
+        uint256 initialBalance = dai.balanceOf(USER_RECEIVER);
+
+        // pull token
+        vm.startPrank(USER_DIAMOND_OWNER);
+
+        receiver.withdrawToken(ADDRESS_DAI, payable(USER_RECEIVER), 1000);
+
+        assertEq(dai.balanceOf(USER_RECEIVER), initialBalance + 1000);
+    }
+
+    function test_OwnerCanWithdrawNativeToken() public {
+        // fund receiver with native tokens
+        vm.deal(address(receiver), 1 ether);
+
+        uint256 initialBalance = USER_RECEIVER.balance;
+
+        // pull token
+        vm.startPrank(USER_DIAMOND_OWNER);
+
+        receiver.withdrawToken(address(0), payable(USER_RECEIVER), 1 ether);
+
+        assertEq(USER_RECEIVER.balance, initialBalance + 1 ether);
+    }
+
+    function test_WithdrawTokenWillRevertIfExternalCallFails() public {
+        vm.deal(address(receiver), 1 ether);
+
+        // deploy contract that cannot receive ETH
+        NonETHReceiver nonETHReceiver = new NonETHReceiver();
+
+        vm.startPrank(USER_DIAMOND_OWNER);
+
+        vm.expectRevert(abi.encodeWithSignature("ExternalCallFailed()"));
+
+        receiver.withdrawToken(
+            address(0),
+            payable(address(nonETHReceiver)),
+            1 ether
+        );
+    }
+
+    function test_revert_WithdrawTokenNonOwner() public {
+        vm.startPrank(USER_SENDER);
+        vm.expectRevert(UnAuthorized.selector);
+        receiver.withdrawToken(ADDRESS_DAI, payable(USER_RECEIVER), 1000);
+    }
+
+    function test_contractIsSetUpCorrectly() public {
+        assertEq(address(receiver.EXECUTOR()) == address(executor), true);
+        assertEq(receiver.MINIMUM_GAS() == MINIMUM_GAS, true);
+        assertEq(receiver.RECOVER_GAS() == RECOVER_GAS, true);
+        assertEq(receiver.OUTPUT_SETTLER() == OUTPUT_SETTLER_COIN, true);
+    }
+
+    function _getSwapData(
+        address _sendingAssetId,
+        address _receivingAssetId
+    )
+        public
+        view
+        returns (LibSwap.SwapData[] memory swapData, uint256 amountOutMin)
+    {
+        address[] memory path = new address[](2);
+        path[0] = _sendingAssetId;
+        path[1] = _receivingAssetId;
+
+        uint256 amountIn = defaultUSDCAmount;
+
+        // Calculate USDC input amount
+        uint256[] memory amounts = uniswap.getAmountsOut(amountIn, path);
+        amountOutMin = amounts[1];
+        swapData = new LibSwap.SwapData[](1);
+        swapData[0] = LibSwap.SwapData({
+            callTo: address(uniswap),
+            approveTo: address(uniswap),
+            sendingAssetId: _sendingAssetId,
+            receivingAssetId: _receivingAssetId,
+            fromAmount: amountIn,
+            callData: abi.encodeWithSelector(
+                uniswap.swapExactTokensForTokens.selector,
+                amountIn,
+                amountOutMin,
+                path,
+                address(executor),
+                block.timestamp + 20 minutes
+            ),
+            requiresDeposit: true
+        });
+    }
+
+    function test_OnlyOutputSettlerCanCallOutputFilled() public {
+        // mock-send bridged funds to receiver contract
+        deal(ADDRESS_USDC, address(receiver), defaultUSDCAmount);
+
+        (LibSwap.SwapData[] memory swapData, ) = _getSwapData(
+            ADDRESS_USDC,
+            ADDRESS_WRAPPED_NATIVE
+        );
+
+        bytes memory payload = abi.encode(transferId, swapData, USER_RECEIVER);
+
+        // call from deployer of ReceiverStargateV2
+        vm.startPrank(address(this));
+        vm.expectRevert(UnAuthorized.selector);
+
+        receiver.outputFilled{ gas: 400000 }(
+            bytes32(uint256(uint160(ADDRESS_USDC))),
+            defaultUSDCAmount,
+            payload
+        );
+        // call from owner of ReceiverStargateV2
+        vm.startPrank(address(this));
+        vm.expectRevert(UnAuthorized.selector);
+
+        receiver.outputFilled{ gas: 400000 }(
+            bytes32(uint256(uint160(ADDRESS_USDC))),
+            defaultUSDCAmount,
+            payload
+        );
+        // call from owner of user
+        vm.startPrank(USER_SENDER);
+        vm.expectRevert(UnAuthorized.selector);
+
+        receiver.outputFilled{ gas: 400000 }(
+            bytes32(uint256(uint160(ADDRESS_USDC))),
+            defaultUSDCAmount,
+            payload
+        );
+    }
+
+    function test_revert_lessThanMinimumGasProvided() public {
+        (LibSwap.SwapData[] memory swapData, ) = _getSwapData(
+            ADDRESS_USDC,
+            ADDRESS_DAI
+        );
+
+        bytes memory payload = abi.encode(transferId, swapData, USER_RECEIVER);
+
+        vm.startPrank(OUTPUT_SETTLER_COIN);
+        vm.expectRevert(ReceiverOIF.NotEnoughGas.selector);
+
+        receiver.outputFilled{ gas: MINIMUM_GAS - 1 }(
+            bytes32(uint256(uint160(ADDRESS_USDC))),
+            defaultUSDCAmount,
+            payload
+        );
+    }
+
+    function test_canExecuteSwap() public {
+        (
+            LibSwap.SwapData[] memory swapData,
+            uint256 amountOutMin
+        ) = _getSwapData(ADDRESS_USDC, ADDRESS_DAI);
+
+        bytes memory payload = abi.encode(transferId, swapData, USER_RECEIVER);
+
+        MandateOutput memory output = MandateOutput({
+            oracle: bytes32(0), // not relevant
+            settler: bytes32(uint256(uint160(OUTPUT_SETTLER_COIN))),
+            chainId: block.chainid,
+            token: bytes32(uint256(uint160(ADDRESS_USDC))),
+            amount: defaultUSDCAmount,
+            recipient: bytes32(uint256(uint160(address(receiver)))),
+            callbackData: payload,
+            context: hex""
+        });
+
+        // demonstrates that fill(...) call is not permissioned or have to be connected to an actual contract.
+        address randomFillerAccount = makeAddr("randomFillerAccount");
+        vm.startPrank(randomFillerAccount);
+
+        // Give filler funds to fill output
+        deal(ADDRESS_USDC, address(randomFillerAccount), defaultUSDCAmount);
+
+        IERC20(ADDRESS_USDC).approve(OUTPUT_SETTLER_COIN, defaultUSDCAmount);
+
+        vm.expectEmit();
+        emit LiFiTransferCompleted(
+            transferId,
+            ADDRESS_USDC,
+            USER_RECEIVER,
+            amountOutMin,
+            block.timestamp
+        );
+        OutputSettler(OUTPUT_SETTLER_COIN).fill{ gas: 600000 }(
+            keccak256("orderId"),
+            output,
+            type(uint48).max,
+            abi.encode(randomFillerAccount)
+        );
+
+        assertTrue(dai.balanceOf(USER_RECEIVER) == amountOutMin);
+    }
+
+    function test_canRecoverRevertsERC20() public {
+        (LibSwap.SwapData[] memory swapData, ) = _getSwapData(
+            ADDRESS_USDC,
+            ADDRESS_DAI
+        );
+        // Screw up the swapData. This is not be valid calldata.
+        swapData[0].callData = abi.encode(swapData[0].callData);
+
+        bytes memory payload = abi.encode(transferId, swapData, USER_RECEIVER);
+
+        MandateOutput memory output = MandateOutput({
+            oracle: bytes32(0), // not relevant
+            settler: bytes32(uint256(uint160(OUTPUT_SETTLER_COIN))),
+            chainId: block.chainid,
+            token: bytes32(uint256(uint160(ADDRESS_USDC))),
+            amount: defaultUSDCAmount,
+            recipient: bytes32(uint256(uint160(address(receiver)))),
+            callbackData: payload,
+            context: hex""
+        });
+
+        // demonstrates that fill(...) call is not permissioned or have to be connected to an actual contract.
+        address randomFillerAccount = makeAddr("randomFillerAccount");
+        vm.startPrank(randomFillerAccount);
+
+        // Give filler funds to fill output
+        deal(ADDRESS_USDC, address(randomFillerAccount), defaultUSDCAmount);
+        IERC20(ADDRESS_USDC).approve(OUTPUT_SETTLER_COIN, defaultUSDCAmount);
+
+        vm.expectEmit();
+        emit LiFiTransferRecovered(
+            transferId,
+            ADDRESS_USDC,
+            USER_RECEIVER,
+            defaultUSDCAmount,
+            block.timestamp
+        );
+        OutputSettler(OUTPUT_SETTLER_COIN).fill{ gas: 600000 }(
+            keccak256("orderId"),
+            output,
+            type(uint48).max,
+            abi.encode(randomFillerAccount)
+        );
+
+        assertTrue(
+            IERC20(ADDRESS_USDC).balanceOf(USER_RECEIVER) == defaultUSDCAmount
+        );
+    }
+
+    function test_canRecoverRevertsNative() public {
+        uint256 amount = defaultUSDCAmount;
+        // While this is a native swap, we don't need to fix the swapData since we very strictly wants this to fail.
+        (LibSwap.SwapData[] memory swapData, ) = _getSwapData(
+            ADDRESS_USDC,
+            ADDRESS_DAI
+        );
+        // Screw up the swapData. This is not be valid calldata.
+        swapData[0].callData = abi.encode(swapData[0].callData);
+
+        bytes memory payload = abi.encode(transferId, swapData, USER_RECEIVER);
+
+        MandateOutput memory output = MandateOutput({
+            oracle: bytes32(0), // not relevant
+            settler: bytes32(uint256(uint160(OUTPUT_SETTLER_COIN))),
+            chainId: block.chainid,
+            token: bytes32(0),
+            amount: amount,
+            recipient: bytes32(uint256(uint160(address(receiver)))),
+            callbackData: payload,
+            context: hex""
+        });
+
+        // demonstrates that fill(...) call is not permissioned or have to be connected to an actual contract.
+        address randomFillerAccount = makeAddr("randomFillerAccount");
+        vm.startPrank(randomFillerAccount);
+
+        // Give filler funds to fill output
+        deal(address(randomFillerAccount), amount);
+
+        vm.expectEmit();
+        emit LiFiTransferRecovered(
+            transferId,
+            address(0),
+            USER_RECEIVER,
+            amount,
+            block.timestamp
+        );
+        OutputSettler(OUTPUT_SETTLER_COIN).fill{ gas: 600000, value: amount }(
+            keccak256("orderId"),
+            output,
+            type(uint48).max,
+            abi.encode(randomFillerAccount)
+        );
+
+        assertTrue(USER_RECEIVER.balance == amount);
+    }
+}
