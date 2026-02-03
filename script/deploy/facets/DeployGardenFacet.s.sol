5779b385702359babee76efb5e5550ec54bb96e2
Daniel Bläcker <D.Blaecker@gmail.com>
2026-02-03 10:47:15 +0700

    update garden config

--- script/deploy/facets/DeployGardenFacet.s.sol
+++ script/deploy/facets/DeployGardenFacet.s.sol
@@ -1,36 +1,26 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity ^0.8.17;
 
-import { DeployScriptBase } from "./utils/DeployScriptBase.sol";
-import { stdJson } from "forge-std/Script.sol";
-import { GardenFacet } from "lifi/Facets/GardenFacet.sol";
+import {DeployScriptBase} from "./utils/DeployScriptBase.sol";
+import {stdJson} from "forge-std/Script.sol";
+import {GardenFacet} from "lifi/Facets/GardenFacet.sol";
 
 contract DeployScript is DeployScriptBase {
     using stdJson for string;
 
     constructor() DeployScriptBase("GardenFacet") {}
 
-    function run()
-        public
-        returns (GardenFacet deployed, bytes memory constructorArgs)
-    {
+    function run() public returns (GardenFacet deployed, bytes memory constructorArgs) {
         constructorArgs = getConstructorArgs();
 
-        deployed = GardenFacet(
-            deploy(
-                bytes.concat(type(GardenFacet).creationCode, constructorArgs)
-            )
-        );
+        deployed = GardenFacet(deploy(bytes.concat(type(GardenFacet).creationCode, constructorArgs)));
     }
 
     function getConstructorArgs() internal override returns (bytes memory) {
         string memory path = string.concat(root, "/config/garden.json");
 
         // Read the htlcRegistry address from config
-        address htlcRegistry = _getConfigContractAddress(
-            path,
-            string.concat(".", network, ".htlcRegistry")
-        );
+        address htlcRegistry = _getConfigContractAddress(path, string.concat(".htlcRegistry.", network));
 
         return abi.encode(htlcRegistry);
     }
