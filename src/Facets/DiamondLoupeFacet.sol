33185f37f98a3325ca3412dfe34c06ed8fc7ea77
Pablo Urriza <pablo.urriza@gmail.com>
2026-02-25 15:48:45 +0100

    chore: bump DiamondLoupeFacet to 1.0.2

--- src/Facets/DiamondLoupeFacet.sol
+++ src/Facets/DiamondLoupeFacet.sol
@@ -8,7 +8,7 @@ import { IERC165 } from "../Interfaces/IERC165.sol";
 /// @title Diamond Loupe Facet
 /// @author LI.FI (https://li.fi)
 /// @notice Core EIP-2535 Facet for inspecting Diamond Proxies.
-/// @custom:version 1.0.1
+/// @custom:version 1.0.2
 contract DiamondLoupeFacet is IDiamondLoupe, IERC165 {
     // Diamond Loupe Functions
     ////////////////////////////////////////////////////////////////////
