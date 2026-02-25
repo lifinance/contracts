4ec24d648b4607652a9a91583f783e0079ac9578
Pablo Urriza <pablo.urriza@gmail.com>
2026-02-25 13:16:12 +0100

    DiamondLoupeFacet modified

--- src/Facets/DiamondLoupeFacet.sol
+++ src/Facets/DiamondLoupeFacet.sol
@@ -8,7 +8,7 @@ import { IERC165 } from "../Interfaces/IERC165.sol";
 /// @title Diamond Loupe Facet
 /// @author LI.FI (https://li.fi)
 /// @notice Core EIP-2535 Facet for inspecting Diamond Proxies.
-/// @custom:version 1.0.0
+/// @custom:version 1.0.1
 contract DiamondLoupeFacet is IDiamondLoupe, IERC165 {
     // Diamond Loupe Functions
     ////////////////////////////////////////////////////////////////////
@@ -67,16 +67,24 @@ contract DiamondLoupeFacet is IDiamondLoupe, IERC165 {
     }
 
     /// @notice Gets the facet that supports the given selector.
-    /// @dev If facet is not found return address(0).
+    /// @dev If selector is zero or facet is not found return address(0).
     /// @param _functionSelector The function selector.
     /// @return facetAddress_ The facet address.
     function facetAddress(
         bytes4 _functionSelector
     ) external view override returns (address facetAddress_) {
+        if (_functionSelector == 0) return address(0);
         LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
         facetAddress_ = ds
             .selectorToFacetAndPosition[_functionSelector]
             .facetAddress;
+    }
+
+    /// @notice Returns the number of facets registered on the diamond.
+    /// @return count_ The number of facets.
+    function facetCount() external view returns (uint256 count_) {
+        LibDiamond.DiamondStorage storage ds = LibDiamond.diamondStorage();
+        count_ = ds.facetAddresses.length;
     }
 
     // This implements ERC-165.
